4558bcf342b16fabc50ef9e8379a0fb6dd05027b
Michael Kareev <mkareev@umich.edu>
2020-05-09 23:05:20 -0400

    updates

--- Basic Data Structures/121_Best_Time_To_Buy_and_Sell_Stock.py
+++ Basic Data Structures/121_Best_Time_To_Buy_and_Sell_Stock.py
@@ -9,3 +9,15 @@ class Solution:
             loc_min = min(k, loc_min)
             gl_max = max(gl_max, k-loc_min)
         return gl_max
+
+    def maxProfit_another(self, prices: List[int]) -> int:
+        if len(prices) < 2:
+            return 0
+        min_price = float('inf')
+        profit = 0
+        for p_ix in range(len(prices)):
+            if prices[p_ix] < min_price:
+                min_price = prices[p_ix]
+            elif prices[p_ix] - min_price > profit:
+                profit = prices[p_ix] - min_price
+        return profit
