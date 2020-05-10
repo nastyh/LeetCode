fd38d450c533fe177fda3d24363a8b4b8c4df0b4
Michael Kareev <mkareev@umich.edu>
2020-05-10 13:05:18 -0400

    LL

--- Basic Data Structures/121_Best_Time_To_Buy_and_Sell_Stock.py
+++ Basic Data Structures/121_Best_Time_To_Buy_and_Sell_Stock.py
@@ -21,3 +21,18 @@ class Solution:
             elif prices[p_ix] - min_price > profit:
                 profit = prices[p_ix] - min_price
         return profit
+
+    def maxProfit_best(stock_prices):
+        if len(stock_prices) < 2:
+        raise ValueError('Getting a profit requires at least 2 prices')
+        min_price  = stock_prices[0]
+        max_profit = stock_prices[1] - stock_prices[0]
+
+        for current_time in range(1, len(stock_prices)):
+            current_price = stock_prices[current_time]
+
+        potential_profit = current_price - min_price
+        max_profit = max(max_profit, potential_profit)
+        min_price = min(min_price, current_price)
+
+        return max_profit
---
+++ Linked Lists/Linked_List_implementation_basic_func.py
@@ -0,0 +1,116 @@
+# Implementation of general Linked List and basic functions
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def getData(self):
+        return self.val
+
+    def getNext(self):
+        return self.next
+
+class LinkedList:
+    def __init__(self,val):
+        self.head = Node(val)
+
+    def isEmpty(self):
+        self.head == None
+
+    def addItem(self, newitem): # adding to the beginning of the list
+        temp = Node(newitem)
+        temp.next = self.head
+        self.head = temp
+
+    def length(self):
+        l = 0
+        curr = self.head
+        while curr:
+            l += 1
+            curr = curr.next
+        return l
+
+    def print(self):
+        curr = self.head
+        while curr:
+            print(curr.val, end=' ')
+            curr = curr.next
+
+    def search(self, searchVal):
+        curr = self.head
+        if not curr:
+            return False
+        while curr:
+            if curr.val == searchVal:
+                return True
+            else:
+                curr = curr.next
+        return False
+
+    def elementIx(self, searchval): # returns index of the given element
+        curr, ix = self.head, 0
+        if not curr:
+            return None
+        while curr:
+            if curr.val == searchVal:
+                return ix
+            else:
+                curr = curr.next
+                ix += 1
+
+    def delete(self, delElement):
+        if not self.head:
+            return None
+        prev, curr = None, self.head
+        if curr and curr.val == delElement: # if the node to be deleted is the head node
+            self.head = curr.next
+            curr = None
+            return
+        while curr and curr.val != delElement: # moving through the list
+            prev = curr
+            curr = curr.next
+        if not curr:
+            return False
+        prev.next = curr.next
+        curr = None
+
+
+    def delAtPos(self, ix): #deleting a node at position ix. ix starts with 0
+        if not self.head:
+            return False
+        curr = self.head
+        if ix == 0:
+            self.head = curr.next
+            curr = None
+            return
+        prev, counter = None, 1
+        while curr and counter != ix:
+            prev = curr
+            curr = curr.next
+            counter += 1
+
+        if curr is None:
+            return False   # went to the end of the list and haven't reached the ix
+        prev.next = curr.next # deletion
+        curr = None
+
+
+    def insertAt(self, newvalue, ix): # insert newvalue at the spot ix
+        prev, curr, counter = None, self.head, 0
+        if curr is None: # if there is nothing, we'll insert in the beginning no matter what ix is passed
+            curr = Node(newvalue)
+        while curr and counter != ix:
+            prev = curr
+            curr = curr.next
+            counter += 1
+        temp = Node(newvalue)
+        prev.next = temp
+        temp.next = curr
+
+
+
+
+
+
+
