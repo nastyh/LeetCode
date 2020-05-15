a5d512c6984c85e5a52eb52a440eb045847f2072
Michael Kareev <mkareev@umich.edu>
2020-05-14 20:33:59 -0400

    various

--- Trees/226_Invert_Binary_tree.py
+++ Trees/226_Invert_Binary_tree.py
@@ -1,38 +1,48 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def invertTree(self, root: TreeNode) -> TreeNode:
-        #DFS
-        # if not root:
-        #     return None
-        # # if not root.left and not root.right:
-        # #     return root
-        # # if root.left and not root.right:
-        # #     return None
-        # # if not root.left and root.right:
-        # #     return None
-        # root.left, root.right = root.right, root.left
-        # self.invertTree(root.left)
-        # self.invertTree(root.right)
-        # return root
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
-        #BFS
-        from collections import deque
+    def invertTree(self, root):
+        #DFS
         if not root:
             return None
-        s = deque()
-        s.append(root)
-        while s:
-            t = s.popleft()
-            if t:
-                t.left, t.right = t.right, t.left
-                s.extend([t.right, t.left])
-                # if t.left:
-                #     s.append(t.right)
-                # if t.right:
-                #     s.append(t.left)
+        if root and (not root.left and not root.right):
+            return root
+        if root.left and not root.right:
+            return None
+        if not root.left and root.right:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
         return root
+
+        #BFS
+        # from collections import deque
+        # if not root:
+        #     return None
+        # s = deque()
+        # s.append(root)
+        # while s:
+        #     t = s.popleft()
+        #     if t:
+        #         t.left, t.right = t.right, t.left
+        #         s.extend([t.right, t.left])
+
+        # return root
+
+
+if __name__ == '__main__':
+    l = TreeNode(4)
+    l.left = TreeNode(2)
+    l.right = TreeNode(7)
+    l.left.left = TreeNode(1)
+    l.left.right = TreeNode(6)
+    l.right.left = TreeNode(3)
+    l.right.right = TreeNode(1)
+    # l.right.right = TreeNode(18)
+
+print(l.invertTree(l))
--- Trees/230_Kth_smallest_el_in_binary_tree.py
+++ Trees/230_Kth_smallest_el_in_binary_tree.py
@@ -25,6 +25,20 @@ class TreeNode:
         return out[k-1] if k <= len(out) else None
 
 
+     def kthSmallest_another(self, root, k):
+        stack = []
+
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
+
+
 if __name__ == '__main__':
     l = TreeNode(3)
     l.left = TreeNode(1)
