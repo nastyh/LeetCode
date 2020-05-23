95662e18f66f78eaa29757910c9f57879b63fad0
Michael Kareev <mkareev@umich.edu>
2020-05-23 15:24:39 -0400

    f

---
+++ Basic Data Structures/5_Longest_Palindromic_Substring.py
@@ -0,0 +1,15 @@
+if not s: return s
+
+        res = ""
+        for i in range(len(s)):
+            j = i + 1
+            # While j is less than length of string
+            # AND res is *not* longer than substring s[i:]
+            while j <= len(s) and len(res) <= len(s[i:]):
+                # If substring s[i:j] is a palindrome
+                # AND substring is longer than res
+                if s[i:j] == s[i:j][::-1] and len(s[i:j]) > len(res):
+                    res = s[i:j]
+                j += 1
+
+        return res
--- Trees/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
+++ Trees/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.py
@@ -22,3 +22,55 @@ class TreeNode:
 
         return res
 
+    def buildTree_Iter(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+    if not len(preorder) or not len(inorder):
+        return None
+
+    root = TreeNode()
+    #create a hash table of indexes and use it instead of array for faster access
+    inorder_index = {inorder[i]:i for i in range(0, len(inorder))}
+    #a stack of nodes and its corresponding ranges
+    nodes = [[root, 0, len(preorder)-1]]
+
+    for i in range(len(preorder)):
+        curr_node, l, h = nodes.pop()
+        curr_node.val = preorder[i]
+        #find the index of the node in inorder, then use the divide and conquere technique
+        mid = inorder_index[curr_node.val]
+
+        if mid<h:
+            curr_node.right = TreeNode()
+            #appent the right subtree first, as stack follows LIFO approach
+            nodes.append([curr_node.right, mid+1, h])
+        if l<mid:
+            curr_node.left = TreeNode()
+            #this will be popped before the right subtree
+            nodes.append([curr_node.left, l, mid-1])
+
+    return root
+
+    def buildTree_iter_another(self, preorder, inorder):
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        stack = []
+        stack.append(root)
+
+        pre = 1
+        ino = 0
+        while (pre < len(preorder)):
+            curr = TreeNode(preorder[pre])
+            pre += 1
+            prev = None
+            while stack and stack[-1].val == inorder[ino]:
+                prev = stack.pop()
+                ino += 1
+            if prev:
+                prev.right = curr
+            else:
+                stack[-1].left = curr
+
+            stack.append(curr)
+        return root
+
