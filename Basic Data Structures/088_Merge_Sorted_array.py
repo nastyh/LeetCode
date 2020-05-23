2727b05fc6a8c05d64de8a28ebc3ec97ad9fb1fb
Michael Kareev <mkareev@umich.edu>
2020-05-22 20:15:06 -0400

    Another implementation

--- Basic Data Structures/088_Merge_Sorted_array.py
+++ Basic Data Structures/088_Merge_Sorted_array.py
@@ -3,28 +3,45 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        res = [None] * (m + n)
-        i, j, k = 0, 0, 0
 
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                res[k] = nums1[i]
-                i += 1
-                k += 1
-            else:
-                res[k] = nums2[j]
-                j += 1
-                k += 1
+        nums2_index=0
+        nums1_index = 0
+        while nums2_index < n and nums1_index < m:
+            if nums2[nums2_index] <= nums1[nums1_index]:
+                nums1.insert(nums1_index, nums2[nums2_index])
+                m += 1
+                nums2_index += 1
+            nums1_index += 1
+        while nums2_index < n:
+            nums1.insert(m, nums2[nums2_index])
+            m += 1
+            nums2_index += 1
+        for _ in range(n):
+            nums1.pop()
 
-        while i < m:
-            res[k] = nums1[i]
-            i += 1
-            k += 1
 
-        while j < n:
-            res[k] = nums2[j]
-            j += 1
-            k += 1
+        # res = [None] * (m + n)
+        # i, j, k = 0, 0, 0
 
-        return res
+        # while i < m and j < n:
+        #     if nums1[i] <= nums2[j]:
+        #         res[k] = nums1[i]
+        #         i += 1
+        #         k += 1
+        #     else:
+        #         res[k] = nums2[j]
+        #         j += 1
+        #         k += 1
+
+        # while i < m:
+        #     res[k] = nums1[i]
+        #     i += 1
+        #     k += 1
+
+        # while j < n:
+        #     res[k] = nums2[j]
+        #     j += 1
+        #     k += 1
+
+        # return res
 
