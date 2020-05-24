b5fe749d6f29563d471cecfb80d532b635a78b3f
Michael Kareev <mkareev@umich.edu>
2020-05-24 12:36:05 -0400

    b

---
+++ Basic Data Structures/003_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,27 @@
+def lengthOfLongestSubstring(s):
+    m = set()
+    res, l, i = 0, 0, 0
+
+    while i < len(s):
+        if s[i] not in m:
+            m.add(s[i])
+            res = max(res, len(m))
+            i += 1
+        else:
+            m.remove(s[l])
+            l += 1
+    return res
+
+def lengthOfLongestSubstring_dict(self, s):
+    dic = {}
+    res = 0
+    left = -1
+    for i, ch in enumerate(s):
+        if ch in dic:
+            left = max(left, dic[ch])
+        dic[ch] = i
+        res = max(res, i-left)
+    return res
+
+if __name__ == '__main__':
+    print(lengthOfLongestSubstring('abcabcbb'))
