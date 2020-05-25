d16f89a238837bf28ae39d3f07f5fb10adb34967
Michael Kareev <mkareev@umich.edu>
2020-05-25 11:46:57 -0400

    Update Basic Data Structures/167_Two_sum_II_Input_is_sorted.py

---
+++ Basic Data Structures/167_Two_sum_II_Input_is_sorted.py
@@ -0,0 +1,10 @@
+def twoSum(numbers, target): # numbers are in the ascending order
+    l, r = 0, len(numbers) - 1
+    while l < r:
+        if numbers[l] + numbers[r] > target:
+            r -=1
+        elif numbers[l] + numbers[r] < target:
+            l += 1
+        else:
+            return [l + 1, r + 1]
+
