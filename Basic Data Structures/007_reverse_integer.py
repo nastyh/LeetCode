cd4425498428bbe1b3304230a968d5a266839439
Michael Kareev <mkareev@umich.edu>
2020-05-25 10:18:59 -0400

    Update Basic Data Structures/007_reverse_integer.py

---
+++ Basic Data Structures/007_reverse_integer.py
@@ -0,0 +1,17 @@
+def reverse(x):
+    if x > 0:  # handle positive numbers
+        a =  int(str(x)[::-1])
+    if x <=0:  # handle negative numbers
+        a = -1 * int(str(x*-1)[::-1])
+        # handle 32 bit overflow
+    mina = -2**31
+    maxa = 2**31 - 1
+    if a not in range(mina, maxa):
+        return 0
+    else:
+        return a
+
+
+
+if __name__ == '__main__':
+    print(reverse(123))
