f6a111abffb9bf62da886256d0137f4f0028603c
Michael Kareev <mkareev@umich.edu>
2020-05-31 14:07:20 -0400

    Update Basic Data Structures/973_K_Closest_Points_to_Origin.py

--- Basic Data Structures/973_K_Closest_Points_to_Origin.py
+++ Basic Data Structures/973_K_Closest_Points_to_Origin.py
@@ -1,4 +1,4 @@
-import math
+import math, heapq
 def kClosest(points, K):
     if K > len(points):
         return None
@@ -15,6 +15,19 @@ def kClosest(points, K):
 
     return values_back[:K]
 
+def kClosest_alt(points, K):
+    if K > len(points):
+        return None
+    res, curr_res = [], []
+    for point in points:
+        heapq.heappush(curr_res, (math.sqrt( (0 - point[0])**2 + (0-point[1])**2 ), point))
+    res = []
+    for i in range(K):
+        res.append(heapq.heappop(curr_res)[1])
+    return res
+
+
 
 if __name__ == '__main__':
     print(kClosest([[1,3],[-2,2]], 1))
+    print(kClosest_alt([[1,3],[-2,2]], 1))
