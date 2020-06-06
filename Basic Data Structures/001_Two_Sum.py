6f5597a214d57b4340ba3cc7a71222403d0befc1
Michael Kareev <mkareev@umich.edu>
2020-06-06 13:38:34 -0400

    Adding alternative solutions

--- Basic Data Structures/001_Two_Sum.py
+++ Basic Data Structures/001_Two_Sum.py
@@ -1,12 +1,26 @@
-class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        for i, n in enumerate(nums):
-            r = target - n
-            if r in nums[i+1:]:
-                j = nums.index(r,i+1)
-                return [i, j]
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    for i, n in enumerate(nums):
+        r = target - n
+        if r in nums[i+1:]:
+            j = nums.index(r,i+1)
+            return [i, j]
+
+def twoSum_dict(nums, target):
+    d = {}
+    for k, v in enumerate(nums):
+        if v not in d:
+            d[target - v] = k
+        else:
+            return [d[v], k]
+    return []
+
+
+if __name__ == '__main__':
+    print(twoSum([2,7,11, 15], 9))
+    print(twoSum_dict([2,7,11, 15], 9))
+
