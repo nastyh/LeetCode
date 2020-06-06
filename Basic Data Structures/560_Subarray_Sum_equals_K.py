7ac78e59e25c148ace0e50634fa8baa2c4b6f069
Michael Kareev <mkareev@umich.edu>
2020-06-06 16:00:22 -0400

    Adding alternative solutions

--- Basic Data Structures/560_Subarray_Sum_equals_K.py
+++ Basic Data Structures/560_Subarray_Sum_equals_K.py
@@ -1,14 +1,5 @@
-def subarraySum(nums, k):
-        res, curr_sum, start = 0, 0, nums[0]
-        for i in range(1, len(nums)):
-            curr_sum = start + nums[i]
-            if curr_sum == k:
-                res += 1
-                start = nums[i]
-        return res
 
-
-def subarraySum(nums, k):
+def subarraySum_d(nums, k):
     count = s = 0
     Map = {}
     Map[0] = 1
@@ -19,7 +10,7 @@ def subarraySum(nums, k):
         Map[s] = Map.get(s,0) +1
     return count
 
-def subarraySum(nums, k):
+def subarraySum_another(nums, k):
     count = 0
     for start in range(len(nums)):
         sum = 0
@@ -31,4 +22,5 @@ def subarraySum(nums, k):
 
 
 if __name__ == '__main__':
-    print(subarraySum([1,1,1], 2))
+    print(subarraySum_d([1,2,3], 3))
+    print(subarraySum_another([1,2,3], 3))
