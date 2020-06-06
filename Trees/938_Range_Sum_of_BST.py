1e484f895e714840b3dea54d566dc5363ff82ec0
Michael Kareev <mkareev@umich.edu>
2020-06-05 23:29:58 -0400

    Adding recursion

--- Trees/938_Range_Sum_of_BST.py
+++ Trees/938_Range_Sum_of_BST.py
@@ -28,6 +28,20 @@ class TreeNode:
         # return sum(i for i in _helper(root) if i >= L and i <= R) # main solution
         return sum(_helper_alt(root, L, R)) # alternative solution
 
+    def rangeSumBST_recurs(self, root, L, R):
+        def _helper(node, L, R):
+            res = 0
+            if not node: return res
+            if node:
+                if L <= node.val <= R:
+                    res += node.val
+                if node.val > L:
+                    res += _helper(node.left, L, R)
+                if node.val < R:
+                    res += _helper(node.right, L, R)
+            return res
+        return _helper(root, L, R)
+
 
 
 
@@ -40,3 +54,4 @@ if __name__ == '__main__':
     l.right.right = TreeNode(18)
 
     print(l.rangeSumBST(l, 7, 15))
+    print(l.rangeSumBST_recurs(l, 7, 15))
