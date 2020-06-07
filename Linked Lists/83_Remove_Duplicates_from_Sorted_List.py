898791da30afc121f26020168cb934066f9e3214
Michael Kareev <mkareev@umich.edu>
2020-06-06 20:44:59 -0400

    Update Linked Lists/83_Remove_Duplicates_from_Sorted_List.py

---
+++ Linked Lists/83_Remove_Duplicates_from_Sorted_List.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        curr = head
+        while curr and curr.next:
+            if curr.next.val == curr.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
