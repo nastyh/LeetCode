1798b49c20041875bc50dd3b20f807fbe97eeccc
Michael Kareev <mkareev@umich.edu>
2020-06-07 12:58:43 -0400

    Added DFS

--- Trees/637_Average_of_Levels_in_Binary_Tree.py
+++ Trees/637_Average_of_Levels_in_Binary_Tree.py
@@ -22,6 +22,27 @@ class TreeNode:
             res.append(curr_avg)
         return res
 
+
+    def averageOfLevels_dfs(self,root):
+        if root is not None:
+            output = []
+            depth = 0
+
+            def trav(node, depth):
+                depth += 1
+                if len(output) < depth:
+                    output.append([])
+                output[depth - 1].append(node.val)
+                if node.left is not None:
+                    trav(node.left, depth)
+                if node.right is not None:
+                    trav(node.right, depth)
+
+            trav(root, depth)
+            return [sum(x)/len(x) for x in output]
+        else:
+            return []
+
 if __name__ == '__main__':
     l = TreeNode(3)
     l.left = TreeNode(9)
@@ -29,3 +50,4 @@ if __name__ == '__main__':
     l.right.left = TreeNode(15)
     l.right.right = TreeNode(7)
     print(l.averageOfLevels(l))
+    print(l.averageOfLevels_dfs(l))
