1eba226d1a40daaa3307ae90f5f4cc81f51c6cd4
Michael Kareev <mkareev@umich.edu>
2020-06-06 20:52:54 -0400

    Adding alt

--- Linked Lists/83_Remove_Duplicates_from_Sorted_List.py
+++ Linked Lists/83_Remove_Duplicates_from_Sorted_List.py
@@ -3,7 +3,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
+    def deleteDuplicates(self, head):
         curr = head
         while curr and curr.next:
             if curr.next.val == curr.val:
@@ -11,3 +11,22 @@ class ListNode:
             else:
                 curr = curr.next
         return head
+
+    # a bit easier to understand
+    def deleteDuplicates_alt(self, head):
+        if head == None:
+            return head
+
+        current = head.next
+        prev = head
+
+        while current != None:
+            if current.val == prev.val:
+                prev.next = current.next
+                current = current.next
+            else:
+                current = current.next
+                prev = prev.next
+
+        return head
+
