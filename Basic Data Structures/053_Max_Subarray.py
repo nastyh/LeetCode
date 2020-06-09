13ba17e20a3c064fef3a95b21a0f0b6db59bc2ea
Michael Kareev <mkareev@umich.edu>
2020-06-09 19:38:11 -0400

    Added alternative solutions

--- Basic Data Structures/053_Max_Subarray.py
+++ Basic Data Structures/053_Max_Subarray.py
@@ -1,15 +1,14 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+def maxSubArray_1(nums):
         if all(x >= 0 for x in nums):
             return sum(nums)
+
         current, glob = nums[0], nums[0]
         for i in range(1, len(nums)):
-            current = max(current, current+nums[i])
+            current = max(nums[i], current+nums[i])
             glob = max(current, glob)
         return glob
 
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+def maxSubArray_2(nums):
         dp = 0
         maxSum = None
         for n in nums:
@@ -17,3 +16,8 @@ class Solution:
             if maxSum is None or dp > maxSum:
                 maxSum = dp
         return maxSum
+
+if __name__ == '__main__':
+    print(maxSubArray_1([-2,1,-3,4,-1,2,1,-5,4]))
+    print(maxSubArray_2([-2,1,-3,4,-1,2,1,-5,4]))
+
