7f4e702d92f8a8a7118bac9a162cf11eb0f73150
Michael Kareev <mkareev@umich.edu>
2020-06-10 14:24:18 -0400

    Alternative solution

--- Basic Data Structures/014_Longest_Common_prefix.py
+++ Basic Data Structures/014_Longest_Common_prefix.py
@@ -8,7 +8,19 @@ def longestCommonPrefix(strs):
                 return strs[0][:i]
     return strs[0]
 
+# easier to comprehend
+
+def longestCommonPrefix_alt(strs):
+    if len(strs) == 0: return ''
+    zipped = zip(*strs)
+    prefix_ix = 0
+    for ch in zipped:
+        unique = set(ch)
+        if len(unique) != 1: break
+        else:
+            prefix_ix += 1
+    return strs[0][:prefix_ix]
 
 if __name__ == '__main__':
     print(longestCommonPrefix(["flower","flow","flight"]))
-
+    print(longestCommonPrefix_alt(["flower","flow","flight"]))
