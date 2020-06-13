2a101dbe1991f2ec2adc6f8932a8cd31193995ef
Michael Kareev <mkareev@umich.edu>
2020-06-13 19:47:10 -0400

    cleaning

--- Basic Data Structures/1198_Find_Smallest_Common_Element_in_All_Rows.py
+++ Basic Data Structures/1198_Find_Smallest_Common_Element_in_All_Rows.py
@@ -1,19 +1,16 @@
 def smallestCommonElement(mat):
     d = {}
-    elements = []
     for el in mat:
         for i in el:
             if i in d:
                 d[i] += 1
             else:
                 d[i] = 1
-    # return d
+
     if len(mat) not in d.values():
         return -1
     else:
         return [k for k,v in d.items() if v == len(mat)][0]
-    # return [k for k,v in d.items() if v == len(mat)][0]
-    # return [k if v == len(mat) else -1 for k,v in d.items()]
 
 if __name__ == '__main__':
     print(smallestCommonElement([[1,2,3,4,5],[2,4,5,8,10],[3,5,7,9,11],[1,3,5,7,9]]))
