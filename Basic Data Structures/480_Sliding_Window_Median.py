afb1c3c6980bdffe106ef7a20090e884a212ee9b
Michael Kareev <mkareev@umich.edu>
2020-06-14 10:53:00 -0400

    Update Basic Data Structures/480_Sliding_Window_Median.py

---
+++ Basic Data Structures/480_Sliding_Window_Median.py
@@ -0,0 +1,38 @@
+import heapq
+def medianSlidingWindow(nums, k): # Brute Force, very slow
+    if len(nums) == 0: return None
+    l, r, res = 0, k, []
+    while r <= len(nums):
+        t = sorted(nums[l:r])
+        if len(t) % 2 == 1:
+            res.append(t[len(t) // 2])
+        else:
+            r_ix = len(t) // 2
+            m = (t[r_ix - 1] + t[r_ix]) / 2
+            res.append(m)
+        l += 1
+        r += 1
+    return res
+
+def medianSlidingWindow_heaps(nums, k): # with two heaps
+    if len(nums) == 0: return None
+    l, r, res, lh, rh = 0, k, [], [], []
+    while r <= len(nums):
+        t = nums[l:r]
+        lh = t[:len(t) // 2 + 1]
+        lh = lh * (-1)
+        rh = t[len(t) // 2:]
+        heapq.heapify(lh)
+        heapq.heapify(rh)
+        res.append( (heapq.heappop(lh) * (-1) + heapq.heappop(rh)) / 2 )
+        l += 1
+        r += 1
+        # heapq.heappush(lh, nums[])
+    return res
+
+
+
+if __name__ == '__main__':
+    # print(medianSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
+    # print(medianSlidingWindow([1,4,2,3], 4))
+    print(medianSlidingWindow_heaps([1,3,-1,-3,5,3,6,7], 3))
