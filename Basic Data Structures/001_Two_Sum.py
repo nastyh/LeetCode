e5265eb8ca8e964f1f0897475165c978ec72596b
Michael Kareev <mkareev@umich.edu>
2020-06-16 18:42:29 -0400

    Adding recursive solution

--- Basic Data Structures/001_Two_Sum.py
+++ Basic Data Structures/001_Two_Sum.py
@@ -19,8 +19,20 @@ def twoSum_dict(nums, target):
             return [d[v], k]
     return []
 
+def twoSum_recur(nums, target):
+    def _helper(nums, target, d = {}, i = 0):
+        if i < len(nums):
+            if target - nums[i] in d:
+                return [d[target - nums[i]], i]
+            else:
+                d[nums[i]] = i
+            i += 1
+        return _helper(nums, target, d, i)
+    return _helper(nums, target, {}, 0)
+
 
 if __name__ == '__main__':
-    print(twoSum([2,7,11, 15], 9))
-    print(twoSum_dict([2,7,11, 15], 9))
+    # print(twoSum([2,7,11, 15], 9))
+    # print(twoSum_dict([2,7,11, 15], 9))
+    print(twoSum_recur([2,7,11, 15], 9))
 
