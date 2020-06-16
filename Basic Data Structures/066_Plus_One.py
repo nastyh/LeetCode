7258da4b50f461f4d50f4d4778a54e7b19bb3a5d
Michael Kareev <mkareev@umich.edu>
2020-06-16 19:54:27 -0400

    Added recursion

--- Basic Data Structures/066_Plus_One.py
+++ Basic Data Structures/066_Plus_One.py
@@ -22,10 +22,24 @@ def plusOne_alt(digits):
     num = int(''.join([str(i) for i in digits])) + 1
     return [i for i in str(num)]
 
+def plusOne_recursion(digits):
+    if digits[-1] < 9:
+        digits[-1] += 1
+        return digits
+    elif len(digits) == 1 and digits[0] == 9:
+        return [1, 0]
+    else:
+        digits[-1] = 0
+        digits[0:-1] = plusOne(digits[0:-1])
+        return digits
+
 
 
 if __name__ == '__main__':
     print(plusOne([1,9,9]))
     print(plusOne_alt([1,9,9]))
+    print(plusOne_recursion([4]))
+    print(plusOne_recursion([9]))
+    print(plusOne_recursion([1, 9, 9]))
 
 
