ce2b91f35d6bd3b9e7590a019ef5ba544434ee3f
Michael Kareev <mkareev@umich.edu>
2020-06-19 19:53:30 -0400

    2 Solutions

--- Trees/Inorder_Predcessor_in_BST.py
+++ Trees/Inorder_Predcessor_in_BST.py
@@ -5,7 +5,7 @@ class TreeNode:
         self.left = None
         self.right = None
 
-    def inorderSuccessor(self, root, p): # create a list, return the next value
+    def inorderPredcessor(self, root, p): # create a list, return the previous value
 
         def _helper(root, li):
             if not root: return None
@@ -16,29 +16,29 @@ class TreeNode:
 
         values = _helper(root, [])
         p_ix = values.index(p.val)
-        return values[p_ix + 1] if values[-1] != values[p_ix] else None
+        return values[p_ix - 1] if values[0] != values[p_ix] else None
 
     """
-     if the node has the right subtree, return the most left node of that subtree
-     if the node doesn't have the right subtree, return the node from which you made the last left turn
+    if the node has the the left subtree, return the most right node of that subtree
+    If the node doesn't have the left subtree, return the node from which you made the last right turn
     """
 
-    def inorderSuccessor_BST_property(self, root, p):
+    def inorderPredcessor_BST_property(self, root, p):
         res = None
         if not root: return
-        if p.right:
-            t = p.right
-            while t.left:
-                t = t.left
+        if p.left:
+            t = p.left
+            while t.right:
+                t = t.right
             return t.val
 
         node = root
         while node.val != p.val:
-            if p.val <= node.val:
+            if node.val <= p.val:
                 res = node
-                node = node.left
-            else:
                 node = node.right
+            else:
+                node = node.left
         return res.val if res else None
 
 
@@ -52,6 +52,5 @@ if __name__ == '__main__':
     l.left.right = TreeNode(8)
     l.right.left = TreeNode(17)
     l.right.right = TreeNode(23)
-    print(l.inorderSuccessor(l, l.right.left))
-    print(l.inorderSuccessor_BST_property(l, l.right.left))
-
+    print(l.inorderPredcessor(l, l.left))
+    print(l.inorderPredcessor_BST_property(l, l.left))
