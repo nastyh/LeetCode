ec8a138eda25a92bcd4bcfa2cd8d949ffe6b2efb
Michael Kareev <mkareev@umich.edu>
2020-06-21 13:35:56 -0400

    Update Trees/785_Is_Graph_Bipartite.py

---
+++ Trees/785_Is_Graph_Bipartite.py
@@ -0,0 +1,28 @@
+"""
+a graph is bipartite if we can split it's set of nodes into two independent subsets A and B such that every edge in the graph has one node in A and another node in B.
+"""
+def isBipartite(graph):
+    """
+    Color a node blue if it is part of the first set, else red. We should be able to greedily color the graph if and only if it is bipartite: one node being blue implies all it's neighbors are red, all those neighbors are blue, and so on.
+    We'll keep an array (or hashmap) to lookup the color of each node: color[node]. The colors could be 0, 1, or uncolored (-1 or null).
+    To perform the depth-first search, we use a stack. For each uncolored neighbor in graph[node], we'll color it and add it to our stack, which acts as a sort of "todo list" of nodes to visit next.
+    """
+    color = {}
+    for node in xrange(len(graph)):
+        if node not in color:
+            stack = [node]
+            color[node] = 0
+            while stack:
+                node = stack.pop()
+                for nei in graph[node]:
+                    if nei not in color:
+                        stack.append(nei)
+                        color[nei] = color[node] ^ 1
+                    elif color[nei] == color[node]:
+                        return False
+        return True
+
+        """
+        Time Complexity: O(N + E)O(N+E), where NN is the number of nodes in the graph, and EE is the number of edges
+        Space Complexity: O(N)O(N), the space used to store the color
+        """
