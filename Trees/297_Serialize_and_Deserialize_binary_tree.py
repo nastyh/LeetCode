50a0fb073852f0009c9ddf9ce4944b28dbcb8969
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2020-06-22 18:28:11 -0400

    Create 297_Serialize_and_Deserialize_binary_tree.py

---
+++ Trees/297_Serialize_and_Deserialize_binary_tree.py
@@ -0,0 +1,83 @@
+# Serialization 
+class Codec:
+
+    def serialize(self, root):
+        """ Encodes a tree to a single string.
+        :type root: TreeNode
+        :rtype: str
+        """
+        def rserialize(root, string):
+            """ a recursive helper function for the serialize() function."""
+            # check base case
+            if root is None:
+                string += 'None,'
+            else:
+                string += str(root.val) + ','
+                string = rserialize(root.left, string)
+                string = rserialize(root.right, string)
+            return string
+        
+        return rserialize(root, '')
+
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        :type data: str
+        :rtype: TreeNode
+        """
+        def rdeserialize(l):
+            """ a recursive helper function for deserialization."""
+            if l[0] == 'None':
+                l.pop(0)
+                return None
+                
+            root = TreeNode(l[0])
+            l.pop(0)
+            root.left = rdeserialize(l)
+            root.right = rdeserialize(l)
+            return root
+
+        data_list = data.split(',')
+        root = rdeserialize(data_list)
+        return root
+
+
+        # BFS with queue
+
+    def serialize_bfs(self, root):
+        if not root:
+            return ''
+        queue = collections.deque()
+        queue.append(root)
+        res = ''
+        while queue:
+            node = queue.popleft()
+            if not node:
+                res += 'None,'
+                continue
+            res += str(node.val) + ','
+            queue.append(node.left)
+            queue.append(node.right)
+        return res
+            
+    def deserialize_bfs(self, data):
+        if not data:
+            return None
+        ls = data.split(',')
+        root = TreeNode(int(ls[0]))
+        queue = collections.deque()
+        queue.append(root)
+        i = 1
+        while queue and i < len(ls):
+            node = queue.popleft()
+            if ls[i] != 'None':
+                left = TreeNode(int(ls[i]))
+                node.left = left
+                queue.append(left)
+            i += 1
+            if ls[i] != 'None':
+                right = TreeNode(int(ls[i]))
+                node.right = right
+                queue.append(right)
+            i += 1
+        return root
