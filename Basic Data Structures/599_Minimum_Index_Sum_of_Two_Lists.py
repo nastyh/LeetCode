a77ebc525860d82333d7aba169e22c7d7aa860c2
Michael Kareev <mkareev@umich.edu>
2020-06-28 14:19:53 -0400

    Update Basic Data Structures/599_Minimum_Index_Sum_of_Two_Lists.py

---
+++ Basic Data Structures/599_Minimum_Index_Sum_of_Two_Lists.py
@@ -0,0 +1,21 @@
+def findRestaurant(list1, list2):
+    shorter = list1 if len(list1) < len(list2) else list2
+    longer = list1 if len(list1) >= len(list2) else list2
+    shorter_d, longer_d, combined_d = {}, {}, {}
+    for k, v in enumerate(shorter):
+        shorter_d[v] = k
+    for k, v in enumerate(longer):
+        longer_d[v] = k
+
+    for key in shorter_d:
+        if key in longer_d:
+            combined_d[key] = shorter_d[key] + longer_d[key]
+
+    # return min(combined_d.items(), key = lambda x: x[1])[0]
+    return [k for k, v in combined_d.items() if v == min(combined_d.values())][0]
+
+
+
+if __name__ == '__main__':
+    print(findRestaurant(["Shogun", "Tapioca Express", "Burger King", "KFC"],["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]))
+    print(findRestaurant(["Shogun", "Tapioca Express", "Burger King", "KFC"],["KFC", "Shogun", "Burger King"]))
