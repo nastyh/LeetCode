d16c212b58c323a5eb6f0013a2f12544aac32409
Michael Kareev <mkareev@umich.edu>
2020-07-02 09:44:16 -0400

    alternative solution

--- Basic Data Structures/189_Rotate_Array.py
+++ Basic Data Structures/189_Rotate_Array.py
@@ -1,9 +1,25 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        for _ in range(k % len(nums)):
-            p = nums.pop()
-            nums.insert(0, p)
-        return nums
+def rotate(nums, k):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    for _ in range(k % len(nums)):
+        p = nums.pop()
+        nums.insert(0, p)
+    return nums
+
+def rotate_alt(nums, k):
+    if len(nums) == 0: return []
+    if len(nums) == k: return nums
+    if k > len(nums):
+        rotations = k % len(nums)
+    else:
+        rotations = k
+    # return nums[-rotations:] + nums[:rotations]
+    return nums[-rotations:] + nums[:-rotations]
+
+
+if __name__ == '__main__':
+    print(rotate([1,2,3,4,5,6,7], 3))
+    print(rotate([-1,-100,3,99], 2))
+    print(rotate_alt([1,2,3,4,5,6,7], 3))
+    print(rotate_alt([-1,-100,3,99], 2))
