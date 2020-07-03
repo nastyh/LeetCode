cb4d165b4b26fdffed4a01e23db9913a8887bb05
Michael Kareev <mkareev@umich.edu>
2020-07-03 17:30:33 -0400

    Alternative solutions

---
+++ Basic Data Structures/268_Missing_Number.py
@@ -0,0 +1,23 @@
+def missingNumber(nums): # brute force
+    d = dict.fromkeys(range(min(nums), len(nums) + 1))
+    for n in nums:
+        if n in d:
+            d[n] = 1
+    return [k for k,v in d.items() if v is None][0]
+
+def missingNumber_alt(nums):
+    missing = len(nums)
+    for i, num in enumerate(nums):
+        missing ^= i ^ num
+    return missing
+
+def missingNumber_Gauss(nums):
+    target_sum = len(nums) * (len(nums) + 1) / 2
+    real_sum = sum(nums)
+    return int(target_sum - real_sum)
+
+if __name__ == '__main__':
+    print(missingNumber([3,0,1]))
+    print(missingNumber([0]))
+    print(missingNumber_Gauss([3,0,1]))
+    print(missingNumber_Gauss([0]))
