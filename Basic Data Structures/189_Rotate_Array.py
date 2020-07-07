b7012383770b651eb60726e33ae80890e2563fb6
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2020-07-07 10:48:20 -0400

    Alternative solutions

--- Basic Data Structures/189_Rotate_Array.py
+++ Basic Data Structures/189_Rotate_Array.py
@@ -1,9 +1,31 @@
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        for _ in range(k % len(nums)):
-            p = nums.pop()
-            nums.insert(0, p)
-        return nums
+def rotate(nums, k):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    for _ in range(k % len(nums)):
+        p = nums.pop()
+        nums.insert(0, p)
+    return nums
+
+def rotate_alt(nums, k):
+	if len(nums) == 0: return 
+	if len(nums) == 1: return nums
+	if len(set(nums)) == 1: return nums
+
+	for _ in range(k % len(nums)):
+		l = nums.pop()
+		nums = [l] + nums[:]
+	return nums
+
+def rotate_reversal(nums, k):
+	r = nums[-(k % len(nums)):]
+	rr = list(reversed(r))
+	rest = nums[:(k % len(nums)) + 1]
+	# return r + rest
+	return nums[-(k % len(nums)):] + nums[:(k % len(nums)) + 1]
+
+
+if __name__ == '__main__':
+	print(rotate_alt([1,2,3,4,5,6,7], 3))
+	print(rotate_reversal([1,2,3,4,5,6,7], 3))
+
---
+++ Basic Data Structures/322_Coin_Change.py
@@ -0,0 +1,28 @@
+def coinchange(nums, target):
+	if len(nums) == 0: return -1
+	i, res = len(nums) - 1, 0
+	nums.sort()
+	while i != 0 or target != 0:
+		if nums[i] > target:
+			i -= 1
+		elif nums[i] == target:
+			return res
+		else:
+			continue
+		res += target // nums[i]
+		target = target % nums[i]
+		i -= 1
+	return res 
+
+def coinchange_naive(nums, target):
+	if len(nums) == 0: return -1
+	res, curr = 0, 0
+	for coin in nums:
+		while target >= coin:
+			target -= coin
+			res += 1
+	return res
+
+if __name__ == '__main__':
+	# print(coinchange([1,2,5], 11))
+	print(coinchange_naive([1,2,5], 11))
