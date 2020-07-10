8d282b7ea8de7d12c2bdeb5bc03aa0ae5b614e3f
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2020-07-10 19:48:37 -0400

    Updates

---
+++ Basic Data Structures/064_Minimum_Path_Sum.py
@@ -0,0 +1,16 @@
+def minPathSum(grid):
+    dp = grid
+    col, row = len(grid[0]), len(grid)
+    for i in range(1, col):
+        dp[0][i] = dp[0][i - 1] + grid[0][i]
+    for j in range(1, row):
+        dp[j][0] = dp[j - 1][0] + grid[j][0]
+    for y in range(1, len(grid)):
+        for x in range(1, len(grid[0])):
+            dp[y][x] = min(dp[y][x - 1], dp[y - 1][x]) + grid[y][x]
+    return dp[-1][-1]
+
+
+if __name__ == '__main__':
+    print(minPathSum([[1, 3, 1], [1, 5, 1], [4, 2, 1]]))
+    print(minPathSum([[1, 2, 5], [3, 2, 1]]))
