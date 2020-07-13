41c3b97deb9873351ad8064eae98f27bf52febc8
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2020-07-13 15:55:59 -0400

    Update 220_Contains_Duplicate_III.py

--- Basic Data Structures/220_Contains_Duplicate_III.py
+++ Basic Data Structures/220_Contains_Duplicate_III.py
@@ -1,13 +1,12 @@
 def containsNearbyAlmostDuplicate(nums, k, t):
     size = len(nums)
-    if t == 0 and len(nums) == len( set(nums) ):
+    if t == 0 and len(nums) == len(set(nums)):
         return False        
     for i, cur_val in enumerate(nums):  
-        for j in range( i+1, i+k+1):
+        for j in range(i + 1, i + k + 1):
             if j >= size:
-                # avoid index out of boundary
                 break
-            if abs(cur_val - nums[j]  ) <= t:
+            if abs(cur_val - nums[j]) <= t:
                 return True
     return False
 
