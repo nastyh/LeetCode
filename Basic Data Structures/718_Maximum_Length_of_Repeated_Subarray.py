0c9dbd90a5aea8a4d30c57e7bff3b5325cc152bb
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2020-07-16 19:52:21 -0400

    Create 718_Maximum_Length_of_Repeated_Subarray.py

---
+++ Basic Data Structures/718_Maximum_Length_of_Repeated_Subarray.py
@@ -0,0 +1,14 @@
+def findLength(A, B):
+    dp = [[0] * (len(A) + 1) for _ in range(len(B) + 1)]
+    for a_ix in range(len(A)):
+        for b_ix in range(len(B)):
+            if A[a_ix] == B[b_ix]:
+                dp[a_ix][b_ix] = dp[a_ix - 1][b_ix - 1] + 1
+    return max([max(i) for i in dp])
+    # return dp
+
+
+if __name__ == '__main__':
+    print(findLength([1, 2, 3, 2, 1], [3, 2, 1, 4, 7]))
+    print(findLength([0, 0, 0, 0, 1], [1, 0, 0, 0, 0]))
+        
