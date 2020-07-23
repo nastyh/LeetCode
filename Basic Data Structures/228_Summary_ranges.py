46bfad9374c12da53633bcdcedc8dddcab06c0d2
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2020-07-23 15:08:47 -0400

    Additions

--- Basic Data Structures/228_Summary_ranges.py
+++ Basic Data Structures/228_Summary_ranges.py
@@ -1,14 +1,11 @@
 def summaryRanges(nums):
     if not nums:
         return []
-
     stack = []
-
     i = 0
     res = []
     while i < len(nums):
-
-        if stack and nums[i] -stack[-1] !=1:
+        if stack and nums[i] - stack[-1] != 1:
             if len(stack)==1:
                 res.append(str(stack[-1]))
             else:
@@ -19,14 +16,34 @@ def summaryRanges(nums):
 
         stack.append(nums[i])
         i+=1
-
     if len(stack)==1:
         res.append(str(stack[-1]))
     else:
         res.append(str(stack[0]) + '->' + str(stack[-1]))
-
     return res
 
+def summaryRanges_alt(nums):
+    if len(nums) == 0:
+        return []
+    if len(nums) == 1:
+        return [str(nums[0])]
+    res = []
+    start = nums[0]
+    prev = nums[0]
+    for n in nums[1:]:
+        if n != prev + 1:
+            if prev == start:
+                res.append(str(start))
+            else:
+                res.append(str(start) + '->' + str(prev))
+            start = n
+        prev = n
+    if prev == start:
+        res.append(str(start))
+    else:
+        res.append(str(start) + '->' + str(prev))
+    return res
 
 if __name__ == '__main__':
     print(summaryRanges([0,2,3,4,6,8,9]))
+    print(summaryRanges_alt([0,2,3,4,6,8,9]))
