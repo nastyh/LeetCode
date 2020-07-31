6e2eeb40143414cf423f8af1c2eb63e672a16b19
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2020-07-31 13:39:24 -0400

    Additions

--- Basic Data Structures/496_Next_Greater_Element_I.py
+++ Basic Data Structures/496_Next_Greater_Element_I.py
@@ -21,20 +21,20 @@ def nextGreaterElement_alt(nums1, nums2):
 
 
     # Just an experiment; isn't required for this question
-    def _helper(nums2): # return the next larger element to the right from a given element
-        res = []
-        st = []
-        for i in range(len(nums2)):
-            if len(st) == 0:
-                st.append(nums2[i])
-            else:
-                while st and st[-1] < nums2[i]:
-                    res.append(nums2[i])
-                    st.pop()
-                st.append(nums2[i])
-        if len(res) != len(nums2):
-            res.extend([-1 for i in range(len(nums2) - len(res))])
-        return res
+def _helper(nums2): # return the next larger element to the right from a given element
+    res = []
+    st = []
+    for i in range(len(nums2)):
+        if len(st) == 0:
+            st.append(nums2[i])
+        else:
+            while st and st[-1] < nums2[i]:
+                res.append(nums2[i])
+                st.pop()
+            st.append(nums2[i])
+    if len(res) != len(nums2):
+        res.extend([-1 for i in range(len(nums2) - len(res))])
+    return res
 
 if __name__ == '__main__':
     # print(nextGreaterElement([4, 1 ,2], [1, 3, 4, 2]))
@@ -42,3 +42,4 @@ if __name__ == '__main__':
     print(nextGreaterElement_alt([2, 4], [1, 2, 3, 4]))   
     print(nextGreaterElement_alt([4, 1, 2], [1, 3, 4, 2]))
     print(nextGreaterElement_alt([1, 3, 5, 2, 4], [6, 5, 4, 3, 2, 1, 7]))
+    print(_helper([6, 10, 8, 5, 2, 11, 12, 4, 20]))
---
+++ Basic Data Structures/Increasing_decreasing_sequencies.py
@@ -0,0 +1,54 @@
+def NGE_brute_force(nums):
+    if len(nums) == 0: return []
+    if len(nums) == 1: return [-1]
+    res = [-1] * len(nums)
+    for i in range(len(nums) - 1):
+        for n in nums[i + 1:]:
+            if n > nums[i]:
+                res[i] = n
+                break
+    return res
+
+
+def NGE(nums):
+    res = []
+    st = []
+    for i in range(len(nums)):
+        if len(st) == 0:
+            st.append(nums[i])
+        else:
+            while st and st[-1] < nums[i]:
+                res.append(nums[i])
+                st.pop()
+            st.append(nums[i])
+    if len(res) != len(nums):
+        res.extend([-1 for i in range(len(nums) - len(res))])
+    return res
+
+
+
+def non_decreasing(nums):
+    if len(nums) == 0: return []
+    if len(nums) == 1: return nums
+    res, st = [], []
+    res.append(nums[0])
+    st.append(nums[0])
+    for i in range(1, len(nums)):
+        # if len(st) == 0:
+        #     st.append(nums[i])
+        while st and nums[i] > st[-1]:
+            res.append(nums[i])
+            st.pop()
+        st.append(nums[i])
+    return res
+
+
+if __name__ == '__main__':
+    print(NGE_brute_force([6, 10, 8, 5, 2, 11, 12, 4, 20]))
+    print(NGE([6, 10, 8, 5, 2, 11, 12, 4, 20]))
+    # print(non_decreasing([6, 10, 8, 5, 2]))
+    # print(non_decreasing([6, 10, 8, 5, 2, 11, 12, 4, 20]))
+    # print(non_decreasing([10, 9, 8, 7]))
+    # print(non_decreasing([6, 6, 6, 6]))
+    # print(non_decreasing([2]))
+    # print(non_decreasing([]))
