80380e6a0cc97de17cf237b2eccc143c4a33db5e
Michael Kareev <mkareev@umich.edu>
2020-08-02 10:59:29 -0400

    Alternative

--- Basic Data Structures/1047_Remove_all_adjacent_duplicates_in_string.py
+++ Basic Data Structures/1047_Remove_all_adjacent_duplicates_in_string.py
@@ -1,12 +1,29 @@
-class Solution:
-    def removeDuplicates(self, S: str) -> str:
-        st = []
-        for el in S:
-            if not st:
-                st.append(el)
-                continue
-            if st[-1] == el:
-                st.pop()
-            else:
-                st.append(el)
-        return ''.join(b for b in st)
+def removeDuplicates(S):
+    st = []
+    for el in S:
+        if not st:
+            st.append(el)
+            continue
+        if st[-1] == el:
+            st.pop()
+        else:
+            st.append(el)
+    return ''.join(b for b in st)
+
+
+def removeDuplicates_alt(S):
+    st = []
+    for ch in S:
+        # if not st:
+        #     st.append(ch)
+        #     continue
+        if st and st[-1] == ch:
+            st.pop()
+        else:
+            st.append(ch)
+    return ''.join(st)
+
+
+if __name__ == '__main__':
+    print(removeDuplicates('abbaca'))
+    print(removeDuplicates_alt('abbaca'))
