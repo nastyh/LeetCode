296d9f1ca20baf4ce02a2dc8e65fd158544b59f1
Michael Kareev <mkareev@umich.edu>
2020-08-02 13:01:21 -0400

    Alternative

--- Basic Data Structures/070_Climbing_Stairs.py
+++ Basic Data Structures/070_Climbing_Stairs.py
@@ -1,5 +1,6 @@
 def stairs(n):
-	if n == 1: return 1
+	if n == 1:
+		return 1
 	dp = [None] * (n + 1)
 	dp[0] = 0
 	dp[1] = 1
@@ -9,5 +10,22 @@ def stairs(n):
 	return dp[n]
 
 
+def stairs_alt(n):
+	if n <= 2:
+		return n 
+	dp = [0] * (n + 1)
+	dp[0] = 0
+	dp[1] = 1
+	dp[2] = 2
+	for i in range(3, n + 1):
+		dp[i] = dp[i - 2] + dp[i - 1]
+	return dp[-1]
+	
+	
 if __name__ == '__main__':
 	print(stairs(3))
+	print(stairs(4))
+	print(stairs(5))
+	print(stairs_alt(3))
+	print(stairs_alt(4))
+	print(stairs_alt(5))
