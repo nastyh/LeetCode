3482523fd268e5b2498229b52da92f83fa82ed53
Michael Kareev <mkareev@umich.edu>
2020-08-04 13:39:35 -0400

    Additions

--- Basic Data Structures/063_Unique_Paths_II.py
+++ Basic Data Structures/063_Unique_Paths_II.py
@@ -27,13 +27,37 @@ def uniquePathsWithObstacles_another(obstacleGrid): # with filling out the corne
 		obstacleGrid[i][0] = int(obstacleGrid[i][0] == 0 and obstacleGrid[i-1][0] == 1)
 	for j in range(1, row):
 		obstacleGrid[0][j] = int(obstacleGrid[0][j] == 0 and obstacleGrid[0][j-1] == 1)
-	for i in range(1,row):
-		for j in range(1,col):
+	for i in range(1, row):
+		for j in range(1, col):
 			if obstacleGrid[i][j] == 0:
 			    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1]
 			else:
 			    obstacleGrid[i][j] = 0
 	return obstacleGrid[-1][-1]
+
+
+def uniquePathsWithObstacles_easy(obstacleGrid): # doesn't work with some edge cases
+    if obstacleGrid[0][0] == 1: return 0
+    dp = [[None for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
+    dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+    for i in range(1, len(dp[0])):
+        if (obstacleGrid[0][i] == 0 and obstacleGrid[0][i - 1] == 0):
+            dp[0][i] = 1
+        else:
+             dp[0][i] = 0
+    for j in range(1, len(dp)):
+        if (obstacleGrid[j][0] == 0 and obstacleGrid[j - 1][0] == 0):
+            dp[j][0] = 1
+        else:
+            dp[j][0] = 0
+        # if obstacleGrid[j - 1][0] == 0:
+        #     dp[j][0] = 1
+        # else:
+        #     dp[j][0] = 0
+    for p in range(1, len(obstacleGrid)):
+        for q in range(1, len(obstacleGrid[0])):
+            dp[p][q] = dp[p - 1][q] + dp[p][q - 1] if obstacleGrid[p][q] == 0 else 0
+    return dp[-1][-1]
 
 def test(grid):
     col, row = len(grid[0]), len(grid)
@@ -51,6 +75,11 @@ def test(grid):
     return dp
 
 if __name__ == '__main__':
-    print(uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]]))
+    # print(uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]]))
     print(uniquePathsWithObstacles_another([[0, 0, 0], [0, 1, 0], [0, 0, 0]]))
-    print(test([[0, 0, 0], [0, 1, 0], [0, 0, 0]]))
+    print(uniquePathsWithObstacles_easy([[0, 0, 0], [0, 1, 0], [0, 0, 0]]))
+    print(uniquePathsWithObstacles_easy([[1, 0]]))
+    print(uniquePathsWithObstacles_easy([[0, 1]]))
+    print(uniquePathsWithObstacles_easy([[1]]))
+    print(uniquePathsWithObstacles_easy([[0, 0]]))
+    # print(test([[0, 0, 0], [0, 1, 0], [0, 0, 0]]))
---
+++ Basic Data Structures/931_Minimum_Falling_Path_Sum.py
@@ -0,0 +1,33 @@
+def minFallingPathSum(A):
+    dp = A
+    for i in range(1, len(A)):
+        # dp[i][0] = min(A[i][0], A[i][1]) + min(dp[i - 1][0], dp[i - 1][1])
+        dp[i][0] = A[i][0] + min(dp[i - 1][0], dp[i - 1][1])
+        # dp[i][-1] = min(A[i][-1], A[i][-2]) + min(dp[i - 1][-1], dp[i - 1][-2])
+        dp[i][-1] = A[i][-1] + min(dp[i - 1][-1], dp[i - 1][-2])
+    for n in range(1, len(A[0]) - 1):
+        for m in range(1, len(A[0])):
+            dp[m][n] = min(A[m][n - 1], A[m][n], A[m][n + 1]) + min(dp[m - 1][n - 1], dp[m - 1][n], dp[m - 1][n + 1])
+    return min(dp[-1])
+    # return dp
+
+
+def minFallingPathSum_alt(arr):
+    for i in range(1, len(arr)):
+        for j in range(len(arr[0])):
+            if j==0:  # first col
+                arr[i][j] += min([arr[i - 1][j + 1], arr[i - 1][j]])
+            elif j==len(arr[0]) - 1:  # last col
+                arr[i][j] += min([arr[i - 1][j - 1], arr[i - 1][j]])
+            else:  # all other cols
+                arr[i][j] += min([arr[i - 1][j - 1], arr[i - 1][j], arr[i - 1][j + 1]])
+    return min(arr[-1])
+
+
+if __name__ == '__main__':
+    print(minFallingPathSum([[1, 2, 3],[4, 5, 6],[7, 8, 9]]))
+    print(minFallingPathSum([[-19, 57],[-40, -5]])) # doesn't work with this one
+    # print(minFallingPathSum_alt([[1, 2, 3],[4, 5, 6],[7, 8, 9]]))
+    # print(minFallingPathSum_alt([[-19, 57],[-40, -5]]))
+
+
