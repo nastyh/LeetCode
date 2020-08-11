e6c8ce27c603ce76c1040ab0c0b3f4e7b86b8b63
Michael Kareev <mkareev@umich.edu>
2020-08-10 20:52:49 -0400

    Update Basic Data Structures/Lint_code_word_sorting.py

--- Basic Data Structures/Lint_code_word_sorting.py
+++ Basic Data Structures/Lint_code_word_sorting.py
@@ -13,10 +13,6 @@ def wordSorting(alphabet, words):
         new_words.append(curr)
     return new_words
 
-def test(nums):
-    return sorted(nums)
-
 
 if __name__ == '__main__':
     print(wordSorting(['c','b','a','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], ['cab', 'cba', 'abc']))
-    # print(test([[1, 3 , 2], [1, 2, 3], [3, 2, 1]])) 
