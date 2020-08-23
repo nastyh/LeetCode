edeb62e160cfc5a50b971535ea27a38d5499310e
Michael Kareev <mkareev@umich.edu>
2020-08-23 09:23:55 -0400

    Cleaning

--- Basic Data Structures/152_Maximum_Product_Subarray.py
+++ Basic Data Structures/152_Maximum_Product_Subarray.py
@@ -1,3 +1,4 @@
+import math
 def maxProduct(nums):
     maxval=float('-inf')
     imax,imin=1,1
@@ -9,18 +10,20 @@ def maxProduct(nums):
         maxval=max(maxval,imax)
     return maxval
 
- def maxProduct_alt(nums):
-        if not nums: return 0
-        cur_max = final_max = cur_min = nums[0]
-        for i in range(1 , len(nums)):
-            temp = cur_max
-            cur_max = max(max(cur_max * nums[i] , cur_min * nums[i]) , nums[i])
-            cur_min = min(min(temp * nums[i] , cur_min * nums[i]) , nums[i])
-            if cur_max > final_max:
-                final_max = cur_max
-        return final_max
 
-def maxProduct_brute_force(self, nums: List[int]) -> int:
+def maxProduct_alt(nums):
+    if not nums: return 0
+    cur_max = final_max = cur_min = nums[0]
+    for i in range(1 , len(nums)):
+        temp = cur_max
+        cur_max = max(max(cur_max * nums[i] , cur_min * nums[i]) , nums[i])
+        cur_min = min(min(temp * nums[i] , cur_min * nums[i]) , nums[i])
+        if cur_max > final_max:
+            final_max = cur_max
+    return final_max
+    
+
+def maxProduct_brute_force(nums):
         res, curr = -math.inf, 1
         for i in range(len(nums)):
             curr = nums[i]
@@ -29,3 +32,18 @@ def maxProduct_brute_force(self, nums: List[int]) -> int:
                 curr = curr * nums[j]
                 res = max(res, curr)
         return res
+
+
+if __name__ == '__main__':
+    print(maxProduct([2, 3, -2, 4]))
+    print(maxProduct_alt([2, 3, -2, 4]))
+    print(maxProduct_brute_force([2, 3, -2, 4]))
+    print(maxProduct([-2, 0, -1]))
+    print(maxProduct_alt([-2, 0, -1]))
+    print(maxProduct_brute_force([-2, 0, -1]))
+    print(maxProduct([-4, -3, -2]))
+    print(maxProduct_alt([-4, -3, -2]))
+    print(maxProduct_brute_force([-4, -3, -2]))
+    print(maxProduct([-2, 3, -4]))
+    print(maxProduct_alt([-2, 3, -4]))
+    print(maxProduct_brute_force([-2, 3, -4]))
