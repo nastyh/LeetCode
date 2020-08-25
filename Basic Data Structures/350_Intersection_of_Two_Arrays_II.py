a8d8b2a3ed214382851d5b3d80f96739ae09a681
Michael Kareev <mkareev@umich.edu>
2020-08-24 21:01:17 -0400

    Additions

--- Basic Data Structures/350_Intersection_of_Two_Arrays_II.py
+++ Basic Data Structures/350_Intersection_of_Two_Arrays_II.py
@@ -1,15 +1,39 @@
-class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if len(nums1) <= len(nums2):
-            short = nums1
-            long = nums2
-        else:
-            short = nums2
-            long = nums1
+from collections import Counter
+def intersect(nums1, nums2):  # straigthforward
+    if len(nums1) <= len(nums2):
+        short = nums1
+        long = nums2
+    else:
+        short = nums2
+        long = nums1
 
-        res = []
-        for i in short:
-            if i in long:
-                res.append(i)
-                long.remove(i)
-        return res
+    res = []
+    for i in short:
+        if i in long:
+            res.append(i)
+            long.remove(i)
+    return res
+
+
+def intersect_dict(nums1, nums2):  # a bit faster but uses extra space
+    res = []
+    if len(nums1) <= len(nums2):
+        shorter = nums1
+        longer = nums2
+    else:
+        shorter = nums2
+        longer = nums1
+    d_short = Counter(shorter)
+    d_long = Counter(longer)
+    for k in shorter:
+        if d_long.get(k, 0) != 0:
+            res.append(k)
+            d_long[k] -= 1
+    return res
+
+
+if __name__ == '__main__':
+    print(intersect([1, 2, 2, 1], [2, 2]))
+    print(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
+    print(intersect_dict([1, 2, 2, 1], [2, 2]))
+    print(intersect_dict([4, 9, 5], [9, 4, 9, 8, 4]))
--- Basic Data Structures/628_Maximum_Product_of_Three_Numbers.py
+++ Basic Data Structures/628_Maximum_Product_of_Three_Numbers.py
@@ -1,4 +1,3 @@
-import heapq
 def maximumProduct(nums):  # sorting
     nums.sort()
     return max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3])
