3651300ebe32ba58975b8075358b2f384826465e
Michael Kareev <mkareev@umich.edu>
2020-08-24 21:01:34 -0400

    Cleaning

--- Basic Data Structures/017_Letter_Combinations_of_a_Phone_Number.py
+++ Basic Data Structures/017_Letter_Combinations_of_a_Phone_Number.py
@@ -1,4 +1,5 @@
 from itertools import product
+from collections import Counter
 def letterCombinations(digits):
     res, l = '', []
     d = {'2': 'abc',
@@ -35,19 +36,31 @@ def letterCombinations_alt(digits):
     return result
 
 
-
-
-
-
-
-    # for digit in digits:
-    #     res += ''.join(d[int(digit)])
-    # # return d[int(digits)]
-    # # res += ''.join([v for k, v in d.items()])
-    # l = [i for i in product(res, repeat = len(digits))]
-    # return l
+def letterCombinations_another(digits):
+    d = {'2': 'abc',
+         '3': 'def',
+         '4': 'ghi',
+         '5': 'jkl',
+         '6': 'mno',
+         '7': 'pqrs',
+         '8': 'tuv',
+         '9': 'wxyz'
+         }
+    if len(digits) == 0: return []
+    if len(digits) == 1: return [digits[0]]
+    def _helper(digits, d, ix, curr, res):
+        if ix == len(digits):
+            res.append(curr)
+            return
+        for ch in d[digits[ix]]:
+            curr += ch
+            _helper(digits, d, ix + 1, curr, res)
+            curr = curr[:-1]
+        return res
+    return _helper(digits, d, 0, '', [])
 
 
 if __name__ == '__main__':
     print(letterCombinations('23'))
     print(letterCombinations_alt('23'))
+    print(letterCombinations_another('23'))
