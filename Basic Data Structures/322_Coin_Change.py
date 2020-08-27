305f342c428a55db21dbbe2b2266d159581631de
Michael Kareev <mkareev@umich.edu>
2020-08-26 20:05:39 -0400

    Updates

--- Basic Data Structures/322_Coin_Change.py
+++ Basic Data Structures/322_Coin_Change.py
@@ -1,30 +1,12 @@
-def coinchange(nums, target):
-	if len(nums) == 0: return -1
-	i, res = len(nums) - 1, 0
-	nums.sort()
-	while i != 0 or target != 0:
-		if nums[i] > target:
-			i -= 1
-		elif nums[i] == target:
-			return res
-		else:
-			continue
-		res += target // nums[i]
-		target = target % nums[i]
-		i -= 1
-	return res
-
-
-def coinchange_naive(nums, target):
-	if len(nums) == 0: return -1
-	res = 0
-	for coin in nums:
-		while target >= coin:
-			target -= coin
-			res += 1
-	return res
+def coinchange(coins, target):
+	dp = [float('inf')] * (target + 1)
+	dp[0] = 0
+	for coin in coins:
+		for x in range(coin, target + 1):
+			dp[x] = min(dp[x], dp[x - coin] + 1)
+	return dp[target] if dp[target] != float('inf') else -1 
 
 
 if __name__ == '__main__':
-	# print(coinchange([1,2,5], 11))
-	print(coinchange_naive([1,2,5], 11))
+	print(coinchange([1,2,5], 11))
+	print(coinchange([2], 3))
---
+++ Basic Data Structures/983_Minimum_Cost_For_Tickets.py
@@ -0,0 +1,34 @@
+from collections import Counter
+def mincostTickets(days, cost):
+    last = days[-1]
+    dp = [0 for i in range(last + 1)]
+    traveldays = Counter(days)
+    
+    for i in range(last + 1):
+        if i not in traveldays:
+            dp[i] = dp[i-1]
+        else:
+            one = dp[max(0, i - 1)] + cost[0]
+            seven = dp[max(0, i - 7)] + cost[1]
+            thirty = dp[max(0, i - 30)] + cost[2]
+            dp[i] = min(one, seven, thirty)
+    return dp[-1]
+
+def mincostTickets_alt(days, costs):
+    dp_cost = [None] * 366
+    dp_cost[0] = 0
+    for day in days:
+        dp_cost[day] = 0
+    for day_i in range(1, 366):
+        if dp_cost[day_i] == None:
+            dp_cost[day_i] = dp_cost[day_i - 1]      
+        else:  
+            dp_cost[day_i] = min(dp_cost[day_i - 1]  + costs[0],\
+                                 dp_cost[max(day_i - 7, 0)]  + costs[1],\
+                                 dp_cost[max(day_i - 30, 0)] + costs[2])
+    return dp_cost[-1]
+
+if __name__ == '__main__':
+    print(mincostTickets([1, 4, 6, 7, 8, 20], [2, 7, 15]))
+    print(mincostTickets_alt([1, 4, 6, 7, 8, 20], [2, 7, 15]))
+        
