c74f718f41023a051827f4c036dd0300f74334c3
Michael Kareev <mkareev@umich.edu>
2020-09-07 18:51:32 -0400

    Cleaning

--- Basic Data Structures/560_Subarray_Sum_equals_K.py
+++ Basic Data Structures/560_Subarray_Sum_equals_K.py
@@ -25,7 +25,7 @@ def subarraySum_dp(nums, k):
     return res + 1
 
 def subarraySum_dict(nums, k):
-    d, curr_sum, res = {0:1}, 0, 0
+    d, curr_sum, res = {0 : 1}, 0, 0
     for num in nums:
         curr_sum += num
         if curr_sum - k in d:
@@ -36,15 +36,43 @@ def subarraySum_dict(nums, k):
             d[curr_sum] = 1
     return res
 
-def subarraySum_another(nums, k):
-    count = 0
-    for start in range(len(nums)):
-        sum = 0
-        for end in range(start, len(nums)):
-            sum += nums[end]
-            if sum == k:
-                count += 1
-    return count
+
+def subarraySum_n2(nums, k):  # O(n*2). Two loops. B/c r starts at the same position as l, we account for one-element situations 
+    res = 0
+    for l in range(len(nums)):
+        curr_sum = 0
+        for r in range(l, len(nums)):
+            curr_sum += nums[r]
+            if curr_sum == k:
+                res += 1
+    return res
+
+def subarraySum_another(nums, k):  # doesn't work for [-1, -1, 1], 0
+    if len(nums) == 1:
+        if nums[0] == k: return 1
+        else: return 0
+    curr_sum, res = 0, 0
+    l, r = 0, 1
+    if any(i == k for i in nums): res += 1
+    while r < len(nums):
+        curr_sum = sum(nums[l: r + 1])
+        if curr_sum == k:
+            res += 1
+            curr_sum = 0
+            l += 1
+            r += 1
+        else:
+            r += 1
+    if r == len(nums) - 1:
+        while l < len(nums):
+            curr_sum = sum(nums[l: r + 1])
+            if curr_sum == k:
+                res += 1
+                l += 1
+                curr_sum = 0
+            else:
+                l += 1
+    return res
 
 
 if __name__ == '__main__':
@@ -52,6 +80,9 @@ if __name__ == '__main__':
     print(subarraySum_dict([3, 4, 7, 2, -3, 1, 4, 2], 7))
     print(subarraySum_dp([3, 4, 7, 2, -3, 1, 4, 2], 7))
     print(subarraySum_dp([1, 1, 1], 2))
-    print(subarraySum_dp([1, -1, 5 ,-2, 3], 3))
+    print(subarraySum_dp([1, -1, 5, -2, 3], 3))
     print(subarraySum_another([1, 1, 1], 2))
     print(subarraySum_another([1, 1, 1, 2], 2)) # [1,2,3], 3
+    print(subarraySum_another([1, 2, 3], 3))
+    print(subarraySum_n2([1, 2, 3], 3))
+    print(subarraySum_n2([-1, -1, 1], 0))
