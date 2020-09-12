706eaa20090e89bad3f0e6308b485bb1d766b067
Michael Kareev <mkareev@umich.edu>
2020-09-12 12:10:27 -0400

    Update Trees/1022_Sum_of_Root_to_leaf_Binary_Numbers.py

--- Trees/1022_Sum_of_Root_to_leaf_Binary_Numbers.py
+++ Trees/1022_Sum_of_Root_to_leaf_Binary_Numbers.py
@@ -1,35 +1,37 @@
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-    def sumRootToLeaf(self, root):
-        res, l, r = [], [], []
 
-        paths = []
-        if not (root.left or root.right):
-            return [[root.val]]
-        if root.left:
-            paths.extend([[root.val] + child for child in self.sumRootToLeaf(root.left)])
-        if root.right:
-            paths.extend([[root.val] + child for child in self.sumRootToLeaf(root.right)])
-        return paths
+    def sumRootToLeaf_2helpers(self, root):
+        def _helper_traverse(root, curr, res):
+            if not root:
+                return res
+            if root and not root.left and not root.right:
+                res.append(curr)
+            if root.left: _helper_traverse(root.left, curr + [root.left.val], res)
+            if root.right: _helper_traverse(root.right, curr + [root.right.val], res)
+            return res
+        def _helper_binary(lists):
+            res = 0
+            for nums in lists:
+                res += sum([2 ** k if v == 1 else 0 for k,v in enumerate(list(reversed(nums)))])
+            return res
+        numbers = _helper_traverse(root, [root.val], [])
+        return _helper_binary(numbers)
 
-        # return res + [i for i in (self.sumRootToLeaf(root.left) + self.sumRootToLeaf(root.right))]
-
-
-        # return res
 
 if __name__ == '__main__':
-    l = TreeNode(1)
-    l.left = TreeNode(0)
-    l.right = TreeNode(1)
-    l.left.left = TreeNode(0)
-    l.right.left = TreeNode(1)
-    l.right.left = TreeNode(0)
-    l.right.right = TreeNode(1)
+    m = TreeNode(1)
+    m.left = TreeNode(0)
+    m.right = TreeNode(1)
+    m.left.left = TreeNode(0)
+    m.left.right = TreeNode(1)
+    m.right.left = TreeNode(0)
+    m.right.right = TreeNode(1)
 
-print(l.sumRootToLeaf(l))
+    print(m.sumRootToLeaf_2helpers(m))
+
 
