9f8a9bbd7a6c53d44b453aafe2eb013bd49f216a
Michael Kareev <mkareev@umich.edu>
2020-09-12 12:26:21 -0400

    Update Basic Data Structures/227_Basic_Calculator_II.py

---
+++ Basic Data Structures/227_Basic_Calculator_II.py
@@ -0,0 +1,31 @@
+def calculate(s):
+    stack = []
+    tem = 0
+    sign = '+'
+    for i in range(len(s)):
+        if s[i].isdigit():
+            tem = tem * 10 + int(s[i])
+        if s[i] in '+-*/' or i == len(s) - 1:
+            if sign == '+':
+                stack.append(tem)
+            if sign == '-':
+                stack.append(-tem)
+            if sign == '*':
+                tem2 = stack.pop() * tem
+                stack.append(tem2)
+            if sign == '/':
+                a = stack.pop()
+                if a >= 0:
+                    tem2 = a // tem
+                else:
+                    tem2 =- ((-a) // tem)
+                stack.append(tem2)
+            tem = 0
+            sign = s[i]
+    return sum(stack)
+
+
+if __name__ == '__main__':
+    print(calculate('3+2*2'))
+    print(calculate('3/2'))
+    print(calculate('3+5 / 2'))
