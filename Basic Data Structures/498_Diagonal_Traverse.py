2f44e175f0f926779aefffe4351e9dd9dc458228
Michael Kareev <mkareev@umich.edu>
2020-09-19 21:23:54 -0400

    Update Basic Data Structures/498_Diagonal_Traverse.py

--- Basic Data Structures/498_Diagonal_Traverse.py
+++ Basic Data Structures/498_Diagonal_Traverse.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-def findDiagonalOrder(nums):
+def findDiagonalOrder(nums):  # if to sum up elements' indices on a given diagonal, the number will be the same
     d = defaultdict(list)
     for r in range(len(nums)):
         for c in range(len(nums[0])):
@@ -8,12 +8,12 @@ def findDiagonalOrder(nums):
             else:
                 d[r + c].append(nums[r][c])
     res = []
-    for k, v in d.items():
+    for k, v in d.items():  # because the go as a snake
         if k % 2 == 1:
             res.append(v)
         else:
             res.append(v[::-1])
-    return [item for sublist in res for item in sublist]
+    return [item for sublist in res for item in sublist]  # flattening the list of lists 
 
 
 if __name__ == '__main__':
