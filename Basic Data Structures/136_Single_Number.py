949dc5d18c2c8ce1addc5caddc3286ad7391ccc2
Michael Kareev <mkareev@umich.edu>
2020-09-24 23:28:20 -0400

    Update Basic Data Structures/136_Single_Number.py

--- Basic Data Structures/136_Single_Number.py
+++ Basic Data Structures/136_Single_Number.py
@@ -21,6 +21,10 @@ def singleNumber_naive(nums):
     return res[0]
 
 
+def singleNumber_math(nums):
+    return 2 * sum(set(nums)) - sum(nums)
+
+
 if __name__ == '__main__':
     print(singleNumber([2, 2, 1]))
     print(singleNumber([4, 1, 2, 1, 2]))
@@ -28,3 +32,5 @@ if __name__ == '__main__':
     print(singleNumber_dict([4, 1, 2, 1, 2]))
     print(singleNumber_naive([2, 2, 1]))
     print(singleNumber_naive([4, 1, 2, 1, 2]))
+    print(singleNumber_math([2, 2, 1]))
+    print(singleNumber_math([4, 1, 2, 1, 2]))
