78da705627f78b456263cf904a86cdc5e50f4f49
mkareev <mkareev@ebay.com>
2020-10-05 20:22:01 -0400

    Additions

--- Basic Data Structures/1099_Two_Sum_Less_Than_K.py
+++ Basic Data Structures/1099_Two_Sum_Less_Than_K.py
@@ -1,6 +1,5 @@
 import math
-
-def twoSumLessThanK(self, A: List[int], K: int) -> int:
+def twoSumLessThanK(A, K):
     A.sort()
     res = -math.inf
     l, r = 0, len(A) - 1
@@ -11,3 +10,8 @@ def twoSumLessThanK(self, A: List[int], K: int) -> int:
         else:
             r -= 1
     return res if res != -math.inf else -1
+
+
+if __name__ == '__main__':
+    print(twoSumLessThanK([34, 23, 1, 24, 75, 33, 54, 8], 60))
+    print(twoSumLessThanK([10, 20, 30], -1))
