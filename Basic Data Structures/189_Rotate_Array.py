5055e5a6a7b2095ebe4bdd197cbd98a005b2e282
mkareev <mkareev@ebay.com>
2020-10-14 19:14:18 -0400

    Additions

--- Basic Data Structures/189_Rotate_Array.py
+++ Basic Data Structures/189_Rotate_Array.py
@@ -17,6 +17,7 @@ def rotate_alt(nums, k):
 		nums = [l] + nums[:]
 	return nums
 
+
 def rotate_reversal(nums, k):
 	r = nums[-(k % len(nums)):]
 	rr = list(reversed(r))
@@ -25,23 +26,30 @@ def rotate_reversal(nums, k):
 	return nums[-(k % len(nums)):] + nums[:(k % len(nums)) + 1]
 
 
-if __name__ == '__main__':
-	print(rotate_alt([1,2,3,4,5,6,7], 3))
-	print(rotate_reversal([1,2,3,4,5,6,7], 3))
+def rotate_short(nums, k):
+    k %= len(nums)
+    nums[k:], nums[:k] = nums[:-k], nums[-k:]
 
-"""
-    if len(nums) == 0: return []
-    if len(nums) == k: return nums
-    if k > len(nums):
-        rotations = k % len(nums)
-    else:
-        rotations = k
-    # return nums[-rotations:] + nums[:rotations]
-    return nums[-rotations:] + nums[:-rotations]
-"""
+
+def rotate_another(nums, k):
+    """
+    Step 1: rotate the whole thing
+    Step 2: rotate the first k elements
+    Step 3: rotate the last len(nums) - k elements
+    """
+    def _helper(nums, l, r):
+        return nums[l: r + 1][::-1] 
+    nums = _helper(nums, 0, len(nums))
+    nums[:k] = _helper(nums, 0, k - 1)
+    nums[len(nums) - k - 1:] = _helper(nums, len(nums) - k - 1, len(nums))
+    return nums
+
 
 if __name__ == '__main__':
     print(rotate([1,2,3,4,5,6,7], 3))
     print(rotate([-1,-100,3,99], 2))
     print(rotate_alt([1,2,3,4,5,6,7], 3))
+	print(rotate_reversal([1,2,3,4,5,6,7], 3))
+    print(rotate_alt([1,2,3,4,5,6,7], 3))
+    print(rotate_another([1,2,3,4,5,6,7], 3))
 
