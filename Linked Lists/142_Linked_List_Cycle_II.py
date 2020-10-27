2e8b636ac1f7826bbb9fb473dfda99e4d7636998
mkareev <mkareev@ebay.com>
2020-10-27 11:19:33 -0400

    Comments

--- Linked Lists/142_Linked_List_Cycle_II.py
+++ Linked Lists/142_Linked_List_Cycle_II.py
@@ -1,4 +1,10 @@
 def detectCycle(head):
+    """
+    use slow and fast pointers: slow which moves one step at a time and fast, which moves two times at a time.
+    To find the place where loop started, we need to do it in two iterations: first we wait until fast pointe
+    gains slow pointer and then we move slow pointer to the start and run them with the same speed and wait
+    until they concide.
+    """
     if not head: return None
     if head and not head.next: return None
     slow = fast = head
