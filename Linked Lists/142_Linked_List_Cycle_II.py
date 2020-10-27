34ac5ea73de3ede811c64ed8c440fc44d781c059
mkareev <mkareev@ebay.com>
2020-10-27 11:17:54 -0400

    Cleaning

--- Linked Lists/142_Linked_List_Cycle_II.py
+++ Linked Lists/142_Linked_List_Cycle_II.py
@@ -1,29 +1,28 @@
-class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
-        p1 = p2 = head
-        while p2 and p2.next:
-            p1 = p1.next
-            p2 = p2.next.next
-            if p1 == p2:
-                break
-
-        if p2 is None or p2.next is None:
-            return None
-
-        p1 = head
-        while p2 is not p1:
-            p2 = p2.next
-            p1 = p1.next
-
-        return p2
-
-    class Solution:
-    def detectCycle(self, head: ListNode) -> ListNode:
-        cur = head
-        visited = set()
-        while cur:
-            if cur in visited:
-                return cur
-            visited.add(cur)
-            cur = cur.next
+def detectCycle(head):
+    if not head: return None
+    if head and not head.next: return None
+    slow = fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    if not fast or not fast.next:
         return None
+    
+    slow = head
+    while fast is not slow:
+        fast = fast.next
+        slow = slow.next
+    return slow
+
+
+def detectCycle_alt(head):
+    cur = head
+    visited = set()
+    while cur:
+        if cur in visited:
+            return cur
+        visited.add(cur)
+        cur = cur.next
+    return None
