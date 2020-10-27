94f7e3ac7776272e21788e3ac84be1d3cd4aff57
mkareev <mkareev@ebay.com>
2020-10-27 12:06:52 -0400

    Cleaning

--- Trees/173_Binary_Search_Tree_Iterator.py
+++ Trees/173_Binary_Search_Tree_Iterator.py
@@ -1,9 +1,42 @@
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.nodes = []
+        self.pointer = -1
+        self._helper(root)
+        
+    
+    def _helper(self, root):  # helper to traverse the tree in order and save it in a list
+        if not root: return
+        self._helper(root.left)
+        self.nodes.append(root.val)
+        self._helper(root.right)
+        
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        if self.pointer < len(self.nodes) - 1:
+            self.pointer += 1
+            return self.nodes[self.pointer]
+        
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        if self.pointer < len(self.nodes) - 1:
+            return True
+        else:
+            return False
+
+
 def inorder_generator(node):
     if node:
         yield from inorder_generator(node.left)
         yield node
         yield from inorder_generator(node.right)
-
 
 class BSTIterator:
 
