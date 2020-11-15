c6057ebbad99b97166f213074f234f38742d29ed
Michael Kareev <mkareev@umich.edu>
2020-11-15 15:48:23 -0500

    Additions

--- Basic Data Structures/209_Minimum_Size_Subarray_Sum.py
+++ Basic Data Structures/209_Minimum_Size_Subarray_Sum.py
@@ -1,10 +1,8 @@
 import math
-def minSubArrayLen(s, nums):
+def minSubArrayLen(s, nums):  # optimal
     if len(nums) == 0: return 0
     l, r = 0, 0
     curr, glob, curr_sum = 0, math.inf, 0
-
-    # curr_sum = nums[l]
     while r < len(nums):
         curr_sum += nums[r]
         while curr_sum >= s:
@@ -13,7 +11,6 @@ def minSubArrayLen(s, nums):
             curr_sum -= nums[l]
             l += 1
         r += 1
-
     return glob if glob != math.inf else 0
 
 def minSubArrayLen_alt(s, nums): # pretty much the same but list slicing looks more Pythonic; way slower, though
@@ -24,14 +21,55 @@ def minSubArrayLen_alt(s, nums): # pretty much the same but list slicing looks m
             curr_l = r - l + 1        
             glob_l = min(glob_l, curr_l)
             l += 1
-            curr_s = sum(nums[l:r + 1])
-            
+            curr_s = sum(nums[l:r + 1])          
         r += 1
     return glob if glob != math.inf else 0
 
+
+def minSubArrayLen_another(s, nums):
+    res = math.inf
+    if any(x == s for x in nums): return 1
+    l, r = 0, 0
+    curr, glob, curr_sum = 0, math.inf, 0
+    for r in range(len(nums)):
+        curr_sum += nums[r]
+        while curr_sum >= s:
+            glob = min(res, r - l + 1)
+            curr_sum -= nums[l]
+            l += 1
+    return glob if glob != math.inf else 0
+
+def minSubArrayLen_binary(s, nums):  # binary search works b/c the prefix sum is always increasing
+    def bi_search(A, target):
+        le, ri = 0, len(A) - 1
+        while le <= ri:
+            mid = le + (ri - le >> 1)
+            if target == A[mid]:
+                return mid
+            if target < A[mid]:
+                ri = mid - 1
+            elif target > A[mid]:
+                le = mid + 1
+        return le
+    ret = float('inf')
+    # get prefix sum for nums
+    cum = [0] * len(nums) + [0]
+    cum[0] = 0
+    for i in range(1, len(cum)):
+        cum[i] = cum[i-1] + nums[i-1]
+    for le, v in enumerate(cum):
+        # use binary search to locate 'ri' index
+        ri = bi_search(cum, v + s)
+        if ri < len(cum):
+            ret = min(ret, ri - le)
+    return 0 if ret == float('inf') else ret  
+            
+            
 if __name__ == '__main__':
     print(minSubArrayLen(7, [2,3,1,2,4,3]))
-    print(minSubArrayLen(11, [1,2,3,4,5]))
-    print(minSubArrayLen_alt(11, [1,2,3,4,5]))
-    print(minSubArrayLen_alt(7, [2, 3, 1, 2, 4, 3]))
+    # print(minSubArrayLen(11, [1,2,3,4,5]))
+    # print(minSubArrayLen_alt(11, [1,2,3,4,5]))
+    # print(minSubArrayLen_alt(7, [2, 3, 1, 2, 4, 3]))
+    print(minSubArrayLen_another(7, [2, 3, 1, 2, 4, 3]))
+    print(minSubArrayLen_binary(7, [2, 3, 1, 2, 4, 3]))
 
