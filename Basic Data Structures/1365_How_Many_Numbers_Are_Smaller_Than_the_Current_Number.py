d5e1d9994b64309d7be781d375045b4129333cfd
mkareev <mkareev@ebay.com>
2020-11-17 12:49:55 -0500

    Additions

--- Basic Data Structures/1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
+++ Basic Data Structures/1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.py
@@ -1,4 +1,4 @@
-def smallerNumbersThanCurrent(nums):
+def smallerNumbersThanCurrent(nums):  # O(n^2)
 	res = []
 	for k, v in enumerate(nums):
 		curr = 0
@@ -8,9 +8,33 @@ def smallerNumbersThanCurrent(nums):
 		res.append(curr)
 	return res
 
+def smallerNumbersThanCurrent_sort(nums):  # O(n*logn)
+	nums_sorted = sorted(nums)
+	res = []
+	for num in nums:
+		res.append(nums_sorted.index(num))
+	return res
+
+
+def smallerNumbersThanCurrent_zip(nums):
+	nums_sorted = sorted(nums)
+	res = [0] * len(nums)
+	for i in range(1, len(nums_sorted)):
+		if nums_sorted[i] > nums_sorted[i - 1]:
+			res[i] = i
+		else:
+			res[i] = res[i - 1]
+	combo = zip(nums, nums_sorted, res)
+	return [x for _,_,x in sorted(combo, key = lambda x: x[1])]
+
+
 if __name__ == '__main__':
-	print(smallerNumbersThanCurrent([8,1,2,2,3]))
-	print(smallerNumbersThanCurrent([6, 5, 4, 8]))
-	print(smallerNumbersThanCurrent([7, 7, 7, 7]))
+	print(smallerNumbersThanCurrent([8, 1, 2, 2, 3]))
+	# print(smallerNumbersThanCurrent([6, 5, 4, 8]))
+	# print(smallerNumbersThanCurrent([7, 7, 7, 7]))
+	# print(smallerNumbersThanCurrent_sort([8, 1, 2, 2, 3]))
+	# print(smallerNumbersThanCurrent_sort([6, 5, 4, 8]))
+	# print(smallerNumbersThanCurrent_sort([7, 7, 7, 7]))
+	print(smallerNumbersThanCurrent_zip([8, 1, 2, 2, 3]))
 
     
