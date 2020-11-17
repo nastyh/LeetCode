b1ca0706ad44c9e625e51c5e91a17f674bcd8b21
mkareev <mkareev@ebay.com>
2020-11-17 15:23:33 -0500

    Additions

--- Basic Data Structures/Sum_to_100.py
+++ Basic Data Structures/Sum_to_100.py
@@ -1,5 +1,6 @@
 """
-Add the mathematical operators + or - before any of the digits in the decimal numeric string 123456789 such that the resulting mathematical expression adds up to 100. Return all possible solutions.
+Add the mathematical operators + or - before any of the digits in the decimal numeric string 123456789 such that the
+resulting mathematical expression adds up to 100. Return all possible solutions.
 """
 
 class Solution:
@@ -16,4 +17,21 @@ a = Solution()
 a.dfs("123456789",0)
 for i in a.ans:
     print(i)
+
+
+# Backtracking solution
+def combinations(index, sofar, total):
+  if index == len(s):
+    if total == 100:
+      res.append(sofar)
+    return
+
+  for i in range(index+1, len(s) + 1):
+    combinations(i, sofar+'+' + s[index:i] if sofar else s[index:i], total + int(s[index:i]))
+    combinations(i, sofar + '-' + s[index:i], total - int(s[index:i]))
+
+combinations(0, '', 0)
+
+for idx, r in enumerate(res):
+  print(idx + 1, r)
 
--- Trees/987_Vertical_Order_Traversal_of_a_Binary Tree.py
+++ Trees/987_Vertical_Order_Traversal_of_a_Binary Tree.py
@@ -1,4 +1,3 @@
-
 from collections import deque, defaultdict
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -16,7 +15,6 @@ class TreeNode:
         min_col, max_col = 0, 0
         q = deque()
         q.append([root, 0, 0]) # order is node, col, row
-
         while q:
             t, col, row = q.popleft()
             d[col].append((row, t.val))
@@ -31,14 +29,51 @@ class TreeNode:
         res = []
         for c in range(min_col, max_col + 1):
             res.append([val for row, val in sorted(d[c])])
-        # return d
         return res
+
+
+    def verticalTraversal_BFS_other(self, root):
+        """
+        Slightly different from above. 
+        Build defaultdict in form of { col: [[row, val]] }
+        Then we will unpack it in two steps
+        First step: we will create a list of lists by_row that will contain dictionary's values (lists of lists)
+        sorted by row and value
+        Second step: iterate through every element inside by_row and extract the second element (value) of every element
+        Store everything in res and return 
+        """
+        if not root: return None
+        if root and not root.left and not root.right: return [root]
+        d = defaultdict(list)
+        q = deque()
+        res = []
+        q.append([0, 0, root])  # [col, row, val]
+        while q: 
+            for _ in range(len(q)):
+                combo = q.popleft()
+                col, row, t = combo[0], combo[1], combo[2]
+                d[col].append([row, t.val])
+                if t.left or t.right:
+                    row += 1
+                if t.left:
+                    q.append([col - 1, row, t.left])
+                if t.right:
+                    q.append([col + 1, row, t.right])
+        by_row = []
+        for x in sorted(d.items(), key = lambda x: (x[0], x[1])):
+            by_row.append(sorted(x[1]))
+        for el in by_row:
+            curr = []
+            for node in el:
+                curr.append(node[1])
+            res.append(curr)
+        return res
+
 
     def verticalTraversal_DFS(self, root): # DFS applied to filling the dictionary
         if not root: return None
         d = defaultdict(list)
         min_col, max_col = 0, 0
-
         def _helper(root, row, col):
             if root:
                 nonlocal min_col, max_col
@@ -48,16 +83,12 @@ class TreeNode:
 
                 _helper(root.left, row + 1, col - 1)
                 _helper(root.right, row + 1, col + 1)
-
         _helper(root, 0, 0)
         res = []
         for c in range(min_col, max_col + 1):
             res.append([val for row, val in sorted(d[c])])
     # return d
         return res
-
-
-
 
 
 if __name__ == '__main__':
@@ -68,5 +99,13 @@ if __name__ == '__main__':
     l.right.left = TreeNode(2)
     l.left.right = TreeNode(9)
     l.right.right = TreeNode(14)
-    print(l.verticalTraversal_BFS(l))
-    print(l.verticalTraversal_DFS(l))
+    m = TreeNode(1)
+    m.left = TreeNode(2)
+    m.right = TreeNode(3)
+    m.left.left = TreeNode(4)
+    m.left.right = TreeNode(5)
+    m.right.left = TreeNode(6)
+    m.right.right = TreeNode(7)
+    # print(l.verticalTraversal_BFS(l))
+    # print(l.verticalTraversal_DFS(l))
+    print(m.verticalTraversal_BFS_other(m))
