18e030e1e57d7b85000285e0ae5f5912bc0a5520
mkareev <mkareev@ebay.com>
2020-12-01 13:15:57 -0500

    Additions

--- Trees/111_Min_Depth_of_Binary_Tree.py
+++ Trees/111_Min_Depth_of_Binary_Tree.py
@@ -17,6 +17,7 @@ class TreeNode:
             return self.minDepth(root.left) + 1
         return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
 
+
     def minDepth_stupid_DFS(self, root):
         if not root: return 0
         def _helper(root):
@@ -29,8 +30,39 @@ class TreeNode:
                 return _helper(root.right) + 1
             if root.left and root.right:
                 return min(_helper(root.left), _helper(root.right)) + 1
-
         return _helper(root)
+
+
+    def minDepthDFS_another(self, root):
+        """
+        With a tiny twist
+        Start calculating the depth in a helper function
+        The key here is to initialize l and r to None
+        If any of these stay None, it means that there was no left (or right) tree. 
+        Then in the if portion we need to handle cases when there is no subtree. We don't want to get a zero in this case.
+        It handles edge cases when the whole tree is just left (or right) subtrees (like a linked list, essentially)
+        """
+        if not root: return 0
+        if root and not root.left and not root.right: return 1
+        def _helper(node, d):
+            if not node: return 
+            if node and not node.left and not node.right:
+                d += 1
+                return d
+            l, r = None, None
+            if node.left:
+                l = _helper(node.left, d)
+            if node.right:
+                r = _helper(node.right, d)
+            # this stupid portion is required to handle edge cases when a tree has only left (or right) children
+            if l is not None and r is not None:
+                return min(l, r) + 1
+            if l is None and r is not None:
+                return r + 1
+            if l is not None and r is None:
+                return l + 1
+        return _helper(root, 0)
+
 
     def minDepthDFS(self, root):
         if not root: return 0
@@ -48,12 +80,10 @@ class TreeNode:
 
     def minDepthBFS(self, root):
         if not root: return 0
-
         d = deque()
         d.append([root, 1])
         while d:
             t, curr_d = d.popleft()
-
             if not t.left and not t.right:
                 return curr_d
             if t.left:
@@ -72,5 +102,6 @@ if __name__ == '__main__':
 
     print(l.minDepth(l))
     print(l.minDepthDFS(l))
+    print(l.minDepthDFS_another(l))
     print(l.minDepthBFS(l))
     print(l.minDepth_stupid_DFS(l))
--- Trees/559_Max_depth_of_n-ary_tree.py
+++ Trees/559_Max_depth_of_n-ary_tree.py
@@ -1,44 +1,31 @@
-
+from collections import deque
 # Definition for a Node.
 class Node:
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children
 
-from collections import deque
-class Solution:
-    def maxDepth(self, root: Node) -> int:
+    def maxDepth(self, root: Node) -> int:  # times out
+        res = 0
+        if not root:
+            return 0
+        levels = deque([root])
+        while levels:
+            curr = levels.popleft()
+            res += 1
+            if root.children:
+                levels.append(root.children)
+        return res
 
 
-
-
-
-
-
-
-        # doing bfs
-
-#         res = 0
-#         if not root:
-#             return 0
-#         levels = deque([root])
-#         while levels:
-#             curr = levels.popleft()
-#             res += 1
-#             if root.children:
-#                 levels.append(root.children)
-#         return res
-
-
-
-        # doing dfs
-         if not root:
+    def maxDepth_dfs(self, root):  # works
+        if not root:
             return 0
         if not root.children:
             return 1
         levels = []
         for node in root.children:
-            levels.append(self.maxDepth(node))
+        levels.append(self.maxDepth(node))
         return max(levels) + 1
 
 
