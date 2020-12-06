0b0a99d57fc856e06733a88c55d70ff04f203f17
Michael Kareev <mkareev@umich.edu>
2020-12-06 15:40:54 -0500

    Update Basic Data Structures/1143_Longest_Common_Subsequence.py

--- Basic Data Structures/1143_Longest_Common_Subsequence.py
+++ Basic Data Structures/1143_Longest_Common_Subsequence.py
@@ -18,6 +18,28 @@ def longestCommonSubsequence(text1, text2):  # O(n^2) and O(len(text1) * len(tex
     return dp[-1][-1]
 
 
+def longestCommonSubsequence_alt(text1, text2):
+    """"
+    create a dp matrix where cols are text1 + 1
+    and rows are text2 + 1
+    everything in the first row and the first col stays the same: 0
+    Then start filling with (1, 1)
+    It means comparing the first character in text1 to the first character in text2. 
+    If they're the same, take the value from the diagonal (left and up) and add 1 to it
+    If they're different, take the max of (cell to the left, cell up)
+    Return the last element in the matrix
+    """
+    dp = [[0] * (len(text1) + 1) for _ in range((len(text2) + 1))]  # text1 is along the cols; text2 is along the rows
+    for col in range(1, len(text1) + 1):
+        for row in range(1, len(text2) + 1):
+            if text1[col - 1] == text2[row - 1]:
+                dp[row][col] = dp[row - 1][col - 1] + 1
+            else:
+                dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
+    print(dp)
+    return dp[-1][-1]
+
+
 if __name__ == '__main__':
-    # print(longestCommonSubsequence('aab', 'azb'))
+    print(longestCommonSubsequence('aab', 'azb'))
     print(longestCommonSubsequence('abcde', 'ace'))
