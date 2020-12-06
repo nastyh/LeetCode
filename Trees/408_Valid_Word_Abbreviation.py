f442f9dcb19c4e944e2f75ca1ffa01cee15c6ba7
Michael Kareev <mkareev@umich.edu>
2020-12-06 18:49:19 -0500

    Update Trees/408_Valid_Word_Abbreviation.py

---
+++ Trees/408_Valid_Word_Abbreviation.py
@@ -0,0 +1,17 @@
+def validWordAbbreviation(word, abbr):
+    i = j = 0 
+    while j < len(abbr) and i < len(word): 
+        if abbr[j].isalpha(): 
+            if abbr[j] != word[i]: 
+                return False 
+            i += 1 
+            j += 1 
+        else: 
+            if abbr[j] == '0':  # to handle edge cases such as "01", which are invalid
+                return False 
+            temp = 0 
+            while j < len(abbr) and abbr[j].isdigit(): 
+                temp = temp * 10 + int(abbr[j]) 
+                j += 1 
+            i += temp  
+    return j == len(abbr) and i == len(word)
