db8e4261eb26acf6553384d5ba5bb566e981d6d4
Michael Kareev <mkareev@umich.edu>
2020-12-06 12:42:37 -0500

    Additions

--- Trees/117_Populating_Next_Right_Pointers_in_Each_Node_II.py
+++ Trees/117_Populating_Next_Right_Pointers_in_Each_Node_II.py
@@ -16,7 +16,7 @@ class Solution:
             size = len(q)
             while size > 0:
                 node = q.popleft()
-                if size > 1 :#
+                if size > 1 :
                     node.next = q[0]
                 size -= 1
 
@@ -27,7 +27,7 @@ class Solution:
         return root
     
 
-    def connect_prev(self, root):  # easier to comprehend
+    def connect_prev(self, root):  # easier to comprehend  O(n) and O(n)
         """
         Maintain prev as a node. If it exists, then point prev.next to the current node.
         Otherwise, do nothing
@@ -48,3 +48,51 @@ class Solution:
                     d.append(t.right)
         return root
 
+
+    def connect_w_linked_list(self, root):  # O(n) and O(1)
+        
+        def processChild(childNode, prev, leftmost):  # helper func
+            if childNode:
+                # If the "prev" pointer is alread set i.e. if we
+                # already found atleast one node on the next level,
+                # setup its next pointer
+                if prev:
+                    prev.next = childNode
+                else:    
+                    # Else it means this child node is the first node
+                    # we have encountered on the next level, so, we
+                    # set the leftmost pointer
+                    leftmost = childNode
+                prev = childNode 
+            return prev, leftmost
+
+        if not root:
+            return root
+        # The root node is the only node on the first level
+        # and hence its the leftmost node for that level
+        leftmost = root
+        # We have no idea about the structure of the tree,
+        # so, we keep going until we do find the last level.
+        # The nodes on the last level won't have any children
+        while leftmost:
+            # "prev" tracks the latest node on the "next" level
+            # while "curr" tracks the latest node on the current
+            # level.
+            prev, curr = None, leftmost
+            # We reset this so that we can re-assign it to the leftmost
+            # node of the next level. Also, if there isn't one, this
+            # would help break us out of the outermost loop.
+            leftmost = None
+            # Iterate on the nodes in the current level using
+            # the next pointers already established.
+            while curr:
+                
+                # Process both the children and update the prev
+                # and leftmost pointers as necessary.
+                prev, leftmost = processChild(curr.left, prev, leftmost)
+                prev, leftmost = processChild(curr.right, prev, leftmost)
+                # Move onto the next node.
+                curr = curr.next         
+        return root 
+        
+       
---
+++ Trees/545_Boundary_of_Binary_Tree.py
@@ -0,0 +1,111 @@
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+    def boundaryOfBinaryTree_alt(self, root):
+        if not root:
+            return []
+        if not root.left and not root.right:
+            return [root.val]
+        left_boundary = [root.val]
+        leaves = []
+        right_boundary = []
+        if root.left:
+            n = root.left
+            while n.left or n.right:
+                left_boundary.append(n.val)
+                if n.left:
+                    n = n.left
+                else:
+                    n = n.right
+        if root.right:
+            n = root.right
+            while n.right or n.left:
+                right_boundary.append(n.val)
+                if n.right:
+                    n = n.right
+                else:
+                    n = n.left
+        def _leaves(root):
+            if not root:
+                return
+            if not root.left and not root.right:
+                leaves.append(root.val)
+                return
+            _leaves(root.left)
+            _leaves(root.right)
+        _leaves(root)
+        return left_boundary + leaves + right_boundary[::-1]
+
+
+    def boundaryOfBinaryTree(self, root):  # don't work for edge cases
+        left_res = []
+        right_res = []
+        leaves_res = []
+        def _left_helper(node): 
+            # d = deque()
+            # d.append(node)
+            # while d:
+            #     t = d.popleft()
+            #     if not t.left and not t.right:
+            #         return
+            #     left_res.append(t.val)
+
+            nonlocal left_res
+            if node:
+                if node.left:
+                    left_res.append(node.val)
+                    _left_helper(node.left)
+                elif node.right:
+                    left_res.append(node.val)
+                    _left_helper(node.right)
+        def _right_helper(node):
+            nonlocal right_res
+            if node:
+                if node.right:
+                    right_res.append(node.val)
+                    _right_helper(node.right)
+                elif node.left:
+                    right_res.append(node.val)
+                    _right_helper(node.left)
+        def _leaves_helper(node):
+            nonlocal leaves_res
+            if node:
+                if not node.left and not node.right:
+                    leaves_res.append(node.val)
+                if node.left:
+                    _leaves_helper(node.left)
+                if node.right:
+                    _leaves_helper(node.right)
+        if root.left: _left_helper(root)
+        if root.left or root.right: _leaves_helper(root)
+        if root.right: _right_helper(root)
+        
+        return [root.val] + left_res + leaves_res + right_res[1:][::-1] if not root.left else left_res + leaves_res + right_res[1:][::-1]
+        # return leaves_res
+
+
+if __name__ == '__main__':
+    l = TreeNode(1)
+    l.left = TreeNode(2)
+    l.right = TreeNode(3)
+    l.left.left = TreeNode(4)
+    l.left.right = TreeNode(5)
+    l.left.right.left = TreeNode(7)
+    l.left.right.right = TreeNode(8)
+    l.right.left = TreeNode(6)
+    l.right.left.left = TreeNode(9)
+    l.right.left.right = TreeNode(10)
+    m = TreeNode(1)
+    m.right = TreeNode(2)
+    m.right.left = TreeNode(3)
+    m.right.right = TreeNode(4)
+
+    print(l.boundaryOfBinaryTree_alt(l))
+    print(m.boundaryOfBinaryTree_alt(m))
+    print(l.boundaryOfBinaryTree(l))
+    print(m.boundaryOfBinaryTree(m))
