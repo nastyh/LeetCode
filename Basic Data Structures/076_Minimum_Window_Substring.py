3eb2590541bf953eaeae6e3801b2d7b86a1ccd8a
mkareev <mkareev@ebay.com>
2020-12-08 16:50:30 -0500

    Additions

--- Basic Data Structures/076_Minimum_Window_Substring.py
+++ Basic Data Structures/076_Minimum_Window_Substring.py
@@ -25,9 +25,7 @@ def minWindow(s, t):
                 ToFind += 1
                 head += 1
 
-    return s[L:R+1]
-
-
+    return s[L:R + 1]
 
     """
 
@@ -39,7 +37,6 @@ Implentation:
 use a dictionary d to store the number of each character we need to find. (negative values mean we have some extra!) 
 ToFind is the total number of characters we still need to find. ind is a list of indices of the characters in d. head is a pointer in ind so ind[head] is the start position of the substring and s[ind[head]] is that character).
     """
-
 
 def minWindow_alt(s,  t): # easier to follow
     l, r, curr_l, glob_ans, glob_l = 0, 0, 0, '', math.inf
@@ -61,6 +58,49 @@ def minWindow_alt(s,  t): # easier to follow
         r += 1   
     return glob_ans
 
+def minWindow_another(s, t):
+    if not t or not s:
+        return ""
+    dict_t = Counter(t)
+    required = len(dict_t)
+        # Filter all the characters from s into a new list along with their index.
+        # The filtering criteria is that the character should be present in t.
+    filtered_s = []
+    for i, char in enumerate(s):
+        if char in dict_t:
+            filtered_s.append((i, char))
+    l, r = 0, 0
+    formed = 0
+    window_counts = {}
+
+    ans = float("inf"), None, None
+
+    # Look for the characters only in the filtered list instead of entire s. This helps to reduce our search.
+    # Hence, we follow the sliding window approach on as small list.
+    while r < len(filtered_s):
+        character = filtered_s[r][1]
+        window_counts[character] = window_counts.get(character, 0) + 1
+        if window_counts[character] == dict_t[character]:
+            formed += 1
+        # If the current window has all the characters in desired frequencies i.e. t is present in the window
+        while l <= r and formed == required:
+            character = filtered_s[l][1]
+
+            # Save the smallest window until now.
+            end = filtered_s[r][0]
+            start = filtered_s[l][0]
+            if end - start + 1 < ans[0]:
+                ans = (end - start + 1, start, end)
+            window_counts[character] -= 1
+            if window_counts[character] < dict_t[character]:
+                formed -= 1
+            l += 1    
+
+        r += 1    
+    return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+
 if __name__ == '__main__':
     print(minWindow_alt("ADOBECODEBANC", "ABC"))
     print(minWindow_alt("ab", "a"))
+    print(minWindow_another("ADOBECODEBANC", "ABC"))
+    print(minWindow_another("ab", "a"))
