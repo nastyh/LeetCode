d5ccab5aa4131769d94ef0e56688114277065294
mkareev <mkareev@ebay.com>
2020-12-08 14:01:42 -0500

    Additions

--- Basic Data Structures/349_Intersection_of_Two_Arrays.py
+++ Basic Data Structures/349_Intersection_of_Two_Arrays.py
@@ -24,7 +24,25 @@ def intersection(nums1, nums2):
             res.append(el)
     return set(res)
 
-
+# potential additions
+def intersection_no_sets(nums1, nums2):
+    nums1.sort()
+    nums2.sort()
+    if not nums1 or not nums2 or len(nums1) < 1 or len(nums2)<1:
+        return []
+    l, r = 0, 0
+    res = []
+    while l < len(nums1) and r < len(nums2):
+        if nums1[l] == nums2[r]:
+            if not res or res[-1] != nums1[l]:
+                res.append(nums1[l])
+            l += 1
+            r += 1
+        elif nums1[l] < nums2[r]:
+            l += 1
+        else:
+            r += 1
+    return res
 
 if __name__ == '__main__':
-    print(intersection([1,2,2,1], [2,2]))
+    print(intersection([1, 2, 2, 1], [2, 2]))
--- Basic Data Structures/350_Intersection_of_Two_Arrays_II.py
+++ Basic Data Structures/350_Intersection_of_Two_Arrays_II.py
@@ -31,9 +31,31 @@ def intersect_dict(nums1, nums2):  # a bit faster but uses extra space
             d_long[k] -= 1
     return res
 
+def intersect_sort(nums1, nums2):
+    nums1.sort()
+    nums2.sort()
+    if not nums1 or not nums2 or len(nums1) < 1 or len(nums2)<1:
+        return []
+    
+    l, r = 0, 0
+    res = []
+    
+    while l < len(nums1) and r<len(nums2):
+        if nums1[l] == nums2[r]:
+            if not res or res[-1] != nums1[l]:
+                res.append(nums1[l])
+            l += 1
+            r += 1
+        elif nums1[l] < nums2[r]:
+            l += 1
+        else:
+            r += 1
+    return res
+
 
 if __name__ == '__main__':
     print(intersect([1, 2, 2, 1], [2, 2]))
     print(intersect([4, 9, 5], [9, 4, 9, 8, 4]))
     print(intersect_dict([1, 2, 2, 1], [2, 2]))
     print(intersect_dict([4, 9, 5], [9, 4, 9, 8, 4]))
+    print(intersect_sort([4, 9, 5], [9, 4, 9, 8, 4]))
