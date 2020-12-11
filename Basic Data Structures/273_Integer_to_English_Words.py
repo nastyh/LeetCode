28324bc8e9c5d568e1a927b41e94aab0fc6a7b78
mkareev <mkareev@ebay.com>
2020-12-11 14:06:10 -0500

    Alternative versions

--- Basic Data Structures/273_Integer_to_English_Words.py
+++ Basic Data Structures/273_Integer_to_English_Words.py
@@ -1,7 +1,8 @@
  def numberToWords(self, num: int) -> str:
         ## RC ##
         ## APPROACH : BRUTE FORCE ##
-        ## 1. For a two digit number if there is no ones digit 20 -> twenty + " " (number generally), dont leave the space behind, use if else case with "" (empty). Similarly for 20,000 etc.
+        ## 1. For a two digit number if there is no ones digit 20 -> twenty + " " (number generally), dont leave the space behind, use if else case with "" (empty).
+        # Similarly for 20,000 etc.
         
     one_digit = {
         1: 'One',
@@ -76,3 +77,34 @@
         result += ' ' if result else ''
         result += get_three_digit_num(last_three)
     return result
+
+def numberToWords(num):
+    def convert(n):
+        units_words = ['', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+        tens_words = ['', 'Ten', 'Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
+        teens_words = ['', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen']
+        
+        n, units = divmod(n, 10)            
+        n, tens = divmod(n, 10)
+        hundreds = n
+        
+        s = ""
+        if hundreds:
+            s += units_words[hundreds] + " Hundred"
+            
+        if tens == 1 and units > 0:
+            s += (" " if s else "") + teens_words[units]
+        else:
+            if tens:
+                s += (" " if s else "") + tens_words[tens]
+            if units:
+                s += (" " if s else "") + units_words[units]
+                
+        return s
+    result = ""
+    powers = [(10**9, ' Billion'), (10**6, ' Million'), (10**3, ' Thousand'), (1, '')]        
+    for power, word in powers:
+        triad = (num // power) % 10**3
+        if triad:
+            result += (" " if result else "") + convert(triad) + word
+    return result or 'Zero'
