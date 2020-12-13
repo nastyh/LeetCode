2b716586cece1651477166aababccddb36c6e790
Michael Kareev <mkareev@umich.edu>
2020-12-13 13:17:07 -0500

    Update Basic Data Structures/325_Maximum_Size_Subarray_Sum_Equals_k.py

--- Basic Data Structures/325_Maximum_Size_Subarray_Sum_Equals_k.py
+++ Basic Data Structures/325_Maximum_Size_Subarray_Sum_Equals_k.py
@@ -3,9 +3,9 @@ def maxSubArrayLen(nums, k):
         return 0
     curr_max = 0   # current record of max length
     cum_sum = 0    # cumulative sum so far
-    idx_dict = {0:-1} # initialize the dictionary
+    idx_dict = {0 : -1} # initialize the dictionary
 
-    for i,num in enumerate(nums):
+    for i, num in enumerate(nums):
         cum_sum += num
         diff = cum_sum - k
         if diff in idx_dict:
@@ -13,23 +13,24 @@ def maxSubArrayLen(nums, k):
         if cum_sum not in idx_dict:
             idx_dict[cum_sum] = i       # only keep the oldest index to have the longest subarray
     return curr_max
-    
-
-def test(nums):
-    res = [None] * len(nums)
-    res[0] = nums[0]
-    for ix in range(1, len(nums)):
-        res[ix] = res[ix - 1] + nums[ix]
-    return res
 
 
-def test_same(nums):
-    for ix in range(1, len(nums)):
-        nums[ix] += nums[ix - 1]
-    return nums
+def maxSubArrayLen_alt(nums, k):
+    seen_sum = {0:0}
+    total_sum, largest_len = 0, 0
+    for i in range(len(nums)):
+        total_sum += nums[i]
+        if total_sum == k:
+            largest_len = i + 1
+        else: 
+            required = total_sum - k
+            if required in seen_sum:
+                largest_len = max(largest_len, i - seen_sum[required])
+        if total_sum not in seen_sum:
+            seen_sum[total_sum] = i
+    return largest_len
 
 
 if __name__ == '__main__':
     print(maxSubArrayLen([-1], -1))
-    print(test([6, 3, -2, 4, -1, 0, -5]))
-    print(test_same([6, 3, -2, 4, -1, 0, -5]))
+    print(maxSubArrayLen_alt([-1], -1))
