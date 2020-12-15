1403b037b5fddb97f84f6289c38304d1c4cf7592
Michael Kareev <mkareev@umich.edu>
2020-12-14 22:35:21 -0500

    Additions

--- Basic Data Structures/852_Peak_Index_in_a_Mountain_Array.py
+++ Basic Data Structures/852_Peak_Index_in_a_Mountain_Array.py
@@ -6,6 +6,17 @@ def peakIndexInMountainArray(A):
     #     if A[i + 1] < A[i]:
     #         return i
 
+
+def peakIndexInMountainArray_bin_search(A):
+    l, r = 0, len(A) - 1
+    while l < r:
+        m = l + (r - l) // 2
+        if A[m] > A[m + 1]:
+            r = m
+        else:
+            l = m + 1
+    return l
+
 if __name__ == '__main__':
     print(peakIndexInMountainArray([3, 4, 5, 1]))
 
