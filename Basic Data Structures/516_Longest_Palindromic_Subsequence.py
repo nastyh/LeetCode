e77bbe3e5d508cbafe95e9bd23f8f0cc7464ff45
Michael Kareev <mkareev@umich.edu>
2020-12-20 13:09:43 -0500

    Additions

--- Basic Data Structures/516_Longest_Palindromic_Subsequence.py
+++ Basic Data Structures/516_Longest_Palindromic_Subsequence.py
@@ -12,17 +12,47 @@ def longestPalindromeSubseq(s):  # O(n^2) and O(n^2)
     return dp[0][len(s) - 1]
 
 
-def longestPalindromeSubseq_alt(s):  # O(n^2) and O(1)
+def longestPalindromeSubseq_alt(s):  # O(n^2) and O(n)
     pre = [0] * len(s)  
-	for i in reversed(range(len(s))):
-		cur = [0] * len(s)  
-		for j in range(i, len(s)):
-			if i == j:
-				cur[j] = 1
-				continue
-			if s[i] == s[j]:
-				cur[j] = pre[j-1] + 2
-			else:    
-				cur[j] = max(pre[j], cur[j-1])
-		pre = cur        
-	return pre[-1]   
+    for i in reversed(range(len(s))):
+        cur = [0] * len(s)  
+        for j in range(i, len(s)):
+            if i == j:
+                cur[j] = 1
+                continue
+            if s[i] == s[j]:
+                cur[j] = pre[j-1] + 2
+            else:    
+                cur[j] = max(pre[j], cur[j - 1])
+        pre = cur        
+    return pre[-1]   
+
+
+def longestPalindromeSubseq_bottoms_up(s):  # O(n^2) and O(n^2)
+    """
+    Diagonals filled with 1 (b/c strings of one character are palindroms)
+    Then start two loops: the starting index is the second last character
+    Ending index is the starting + 1 index
+    If letters at indices aren't the same, choose the max between the element to the left and below
+    If the same, add 2 to the value in the left lower corner
+    We fill out the triangle above the main diagonal.
+    The answer is in the right upper corner 
+    """
+    dp = [[0] * len(s) for _ in range(len(s))]
+    for i in range(len(s)):  # main diagonal
+        dp[i][i] = 1 
+    for st in range(len(s) - 2, -1, -1):
+        for en in range(st + 1, len(s)):
+            if s[st] != s[en]:
+                dp[st][en] = max(dp[st + 1][en], dp[st][en - 1])
+            else:
+                dp[st][en] = 2 + dp[st + 1][en - 1]
+    return dp[0][-1]
+
+
+if __name__ == '__main__':
+    # print(longestPalindromeSubseq('abdbca'))
+    # print(longestPalindromeSubseq_alt('abdbca'))
+    print(longestPalindromeSubseq_bottoms_up('abdbca'))
+    print(longestPalindromeSubseq_bottoms_up('bbbab'))
+    print(longestPalindromeSubseq_bottoms_up('cbbd'))
