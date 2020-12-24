ed8a73b22c6f0b41a7db8b0e88ffc76ec6937856
Michael Kareev <mkareev@umich.edu>
2020-12-24 10:04:21 -0500

    Update Linked Lists/24_Swap_Nodes_in_Pairs.py

--- Linked Lists/24_Swap_Nodes_in_Pairs.py
+++ Linked Lists/24_Swap_Nodes_in_Pairs.py
@@ -14,19 +14,28 @@ class ListNode:
 			prev.next = second
 			first.next = second.next
 			second.next = first
-			
 			prev = first
 			head = first.next
 		return t.next
 
-	def swapPairs_rec(self, head):
+
+	def swapPairs_alt(self, head):
+		if not head: return
+        first = head
+        second = head.next
+        while first and second:
+            first.val, second.val = second.val, first.val
+            first = second.next
+            if not first: break
+            second = second.next.next
+        return head
+
+	def swapPairs_rec(self, head):  # O(n) and O(n)
 		if not head or not head.next:
             return head
-
         # Nodes to be swapped
         first_node = head
         second_node = head.next
-
         # Swapping
         first_node.next  = self.swapPairs(second_node.next)
         second_node.next = first_node
