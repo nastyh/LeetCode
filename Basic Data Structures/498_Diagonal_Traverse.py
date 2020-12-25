ca8b33801672150e7d3525bc0a032debcbc7631c
Michael Kareev <mkareev@umich.edu>
2020-12-25 10:58:15 -0500

    Update Basic Data Structures/498_Diagonal_Traverse.py

--- Basic Data Structures/498_Diagonal_Traverse.py
+++ Basic Data Structures/498_Diagonal_Traverse.py
@@ -23,5 +23,42 @@ def findDiagonalOrder(nums):  # if to sum up elements' indices on a given diagon
     return res
 
 
+def findDiagonalOrder_simulation(matrix):
+    n, m = len(matrix), len(matrix[0]) if matrix else 0
+    i, j, result = 0, 0, []
+    if not (n or m):
+        return []
+    if n == 1:
+        return matrix[0]
+    if m == 1:
+        return [matrix[i][0] for i in range(n)]
+    # Traverse the matrix diagonally based on current traversing direction (calculated using
+    # Manhattan distance)
+    while i * j <= (n - 1) * (m - 1):
+        # First, add the number to the results list
+        result.append(matrix[i][j])
+        # Second, get the Manhattan distance
+        manhattan = i + j
+        # Based on Manhattan distance, find the traversing direction
+        if not manhattan % 2:
+            # Direction from bottom-left to top-right
+            if not i and j + 1 < m:
+                j = j + 1
+            elif j == m - 1:
+                i = i + 1
+            else:
+                i, j = i - 1, j + 1
+        else:
+            # Direction from top-right to bottom-left
+            if not j and i + 1 < n:
+                i = i + 1
+            elif i == n - 1:
+                j = j + 1
+            else:
+                i, j = i + 1, j - 1
+    return result
+
+ 
 if __name__ == '__main__':
-    print(findDiagonalOrder([[ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ]]))
+    print(findDiagonalOrder([[ 1, 2, 3], [4, 5, 6], [ 7, 8, 9]]))
+    print(findDiagonalOrder_simulation([[ 1, 2, 3], [4, 5, 6], [ 7, 8, 9]]))
