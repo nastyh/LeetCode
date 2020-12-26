41dee675677d618f61e10b9be5c246d305e2d7b6
Michael Kareev <mkareev@umich.edu>
2020-12-26 14:01:16 -0500

    Update Basic Data Structures/303_Range_Sum_Query_Immutable.py

--- Basic Data Structures/303_Range_Sum_Query_Immutable.py
+++ Basic Data Structures/303_Range_Sum_Query_Immutable.py
@@ -11,6 +11,28 @@ class NumArray:
 
 
 
+class NumArray_another:
+    """
+    Slightly different
+    Need to consider an edge case when a provided list is empty, otherwise running_sum will throw an error
+    """
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        if len(self.nums) == 0:
+            self.running_sum = [0]
+        else:
+            self.running_sum = [None] * len(self.nums)
+            self.running_sum[0] = self.nums[0]
+            for i in range(1, len(self.nums)):
+                self.running_sum[i] = self.running_sum[i - 1] + self.nums[i]
+
+
+    def sumRange(self, i: int, j: int) -> int:
+        return self.running_sum[j] - self.running_sum[i] + self.nums[i]
+
+
+
 
 # normal implementation
 def sumRange(nums, i, j):
@@ -27,8 +49,25 @@ def sumRange(nums, i, j):
     return orig_sum == cum_sum_list[j] - cum_sum_list[i] + nums[i]
 
 
+def sumRange_alt(nums, i, j):  # doesn't pass weird edge cases 
+    orig_sum = sum(nums[i:j + 1])
+
+    def test_cum_sum(nums):
+        cum_sum = [0] * len(nums)
+        cum_sum[0] = nums[0]
+        for i in range(1, len(nums)):
+            cum_sum[i] = cum_sum[i - 1] + nums[i]
+        return cum_sum
+    
+    cum_sum_list = test_cum_sum(nums)
+    return orig_sum == cum_sum_list[j] - cum_sum_list[i] + nums[i]
+
+
 if __name__ == '__main__':
     # print(test_cum_sum([-2, 0, 3, -5, 2, -1]))
     print(sumRange([-2, 0, 3, -5, 2, -1], 0, 2))
     print(sumRange([-2, 0, 3, -5, 2, -1], 2, 5))
     print(sumRange([-2, 0, 3, -5, 2, -1], 0, 5))
+    print(sumRange_alt([-2, 0, 3, -5, 2, -1], 0, 2))
+    print(sumRange_alt([-2, 0, 3, -5, 2, -1], 2, 5))
+    print(sumRange_alt([-2, 0, 3, -5, 2, -1], 0, 5))
