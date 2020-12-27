7018f5403671c25b5073f085c2ff83d624f00aaa
Michael Kareev <mkareev@umich.edu>
2020-12-27 14:23:09 -0500

    Update Basic Data Structures/152_Maximum_Product_Subarray.py

--- Basic Data Structures/152_Maximum_Product_Subarray.py
+++ Basic Data Structures/152_Maximum_Product_Subarray.py
@@ -62,6 +62,33 @@ def maxProduct_brute_force_another(nums):  # O(n^2)
     return res
 
 
+def maxProduct_find_indices(nums):
+    if len(nums) == 0:
+        return 0
+    max_so_far = nums[0]
+    min_so_far = nums[0]
+    result = max_so_far
+    end_index = 0
+    for i in range(1, len(nums)):
+        curr = nums[i]
+        temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+        min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+        max_so_far = temp_max
+        if max_so_far > result:
+            result = max_so_far
+            end_index = i 
+    
+    cur = nums[end_index]
+    j = end_index - 1
+    # no change in complexity
+    while cur != result and j >= 0:
+        cur *= nums[j]
+        j -= 1
+    print(nums[j + 1:i + 1]) 
+    return result
+
+
 if __name__ == '__main__':
     print(maxProduct([2, 3, -2, 4]))
     print(maxProduct_alt([2, 3, -2, 4]))
@@ -78,3 +105,4 @@ if __name__ == '__main__':
     print(maxProduct_brute_force_another([-2, 3, -4]))
     print(maxProduct_brute_force_another([2, 3, -2, 4]))
     print(maxProduct_brute_force_another([-2, 0, -1]))
+    print(maxProduct_find_indices([2, 3, -2, 4]))
