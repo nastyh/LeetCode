c90bab591d17dc99b0c4a809cd72eb8d051ef7c5
Michael Kareev <mkareev@umich.edu>
2021-01-01 12:37:14 -0500

    Update Basic Data Structures/267_Palindrome_Permutation_II.py

--- Basic Data Structures/267_Palindrome_Permutation_II.py
+++ Basic Data Structures/267_Palindrome_Permutation_II.py
@@ -39,5 +39,38 @@ def generatePalindromes(s):
     permute('', visited)
     return res
 
+
+def generatePalindromes_alt(s):  # works but quickly times out. O((n + 1)!) and O(n)
+    """
+    Brute force approach:
+    Generate all permutations (_permutations() does it, taking care of repeat characters by doing sorting)
+    Then go one by one and check if it's a palindrome 
+    """
+    ans = []
+    d = Counter(s)
+    if sum([1 for v in d.values() if v % 2 == 1]) > 1: return []
+    def _isPal(string):
+        return string == string[::-1]
+    def _permutations(st):
+        res = []
+        l = len(st)
+        def _helper(string, curr_res):
+            string = "".join(sorted(string))
+            if len(curr_res) == l:
+                res.append(curr_res[:])
+            for i in range(len(string)):
+                if i > 0 and string[i] == string[i - 1]:
+                    continue
+                _helper(string[:i] + string[i + 1:], curr_res + string[i])
+        _helper(st, '')
+        return res
+    all_possible = _permutations(s)
+    for candidate in all_possible:
+        if _isPal(candidate):
+            ans.append(candidate)
+    return ans
+
 if __name__ == '__main__':
-    print(generatePalindromes('aabb'))
+    # print(generatePalindromes('aabb'))
+    print(generatePalindromes_alt('aabb'))
+    # print(generatePalindromes_alt('aabbhijkkjih'))  # takes a lot of time to run
