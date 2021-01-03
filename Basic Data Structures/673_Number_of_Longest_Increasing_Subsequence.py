c039280b40efb0a7ac133f1af8953f02669a9dbd
Michael Kareev <mkareev@umich.edu>
2021-01-03 12:52:03 -0500

    Update Basic Data Structures/673_Number_of_Longest_Increasing_Subsequence.py

--- Basic Data Structures/673_Number_of_Longest_Increasing_Subsequence.py
+++ Basic Data Structures/673_Number_of_Longest_Increasing_Subsequence.py
@@ -11,6 +11,25 @@ def findnumberofLIS(nums):
     return sum(c for l, c in zip(dp, cnt) if l == m)
 
 
+def findnumberofLIS_alt(nums):  # bottoms-up, O(n^2) and O(n)
+    """
+    If the number at the current index is bigger than the number at the previous index, we increment the count for LIS up to the current index.
+    But if there is a bigger LIS without including the number at the current index, we take that.
+    """
+    dp = [0] * len(nums)
+    dp[0] = 1 
+    res = 1 
+    for i in range(1, len(nums)):
+        dp[i] = 1 
+        for j in range(i):
+            if nums[i] < nums[j] and dp[i] <= dp[j]:
+                dp[i] = dp[j] + 1
+                res  = max(res, dp[i])
+    return res 
+
+
 if __name__ == '__main__':
     print(findnumberofLIS([1, 3, 5, 4, 7]))
     print(findnumberofLIS([2, 2, 2, 2, 2]))
+    print(findnumberofLIS_alt([1, 3, 5, 4, 7]))
+    print(findnumberofLIS_alt([2, 2, 2, 2, 2]))
