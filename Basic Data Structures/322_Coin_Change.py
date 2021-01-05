e9966accb2f7a7112bf677b599573c82c81db481
mkareev <mkareev@ebay.com>
2021-01-05 12:55:50 -0500

    Additions

--- Basic Data Structures/322_Coin_Change.py
+++ Basic Data Structures/322_Coin_Change.py
@@ -7,6 +7,17 @@ def coinchange(coins, target):
 	return dp[target] if dp[target] != float('inf') else -1 
 
 
+def coinchange_bottoms_up(coins, target):
+	dp = [[0] * (target + 1) for _ in range(len(coins))]
+	for row in range(len(coins)):  # first col
+		dp[row][0] = 1
+	for row in range(len(coins)):
+		for col in range(1, target):
+
+
 if __name__ == '__main__':
 	print(coinchange([1,2,5], 11))
 	print(coinchange([2], 3))
+
+
+
