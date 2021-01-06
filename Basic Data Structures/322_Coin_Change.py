c0ce9573e3c5efe393e116af61a16b2a6e169aab
mkareev <mkareev@ebay.com>
2021-01-06 14:31:05 -0500

    Additions

--- Basic Data Structures/322_Coin_Change.py
+++ Basic Data Structures/322_Coin_Change.py
@@ -8,11 +8,13 @@ def coinchange(coins, target):
 
 
 def coinchange_bottoms_up(coins, target):  # doesn't work
+	if target == 0: return 0
 	dp = [[0] * (target + 1) for _ in range(len(coins))]
 	for col in range(1, target + 1):  # first row
 		if coins[0] <= col:
-			dp[0][col] = 1 + dp[0][col - coins[0]]
-	print(dp)
+			if col % coins[0] == 0:
+				dp[0][col] = 1 + dp[0][col - coins[0]]
+	# print(dp)
 	for row in range(1, len(coins)):
 		for col in range(1, target + 1):
 			if coins[row] > col:
@@ -23,24 +25,17 @@ def coinchange_bottoms_up(coins, target):  # doesn't work
 	return dp[-1][-1] if dp[-1][-1] != 0 else -1
 
 
-def coinchange_bottoms_up_alt(denominations, total):  # doesn't work
-	n = len(denominations)
-	dp = [[0 for _ in range(total+1)] for _ in range(n)]
-
-	# populate the total = 0 columns, as we will always have an empty set for zero total
-	for i in range(n):
-		dp[i][0] = 1
-
-	# process all sub-arrays for all capacities
-	for i in range(n):
-		for t in range(1, total+1):
-			if i > 0:
-				dp[i][t] = dp[i - 1][t]
-			if t >= denominations[i]:
-				dp[i][t] += dp[i][t - denominations[i]]
-
-	# total combinations will be at the bottom-right corner.
-	return dp[-1][-1] if dp[-1][-1] != 0 else -1
+def coinchange_bottoms_up_alt(coins, target):
+	dp = [[float("inf") for x in range(target + 1)] for y in range(len(coins) + 1)]
+	for i in range(1, len(coins) + 1):
+		dp[i][0] = 0
+	for i in range(len(coins) + 1):
+		for j in range(target + 1):
+			if coins[i - 1] <= j:
+				dp[i][j] = min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j])
+			else:
+				dp[i][j] = dp[i - 1][j]
+	return dp[-1][-1] if dp[-1][-1] != float("inf") else -1
 
 
 
@@ -49,8 +44,11 @@ if __name__ == '__main__':
 	# print(coinchange([2], 3))
 	print(coinchange_bottoms_up([1, 2, 5], 11))
 	print(coinchange_bottoms_up([2], 3))
-	print(coinchange_bottoms_up_alt([1, 2, 5], 11))
-	print(coinchange_bottoms_up_alt([2], 3))
+	print(coinchange_bottoms_up([1], 1))
+	print(coinchange_bottoms_up([1], 2))
+	print(coinchange_bottoms_up([1, 5, 6, 8], 11))
+	# print(coinchange_bottoms_up_alt([1, 2, 5], 11))
+	# print(coinchange_bottoms_up_alt([2], 3))
 
 
 
