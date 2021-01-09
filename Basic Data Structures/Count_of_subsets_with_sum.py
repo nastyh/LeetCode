3153a7a35a8b40f73331aa01ac1e8911aa018021
Michael Kareev <mkareev@umich.edu>
2021-01-09 14:58:31 -0500

    Update Basic Data Structures/Count_of_subsets_with_sum.py

--- Basic Data Structures/Count_of_subsets_with_sum.py
+++ Basic Data Structures/Count_of_subsets_with_sum.py
@@ -21,15 +21,31 @@ def count_subsets(nums, s):
     return dp[-1][-1]
 
 
-def count_subsets_bottoms_up(nums, s):
+def count_subsets_bottoms_up_clean(nums, s):  # O(NS) where N total numbers and S is the desired sum
     dp = [[0] * (s + 1) for _ in range(len(nums))]
-    for row in range(len(nums)):  # first col
+    for row in range(len(nums)):  # first col is one b/c there is always 1 way to have a sum of zero
         dp[row][0] = 1
-    for col in range(1, s + 1):  # first row 
+    for col in range(1, s + 1):  # first row: with one number there is only one way to meet the sum: iff this number equals the sum 
+        if col == nums[0]:
+            dp[0][col] = 1
+    for row in range(1, len(nums)):
+        for col in range(1, s + 1):
+            if nums[row] > col:  # if the current number is too big, don't take it all
+                dp[row][col] = dp[row - 1][col]
+            else:  # otherwise 
+                dp[row][col] = dp[row - 1][col] + dp[row - 1][col - nums[row]]
+    return dp[-1][-1]
+
+
+def count_subsets_bottoms_up(nums, s):  # O(NS) where N total numbers and S is the desired sum
+    dp = [[0] * (s + 1) for _ in range(len(nums))]
+    for row in range(len(nums)):  # first col is one b/c there is always 1 way to have a sum of zero
+        dp[row][0] = 1
+    for col in range(1, s + 1):  # first row: with one number there is only one way to meet the sum: iff this number equals the sum 
         if col == nums[0]:
             dp[0][col] = 1 
     # print(dp)
-    for row in range(1, len(nums)):
+    for row in range(1, len(nums)):  # starting from the cell (1. 1)
         for col in range(1, s + 1):
             dp[row][col] = dp[row - 1][col]
             if col >= nums[row]:
@@ -42,5 +58,7 @@ def count_subsets_bottoms_up(nums, s):
 if __name__ == '__main__':
     print(count_subsets([1, 1, 2, 3], 4))
     print(count_subsets([1, 2, 7, 1, 5], 9))
+    print(count_subsets_bottoms_up_clean([1, 1, 2, 3], 4))
+    print(count_subsets_bottoms_up_clean([1, 2, 7, 1, 5], 9))
     print(count_subsets_bottoms_up([1, 1, 2, 3], 4))
     print(count_subsets_bottoms_up([1, 2, 7, 1, 5], 9))
