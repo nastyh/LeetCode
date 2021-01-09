7c69bc4359f26047282b1246a5b075f91a683255
Michael Kareev <mkareev@umich.edu>
2021-01-09 15:14:11 -0500

    Update Basic Data Structures/Unbounded_Knapsack.py

--- Basic Data Structures/Unbounded_Knapsack.py
+++ Basic Data Structures/Unbounded_Knapsack.py
@@ -1,13 +1,13 @@
 def unbounded_knapsack(profit, weight, capacity):  # O(N*capacity) both, where N = len(profit)
     dp = [[0] * (capacity + 1) for _ in range(len(profit))]
-    for col in range(1, capacity + 1):  # first col
+    for col in range(1, capacity + 1):  # first row
         if weight[0] <= col:
             dp[0][col] = profit[0] + dp[0][col - weight[0]]
     for row in range(1, len(profit)):  # starting from cell (1, 1)
         for col in range(1, capacity + 1):
             if weight[row] > col:  # if current weight is larger than the capacity
                 dp[row][col] = dp[row - 1][col]  # don't take, but go with the previous choice
-            else:  # otherwise, choose the max between the previous choice and taking the current profit and whatever the profit is from the leftovers above
+            else:  # otherwise, choose the max between the previous choice (means not taking current item) and taking the current profit and whatever the profit is from the leftovers above
                 dp[row][col] = max(dp[row - 1][col], dp[row - 1][col - weight[row]] + profit[row])
     return dp[-1][-1]
 
