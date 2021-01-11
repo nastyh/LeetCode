6c4468102578f76c4294e0465e35918d35bffd6c
mkareev <mkareev@ebay.com>
2021-01-11 15:36:17 -0500

    Update 977_Squares_of_a_Sorted_Array.py

--- Basic Data Structures/977_Squares_of_a_Sorted_Array.py
+++ Basic Data Structures/977_Squares_of_a_Sorted_Array.py
@@ -1,8 +1,8 @@
-def sortedSquares(nums): # pythonic
+def sortedSquares(nums): # pythonic  O(nlogn) and O(N)
     return sorted([i**2 for i in nums])
 
 
-def sortedSquares_list(nums):
+def sortedSquares_list(nums):  # O(N) both
     res = [None] * len(nums)
     l, r = 0, len(nums) - 1
     for i in range(len(nums) - 1, -1, -1):
@@ -15,18 +15,19 @@ def sortedSquares_list(nums):
     return res
 
 
-def sortedSquares_efficient(nums):
+def sortedSquares_efficient(nums):  # O(N) both
     if len(nums) == 0: return
     if len(nums) == 1: return [x**2 for x in nums]
-    if all([x >=0 for x in nums]): return  [x**2 for x in nums]
+    if all([x >= 0 for x in nums]): return  [x**2 for x in nums]
+    if all([x < 0 for x in nums]): return  [x**2 for x in nums][::-1]
     l, r, res = 0, len(nums) - 1, []
 
     while nums[l] < 0: # finding the index of the most right element that is < 0
         l += 1
-    l -= 1
+    l -= 1  # stepping back b/c we overshoot
     while nums[r] >= 0: # finding the index of the most left element that is > 0
         r -= 1
-    r += 1
+    r += 1  # stepping back b/c we overshoot
 
     while l >= 0 and r < len(nums): # moving l to the left and r to the right
         if abs(nums[l]) <= abs(nums[r]):
@@ -93,13 +94,15 @@ def sortedSquares_alt(nums):
 
 
 if __name__ == '__main__':
-    # print(sortedSquares([-4,-1,0,3,10]))
-    # print(sortedSquares([-7,-3,2,3,11]))
-    # print(sortedSquares_efficient([-4,-1,0,3,10]))
-    # print(sortedSquares_efficient([-7,-3,2,3,11]))
-    print(sortedSquares_alt([-4,-1,0,3,10]))
-    print(sortedSquares_alt([-7,-3,2,3,11]))
+    # print(sortedSquares([-4, -1, 0, 3, 10]))
+    # print(sortedSquares([-7, -3, 2, 3, 11]))
+    # print(sortedSquares_efficient([-4,-1, 0, 3, 10]))
+    # print(sortedSquares_efficient([-7,-3, 2, 3, 11]))
+    # print(sortedSquares_alt([-4,-1, 0, 3, 10]))
+    # print(sortedSquares_alt([-7,-3, 2, 3, 11]))
     # print(sortedSquares_efficient([-2, 0]))
     print(sortedSquares_alt([-2, 0]))
     print(sortedSquares_alt([-5, -3, -2, -1]))
-    # print(sortedSquares_efficient([-5, -3, -2, -1]))
+    print(sortedSquares_efficient([-5, -3, -2, -1]))
+    print(sortedSquares_efficient([-2, 0]))
+
