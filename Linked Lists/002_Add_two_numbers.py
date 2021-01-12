d580edfdc6fa7a869ae9f2d44b28c751b29e283e
mkareev <mkareev@ebay.com>
2021-01-12 12:27:13 -0500

    Update 002_Add_two_numbers.py

--- Linked Lists/002_Add_two_numbers.py
+++ Linked Lists/002_Add_two_numbers.py
@@ -25,6 +25,44 @@ class ListNode(object):
             l3.next = ListNode(1)
         return dummy.next
 
+    
+    def addTwoNumbers_alt(self, l1, l2):
+        """
+        Slighty different in the main loop: while l1 and l2 
+        It finishes looping once we are done with a shorter list.
+        We still have to process a longer one
+        And if after all of that we still have a carry, add an extra 1
+        """
+        if not l1 and not l2: return
+        if l1 and not l2: return l1
+        if not l1 and l2: return l2
+        l3 = dummy = ListNode(-1)
+        carry = 0
+        while l1 and l2:
+            num = (l1.val + l2.val + carry) % 10
+            carry = (l1.val + l2.val + carry) // 10
+            l3.next = ListNode(num)
+            l3 = l3.next
+            l1 = l1.next
+            l2 = l2.next
+        while l1:
+            num = carry + l1.val
+            carry = num // 10
+            l3.next = ListNode(num % 10)
+            l3 = l3.next
+            l1 = l1.next
+        while l2:
+            num = carry + l2.val
+            carry = num // 10
+            l3.next = ListNode(num % 10)
+            l3 = l3.next
+            l2 = l2. next
+        if carry != 0:
+            l3.next = ListNode(1)
+        return dummy.next 
+        
+
+
 if __name__ == '__main__':
     l1 = ListNode(2)
     l1.next = ListNode(4)
