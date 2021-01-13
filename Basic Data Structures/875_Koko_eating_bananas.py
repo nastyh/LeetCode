754767c6ae5d77d9ad29adeb2e7aa2abd4f753a7
mkareev <mkareev@ebay.com>
2021-01-13 15:32:54 -0500

    Update 875_Koko_eating_bananas.py

--- Basic Data Structures/875_Koko_eating_bananas.py
+++ Basic Data Structures/875_Koko_eating_bananas.py
@@ -1,10 +1,8 @@
 def minEatingSpeed(piles, H):
-
     def whether(piles, H, K):
         # print(K)
-        total = [ (bananas+K-1)//K for bananas in piles]
+        total = [ (bananas + K - 1) // K for bananas in piles]
         return sum(total) <= H
-
     left, right = 0, max(piles)
     while left < right: # O(lgn) for binary search, totally O(NlgN)
         mid = (left + right) / 2
@@ -16,6 +14,34 @@ def minEatingSpeed(piles, H):
     return round(left)
 
 
+def minEatingSpeed_alt(piles, H):  # O(nlogn) and O(1)
+    """
+    Binary search approach
+    l = 1, r equals to the max number in piles
+    m here is a number, not an index (like normally in a bin search)
+    helper does the following: if chose 6 as a rate and pile is of the size of 13,
+    how much time it'll take to eat the pile? It's 6 + 6 + 1, thus, it's 3 hours
+    It returns whether we can improve the soluton
+    """
+    res = 0
+    l, r = 1, max(piles)
+    def _helper(piles, k, H):
+        hours = 0
+        for pile in piles:
+            div = pile // k
+            hours += div
+            if pile % k != 0:
+                hours += 1
+        return hours <= H
+    while l <= r:
+        m = l + (r - l) // 2
+        if _helper(piles, m, H):
+            r = m - 1
+        else:
+            l = m + 1
+    return l
+
+
 if __name__ == '__main__':
-    print(minEatingSpeed([30,11,23,4,20], 6))
+    print(minEatingSpeed([30, 11, 23, 4, 20], 6))
 
