a4811633325095aeeaf9b4cd1bd8f15d98290bf2
mkareev <mkareev@ebay.com>
2021-01-14 11:30:49 -0500

    Update 160_Intersection_of_Two_Linked_Lists.py

--- Linked Lists/160_Intersection_of_Two_Linked_Lists.py
+++ Linked Lists/160_Intersection_of_Two_Linked_Lists.py
@@ -1,10 +1,9 @@
 def getIntersectionNode(headA, headB): # using extra space
-d_a = {}
+    d_a = {}
     curr = headA
     while curr:
         d_a[curr] = curr.val
         curr = curr.next
-
     curr_b = headB
     while curr_b:
         if curr_b in d_a:
@@ -12,28 +11,39 @@ d_a = {}
         curr_b = curr_b.next
     return None
 
-def getIntersectionNode(headA, headB): # without extra_space
 
-    hA = headA
-    hB = headB
-    A = 0 #len of headA
-    while headA:
-        A += 1
-        headA = headA.next
-    B = 0 #len of headB
-    while headB:
-        B += 1
-        headB = headB.next
-    diff = A-B #difference
-    while diff>0:
-        hA = hA.next
-        diff -= 1
-    while diff<0:
-        hB = hB.next
-        diff += 1
-    while hA != hB:
-        hA = hA.next
-        hB = hB.next
-    return hA
+def getIntersectionNode(headA, headB): # O(length_a + length_b) and O(1)
+    """
+    calculate the lengths of both lists
+    Calculate the difference in length as length of longer - length of shorter
+    Move the pointer pointing to the head of the longer by the difference
+    Now you have two pointers in way so that the number of elements to the right from them is equal
+    Traverse in parallel, if found equal, return 
+    """
+    length_a, length_b = 0, 0
+    curr_a, curr_b = headA, headB
+    while curr_a:
+        curr_a = curr_a.next
+        length_a += 1
+    while curr_b:
+        curr_b = curr_b.next
+        length_b += 1
+    curr_a, curr_b = headA, headB
+    if length_a > length_b:
+        diff = length_a - length_b
+        while diff > 0:
+            curr_a = curr_a.next
+            diff -= 1
+    else:
+        diff = length_b - length_a
+        while diff > 0:
+            curr_b = curr_b.next
+            diff -= 1
+    while curr_a:
+        if curr_a == curr_b:
+            return curr_a
+        curr_a = curr_a.next
+        curr_b = curr_b.next
+    return None
 
 
