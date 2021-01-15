f2aa5f000f141a312bcb1105174266cfa62f26df
mkareev <mkareev@ebay.com>
2021-01-15 13:24:04 -0500

    Update 364_Nested_List_Weight_Sum_II.py

--- Basic Data Structures/364_Nested_List_Weight_Sum_II.py
+++ Basic Data Structures/364_Nested_List_Weight_Sum_II.py
@@ -1,4 +1,29 @@
 import math
+from collections import deque
+def depthSumInverse_bfs(nestedList):  # O(n) both
+    """
+    max depth is the number of the closing brackets at the end of nestedList
+    Works generally, doesn't work for the LeetCode implementation
+    """
+    max_depth = 0
+    d, res = deque(), 0
+    for ch in str(nestedList)[::-1]:
+        if ch == ']':
+            max_depth += 1
+        else:
+            break
+    for element in nestedList:
+        d.append((element, max_depth))
+    while d:
+        t, depth = d.popleft()
+        if isinstance(t, int):
+            res += t * depth
+        else:
+            for other in t:
+                d.append((other, depth - 1))
+    return res
+
+
 def depthSumInverse(nestedList):
     max_d = -math.inf
     def _helper(curr, numofList, depth):
@@ -16,5 +41,31 @@ def depthSumInverse(nestedList):
         res += (max_d - depth + 1) * num
     return res
 
+
+def depthSum_bfs_leetcode(nestedList):
+    dic = {}
+    level = 0
+    q = collections.deque()
+    q.append(nestedList)
+    res = 0
+    while q:
+        size = len(q)
+        level += 1
+        sums = 0
+        for _ in range(size):
+            nl = q.popleft()
+            for n in nl:
+                if n.isInteger():
+                    sums += n.getInteger()
+                else:
+                    q.append(n.getList())
+        dic[level] = sums
+    for k, v in dic.items():
+        res += (level + 1 - k) * v
+    return res
+
+
 if __name__ == '__main__':
-    print(depthSumInverse([[1,1],2,[1,1]]))
+    print(depthSumInverse([[1, 1], 2, [1, 1]]))
+    print(depthSumInverse_bfs([[1, 1], 2, [1, 1]]))
+    print(depthSumInverse_bfs([1, [4, [6]]]))
