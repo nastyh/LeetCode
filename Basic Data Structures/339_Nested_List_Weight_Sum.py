99e323b31470808f7f8c12fababc57196eeea5cc
mkareev <mkareev@ebay.com>
2021-01-15 12:15:28 -0500

    Update 339_Nested_List_Weight_Sum.py

--- Basic Data Structures/339_Nested_List_Weight_Sum.py
+++ Basic Data Structures/339_Nested_List_Weight_Sum.py
@@ -1,4 +1,5 @@
-def depthSum(nestedList):  # plain vanilla implementation from scratch
+from collections import deque
+def depthSum(nestedList):  # plain vanilla implementation from scratch, O(n) both
     if len(nestedList) == 0: return 0
     res = 0
     def _helper(nestedList, depth):
@@ -9,6 +10,22 @@ def depthSum(nestedList):  # plain vanilla implementation from scratch
             else:
                 _helper(item, depth + 1)
     _helper(nestedList, 1)
+    return res
+
+
+def depthSum_bfs(nestedList):  # BFS from scratch, O(n) both
+    if len(nestedList) == 0: return 0
+    d = deque()
+    res = 0
+    for element in nestedList:
+        d.append((element, 1))
+    while d:
+        t, depth = d.popleft()
+        if isinstance(t, int):
+            res += t * depth
+        else:
+            for smaller_element in t:
+                d.append((smaller_element, depth + 1))
     return res
 
 
@@ -24,5 +41,23 @@ def depthSum_leetcode(nestedList):  # using Leetcode's implementation
     return dfs(nestedList, 1)
 
 
+def depthSum_bfs_leetcode(nestedList):
+    res = 0
+    d = deque([])
+    for x in nestedList:
+        d.append((x, 1))
+    while d: 
+        item, level = d.popleft()
+        if item.isInteger():
+            res += item.getInteger() * level
+        else:
+            for val in item.getList():
+                stack.append((val, level + 1))
+    return res
+        
+
 if __name__ == '__main__':
     print(depthSum([[1, 1], 2,[1, 1]]))
+    print(depthSum_bfs([[1, 1], 2,[1, 1]]))
+    print(depthSum_bfs([1, [4, [6]]]))
+    print(depthSum_bfs([0]))
