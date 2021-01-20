dc9576d0d3ff28a69da95eec4947efe967298ec5
mkareev <mkareev@ebay.com>
2021-01-20 13:29:47 -0500

    Update 020_Valid Parentheses.py

--- Basic Data Structures/020_Valid Parentheses.py
+++ Basic Data Structures/020_Valid Parentheses.py
@@ -1,3 +1,37 @@
+def isValid_optimal(s):  # O(n) both
+    """
+    Keep adding brackets till they're opening
+    If you see a closing:
+    if there is nothing in the stack, return False
+    otherwise, check if the last element in the stack and the current element from the same family
+    if no, return False
+    if yes, pop and proceed
+    """
+    opening = ['(', '[', '{']
+    def _helper(l, r):
+        if l == '(' and r == ')':
+            return True
+        elif l == '[' and r == ']':
+            return True
+        elif l == '{' and r == '}':
+            return True
+        else:
+            return False 
+    if len(s) % 2 == 1: return False
+    st = []
+    for ch in s:
+        if ch in opening:
+            st.append(ch)
+        else:
+            if len(st) == 0:
+                return False
+            elif _helper(st[-1], ch):
+                st.pop()
+            else:
+                return False
+    return len(st) == 0 
+
+
 def isValid(s):
     op, l = ['(','{','['], []
     if len(s) == 0:   # edge cases
@@ -29,4 +63,5 @@ def isValid(s):
 
 
 if __name__ == '__main__':
+    print(isValid_optimal('()'))
     print(isValid('()'))
