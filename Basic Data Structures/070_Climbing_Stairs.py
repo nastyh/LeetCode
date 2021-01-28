474e3021e572bd2361edc63aaf6e11a991b5f060
mkareev <mkareev@ebay.com>
2021-01-28 11:29:41 -0500

    Update 070_Climbing_Stairs.py

--- Basic Data Structures/070_Climbing_Stairs.py
+++ Basic Data Structures/070_Climbing_Stairs.py
@@ -1,8 +1,26 @@
-def stairs(n):
+def stairs_optimal(n):  # O(n) and O(1)
+	"""
+	Don't need the whole dp list. 2 variables are enough
+	l and r keep the initial edge cases
+	Then we need to update them at the same time: turn l into r and for r it's l + r (l)
+	Interesting and not obvious while loop: going to n = 1, not to n = 0
+	"""
+	if n <= 2: return n
+	l, r = 1, 2
+	while n != 1:
+		l, r = r, l + r
+		n -= 1
+	return l
+
+
+def stairs(n):  # O(n) both
+	"""
+	DP approach
+	Base cases are 0, 1, 2 steps. Don't have to have zero, though but for the sake of readability
+	"""
 	if n == 1:
 		return 1
-	dp = [None] * (n + 1)
-	dp[0] = 0
+	dp = [0] * (n + 1)
 	dp[1] = 1
 	dp[2] = 2
 	for i in range(3, len(dp)):
@@ -23,6 +41,9 @@ def stairs_alt(n):
 	
 	
 if __name__ == '__main__':
+	print(stairs_optimal(3))
+	print(stairs_optimal(4))
+	print(stairs_optimal(5))
 	print(stairs(3))
 	print(stairs(4))
 	print(stairs(5))
