e8a0ffe1c69e1d3b146c64532413b72e8b38fec9
mkareev <mkareev@ebay.com>
2021-02-05 13:01:31 -0500

    Additions

--- Basic Data Structures/008_String_to_Integer_atoi.py
+++ Basic Data Structures/008_String_to_Integer_atoi.py
@@ -1,29 +1,36 @@
-def myAtoi(s): # Leetcode's edge cases are weird
-    if len(s) == 0:
-        return 0
-    if s[0].isalpha():
-        return 0
+def myAtoi(self, s: str) -> int:
+    i = 0
+    sign = '+'
+    result = 0
+    # ignore leading whitespaces
+    while i < len(s) and s[i] == ' ':
+        i += 1
+    # locking for result sign
+    if i < len(s) and s[i] in {'-', '+'}:
+        sign = s[i]
+        i += 1
+    # read characters until the next non-digit charcter
+    while i < len(s) and s[i].isdigit():
+        result *= 10
+        result += int(s[i])
+        i += 1
+    result = result if sign == '+' else result * -1
+    int_min_value, int_max_value = (-2 ** 31), (2 ** 31) - 1
+    if result < int_min_value:
+        return int_min_value
+    if int_max_value < result:
+        return int_max_value  
+    return result
 
-    s_stripped = s.strip()
-    digits = ''
-    digits = ''.join([str(i) for i in s_stripped if i.isdigit()])
-    res = int(digits)
-    int_min, int_max = -2147483648, 2147483647
-    if res <= -2**31:
-        return -2**31
-    if res >= 2**31 - 1:
-        return 2**31 - 1
-    return -1 * res if s_stripped[0] == '-' else res
 
 def myAtoi_alt(str):
     index = 0
     if len(str) == 0:
         return 0
     while index != len(str) and str[index] == ' ':
-        index+=1
+        index += 1
     if index == len(str):
         return 0
-
     negative = False
     #see if we have a sign
     if str[index] == '+':
@@ -31,7 +38,6 @@ def myAtoi_alt(str):
     elif str[index] == '-':
         negative = True
         index+=1
-
     #now we check the rest of the string for numbers
     res = ''
     while index < len(str):
@@ -40,18 +46,16 @@ def myAtoi_alt(str):
         else:
             break
         index+=1
-
     if res is not '':
         integer = int(res)
         if negative is True:
             integer = 0-integer
     else:
         return 0
-
-    if integer < 2**31-1 and integer > -2**31 :
+    if integer < 2**31 - 1 and integer > -2**31 :
         return integer
-    elif integer >= 2*31-1:
-        return 2**31-1
+    elif integer >= 2*31 - 1:
+        return 2**31 - 1
     elif integer <= -2**31:
         return -2**31
 
@@ -59,5 +63,4 @@ def myAtoi_alt(str):
 
 
 if __name__ == '__main__':
-    print(myAtoi('-91283472332'))
     print(myAtoi_alt('-91283472332'))
--- Trees/333_Largest_BST_Subtree.py
+++ Trees/333_Largest_BST_Subtree.py
@@ -1,15 +1,19 @@
-class Solution:
-    def largestBSTSubtree(self, root):
-        ans = 0
-        def isBSTandCount(r):  
-            nonlocal ans
-            if not r: return True, 0, math.inf, -math.inf
-            isl, lCount, lMin, lMax = isBSTandCount(r.left)
-            isr, rCount, rMin, rMax = isBSTandCount(r.right)
-            if isl and isr and lMax < r.val and rMin > r.val: 
-                self.ans = max(self.ans, lCount + rCount + 1)
-                return True, lCount + rCount + 1, min(lMin, r.val), max(rMax, r.val)
-            else:
-                return False, 0, 0, 0
-        _,_,_,_ = isBSTandCount(root)
-        return ans
+def largestBSTSubtree(self, root):  # O(N) both
+    """
+    Need to keep track if a current tree is a BST
+    Increment ans
+    Pass appropriate borders to make sure that the current node's value within the boundaries
+    """
+    ans = 0
+    def isBSTandCount(r):  
+        nonlocal ans
+        if not r: return True, 0, math.inf, -math.inf
+        isl, lCount, lMin, lMax = isBSTandCount(r.left)
+        isr, rCount, rMin, rMax = isBSTandCount(r.right)
+        if isl and isr and lMax < r.val and rMin > r.val: 
+            self.ans = max(self.ans, lCount + rCount + 1)
+            return True, lCount + rCount + 1, min(lMin, r.val), max(rMax, r.val)
+        else:
+            return False, 0, 0, 0
+    _,_,_,_ = isBSTandCount(root)
+    return ans
