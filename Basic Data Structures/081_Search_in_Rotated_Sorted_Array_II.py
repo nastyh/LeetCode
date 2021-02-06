01997824b1d89e6f3eecb335798629aa0ce117c6
Michael Kareev <mkareev@umich.edu>
2021-02-06 17:00:48 -0500

    Update Basic Data Structures/081_Search_in_Rotated_Sorted_Array_II.py

--- Basic Data Structures/081_Search_in_Rotated_Sorted_Array_II.py
+++ Basic Data Structures/081_Search_in_Rotated_Sorted_Array_II.py
@@ -1,21 +1,33 @@
- def search(nums, target)
+ def search(nums, target):  # O(logn) and O(1)
+    """
+    Need to account for possible duplicates standing next to each other
+    Keep moving begin and end to the middle till elelements next to them are the same
+    After that the main part starts
+    First, the best scenario
+    Second, nums[mid] > nums[end] means that something's wrong, it shouldn't be like that in a normal sorted array.
+    Thus, let's better go to the left part. If target is within the bounds of the left part, do a usual bin search on the left part
+    Otherwise, move to the right
+    Third, nums[mid] <= nums[end], it means that potentially the right side is normal and sorted.
+    Check if target is within the boundaries of the right side. If yes, do a binary search.
+    If no, go to the left side
+    """
     begin = 0
     end = len(nums) - 1 
-    while begin <= end:
+    while begin <= end:  
         while begin < end and nums[begin] == nums[begin + 1]:
             begin += 1
         while begin < end and nums[end] == nums[end - 1]:
             end -= 1
-        mid = (begin + end)//2
+        mid = begin + (end - begin) // 2
         if nums[mid] == target:
             return True
         if nums[mid] > nums[end]: # Left side of mid is sorted
-            if  nums[begin] <= target and target < nums[mid]: # Target in the left side
+            if  nums[begin] <= target < nums[mid]: # Target in the left side
                 end = mid - 1
             else: # in right side
                 begin = mid + 1
         else: # Right side is sorted
-            if  nums[mid] < target and target <= nums[end]: # Target in the right side
+            if  nums[mid] < target <= nums[end]: # Target in the right side
                 begin = mid + 1
             else: # in left side
                 end = mid - 1
