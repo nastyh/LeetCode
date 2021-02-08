a215311e12f1cbb263fecee3276e72636a6c601b
mkareev <mkareev@ebay.com>
2021-02-08 12:42:29 -0500

    Update 056_Merge_Intervals.py

--- Basic Data Structures/056_Merge_Intervals.py
+++ Basic Data Structures/056_Merge_Intervals.py
@@ -1,3 +1,17 @@
+def merge_optimal(intervals):  # O(nlogn) and O(n)
+    if len(intervals) <=1: return intervals
+    intervals.sort(key = lambda x: x[0])
+    res = []
+    res.append(intervals[0])
+    for ix in range(1, len(intervals)):
+        if res[-1][1] >= intervals[ix][0]:
+            res[-1][1] = max(res[-1][1], intervals[ix][1])
+            res[-1][0] = min(res[-1][0], intervals[ix][0])  # not really necessary
+        else:
+            res.append(intervals[ix])
+    return res
+
+
 def merge(intervals):
     if len(intervals) == 0:
         return []
@@ -11,8 +25,8 @@ def merge(intervals):
                 merged[-1][-1] = interval[1]
         else:
             merged.append(interval)
-
     return merged
+
 
 def merge_m(intervals): # using max instead of if statements, probably looks more professional
     if len(intervals) == 0:
@@ -29,7 +43,7 @@ def merge_m(intervals): # using max instead of if statements, probably looks mor
 
 
 if __name__ == '__main__':
-    print(merge([[1,3],[2,6],[8,10],[15,18]]))
-    print(merge_m([[1,3],[2,6],[8,10],[15,18]]))
-    print(merge([[1,4], [2,3]]))
-    print(merge_m([[1,4], [2,3]]))
+    print(merge([[1, 3], [2, 6], [8, 10], [15, 18]]))
+    print(merge_m([[1, 3],[2, 6], [8, 10], [15, 18]]))
+    print(merge([[1, 4], [2, 3]]))
+    print(merge_m([[1, 4], [2, 3]]))
