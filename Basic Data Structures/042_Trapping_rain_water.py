bb1d9d31044050ef2d529a2f43a4496972676397
Michael Kareev <mkareev@umich.edu>
2021-02-09 07:50:55 -0500

    Update Basic Data Structures/042_Trapping_rain_water.py

--- Basic Data Structures/042_Trapping_rain_water.py
+++ Basic Data Structures/042_Trapping_rain_water.py
@@ -27,6 +27,30 @@ def trap(height):
     return water_volume
 
 
+def trap_two_passes(height):  # O(n) both but technically O(2n)
+    """
+    Need to cover the edge case (important)
+    Do two passes: from left to right and vice versa 
+    Initialize the first element with the first element in height
+    Then choose the max b/w the current height and the previous value in the helper array 
+    This way we'll maximize the gains: it's like moving left/right until we hit a taller building, then go to its roof and repeat
+    Do the same from right to left
+    Finally iterate over two helper arrays, choose the min value (b/c the water will overflow otherwise),
+    and subtract the current height from this number to get how much water is in this cell
+    """
+    if len(height) == 0: return 0
+    from_left, from_right = [0] * len(height), [0] * len(height)
+    from_left[0] = height[0]
+    for i in range(1, len(height)):
+        from_left[i] = max(from_left[i - 1], height[i])
+    from_right[-1] = height[-1]
+    for j in range(len(height) -2, -1, -1):
+        from_right[j] = max(from_right[j + 1], height[j])
+    for z in range(len(height)):
+        res += min(from_left[z], from_right[z]) - height[z]
+    return res
+
+
 def trap_short(height):
     if not height:   return 0
     max_left, max_right = [height[0]], [height[-1]]
@@ -34,3 +58,8 @@ def trap_short(height):
         max_left.append(max(max_left[-1], height[i]))
         max_right.append(max(max_right[-1], height[-1 - i]))
     return sum(min(l, r) - h for l, r, h in zip(max_left, reversed(max_right), height))
+
+
+if __name__ == '__main__':
+    print(trap_two_passes([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
+    print(trap_two_passes([4, 2, 0, 3, 2, 5]))
