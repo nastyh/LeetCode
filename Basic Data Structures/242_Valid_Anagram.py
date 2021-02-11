9b4c7fe38b1966c8cccf3c9e3e07c1dc5d0c615f
Michael Kareev <mkareev@umich.edu>
2021-02-11 08:23:28 -0500

    Additions

--- Basic Data Structures/242_Valid_Anagram.py
+++ Basic Data Structures/242_Valid_Anagram.py
@@ -1,14 +1,37 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        s_d, t_d = {}, {}
-        for i in s:
-            if i in s_d:
-                s_d[i] += 1
-            else:
-                s_d[i] = 1
-        for j in t:
-            if j in t_d:
-                t_d[j] += 1
-            else:
-                t_d[j] = 1
-        return s_d == t_d
+from collections import Counter
+def isAnagram(s, t):
+    s_d, t_d = {}, {}
+    for i in s:
+        if i in s_d:
+            s_d[i] += 1
+        else:
+            s_d[i] = 1
+    for j in t:
+        if j in t_d:
+            t_d[j] += 1
+        else:
+            t_d[j] = 1
+    return s_d == t_d
+
+
+def isAnagram_short(s, t):  # O(n) both
+    return Counter(s) == Counter(t)
+
+
+def isAnagram_sort(s, t):  # O(nlogn) and O(1)
+    return sorted(s) == sorted(t)
+
+
+def isAnagram_one_dict(s, t):  # O(n) both
+    """
+    Checking if the sum of all values == 0 won't work for a case 'aacc' 'ccac' b/c
+    the dictionary will be {a: 1, c: -1}
+    Sum is 0 but it's not an anagram 
+    """
+    s_d = Counter(s)
+    for ch in t:
+        if ch not in s_d:
+            return False
+        else:
+            s_d[ch] -= 1
+    return all(v == 0 for v in s_d.values())
