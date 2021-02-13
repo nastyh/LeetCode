72b90db82ad79810ff80ff7e2103e184649c1c07
Michael Kareev <mkareev@umich.edu>
2021-02-13 15:57:21 -0500

    Update Basic Data Structures/1091_Shortest_Path_in_Binary_Matrix.py

--- Basic Data Structures/1091_Shortest_Path_in_Binary_Matrix.py
+++ Basic Data Structures/1091_Shortest_Path_in_Binary_Matrix.py
@@ -38,29 +38,16 @@ def shortestPathBinaryMatrix_optimized(grid): ## O(MN) and O(1)
     if grid[0][0] == 1: return -1
     if grid[-1][-1] == 1: return -1
     d = deque()
-    res = 1
-    # d.append((0, 0, 1))
-    # grid[0][0] = 2
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            if grid[row][col] == 0:
-                d.append((row, col, 1))
-                grid[row][col] = 2
-                while d:
-                    s = len(d)
-                    for _ in range(s):
-                        r, c, st = d.popleft()
-                        if r == len(grid) - 1 and c == len(grid[0]) - 1:
-                            return st
-                        for row_offset, col_offset in [(r - 1, c - 1), (r + 1, c + 1), (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1), (r - 1, c + 1), (r + 1, c - 1)]:
-                            if 0 <= row_offset < len(grid) and 0 <= col_offset < len(grid[0]) and grid[row_offset][col_offset] == 0:
-                                grid[row_offset][col_offset] = 2
-                                # st += 1
-                                d.append((row_offset, col_offset, st + 1))
-    # if (row_offset != len(grid) -1 and col_offset != len(grid[0]) - 1):
-    #     return -1
-    # else:
-    #     return st
+    d.append((0, 0, 1))
+    grid[0][0] = 2  # marking as visited 
+    while d:
+        r, c, st = d.popleft()
+        if r == len(grid) - 1 and c == len(grid[0]) - 1:  # return condition
+            return st
+        for row_offset, col_offset in [(r - 1, c - 1), (r + 1, c + 1), (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1), (r - 1, c + 1), (r + 1, c - 1)]:
+            if 0 <= row_offset < len(grid) and 0 <= col_offset < len(grid[0]) and grid[row_offset][col_offset] == 0:
+                grid[row_offset][col_offset] = 2
+                d.append((row_offset, col_offset, st + 1))  # add to st here not above: st += 1
     return -1
 
 
