d7a9726e70f86d0ed376b921cb2132998002d6fb
Michael Kareev <mkareev@umich.edu>
2021-02-14 07:57:49 -0500

    Update Basic Data Structures/157_Read_N_Characters_Given_Read_4.py

--- Basic Data Structures/157_Read_N_Characters_Given_Read_4.py
+++ Basic Data Structures/157_Read_N_Characters_Given_Read_4.py
@@ -1,4 +1,4 @@
-def read(buf, n):
+def read(buf, n):  # O(n) to copy N characters and O(1) to keep the buffer of 4 elements
     i = 0
     while i < n: 
         buf4 = ['','','','']
@@ -8,3 +8,17 @@ def read(buf, n):
         buf[i:] = buf4[:count] # Copy from buf4 to buf.
         i += count
     return i
+
+
+def read_alt(buf, n):
+    cnt = 0
+    tmp = []
+    while cnt < n:
+        buf4 = [None] * 4
+        rv = read4(buf4)
+        cnt += rv
+        tmp.extend(buf4)
+        if rv < 4:
+            break
+    buf[:] = [a for a in tmp[:n] if a]
+    return len(buf)
