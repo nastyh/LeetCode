91cde6e4b6d1d6d5de77ec6177259c99be248f96
Michael Kareev <mkareev@umich.edu>
2021-02-14 08:04:03 -0500

    Update Basic Data Structures/158_Read_N_Characters_Given_Read4_II-Call_multiple_times.py

--- Basic Data Structures/158_Read_N_Characters_Given_Read4_II-Call_multiple_times.py
+++ Basic Data Structures/158_Read_N_Characters_Given_Read4_II-Call_multiple_times.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution(object):  # O(n) both
 	def __init__(self):
 		self.remain = deque()                           # Queue to hold extra characters
 	def read(self, buf, n):
@@ -14,30 +14,32 @@ class Solution(object):
 		t = 1
 		while count < n and t > 0:                      # Loop is same as Read Once
 			t = read4(tmp4)
-			buf[count:count+4] = tmp4
+			buf[count:count + 4] = tmp4
 			count += t
 		if count > n:
-			self.remain = deque(tmp4[t-(count-n):t])    # Copy overflow characters TO queue
+			self.remain = deque(tmp4[t - (count - n):t])    # Copy overflow characters TO queue
 			count = n
 		return count
 
     
 # with a queue
 
-from queue import Queue
-class Solution:
+class Solution(object):  # O(nR) n times and R is time complexity of read4. O(f) where f is the size of the file 
+    
     def __init__(self):
-        self.queue = Queue()
-        
+        self.buffer = collections.deque()
+    
     def read(self, buf, n):
-        qnt = 1
-        while self.queue.qsize() < n and qnt:
-            bf = [''] * 4
-            qnt = read4(bf)
-            for i in range(qnt):
-                self.queue.put(bf[i])
+        buf4 = [None] * 4
+        while len(self.buffer) < n:
+            num = read4(buf4)
+            if num == 0:
+                break
+            self.buffer.extend(buf4[:num])
+            
         i = 0
-        while i < n and self.queue.qsize():
-            buf[i] = self.queue.get()
+        while i < n and self.buffer:
+            buf[i] = self.buffer.popleft()
             i += 1
+            
         return i
