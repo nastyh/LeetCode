94fe9550057d54d1fd8dcab0cf3176a4093e94de
Michael Kareev <mkareev@umich.edu>
2021-02-13 21:40:23 -0500

    Update Basic Data Structures/845_Longest_Mountain_in_Array.py

--- Basic Data Structures/845_Longest_Mountain_in_Array.py
+++ Basic Data Structures/845_Longest_Mountain_in_Array.py
@@ -1,4 +1,4 @@
-def longestMountain(A):
+def longestMountain(A):  # O(n) and O(2)
     """
     Traverse the list while you find a (local) peak
     Once you found it, go left and right: check while statement, they're some tricky conditions
@@ -19,7 +19,29 @@ def longestMountain(A):
     return res
 
 
+def longestMountain_alt(nums):
+    """
+    Same as above.
+    It's important not to drag l and r together with i but initialize them everytime when a peak is found.
+    """
+    # l, i, r = 0, 1, 2
+    i = 1
+    res = 0
+    while i < len(nums) - 1:
+        if nums[i - 1] < nums[i] > nums[i + 1]:
+            l, r = i - 1, i + 1
+            while l > 0 and nums[l - 1] < nums[l]:
+                l -= 1
+            while r < len(nums) - 1 and nums[r + 1] < nums[r]:
+                r += 1
+            # print(l, r)
+            res = max(res, r - l + 1)
+        i += 1
+    return res
+
+
 if __name__ == '__main__':
-    print(longestMountain([2, 1, 4, 7, 3, 2, 5]))
-    print(longestMountain([0, 1, 0]))
-    print(longestMountain([2, 0, 2, 0]))
+    # print(longestMountain([2, 1, 4, 7, 3, 2, 5]))
+    # print(longestMountain([0, 1, 0]))
+    # print(longestMountain([2, 0, 2, 0]))
+    print(longestMountain_alt([5770, 9921, 1853, 441, 6808, 8847, 9186, 3091, 1524, 2415]))
