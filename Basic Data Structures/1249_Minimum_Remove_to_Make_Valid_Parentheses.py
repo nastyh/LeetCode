72aa108efdec2d49a802c3118f34310dd3cb9a31
mkareev <mkareev@ebay.com>
2021-02-19 12:57:38 -0500

    Update 1249_Minimum_Remove_to_Make_Valid_Parentheses.py

--- Basic Data Structures/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
+++ Basic Data Structures/1249_Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -1,16 +1,32 @@
-def minRemoveToMakeValid_bad(s):
-    st_ix = []
-    for i in range(len(s)):
-        if s[i] == '(':
-            st_ix.append(i)
-        elif s[i] == ')':
-            if len(st_ix) == 0:
-                st_ix.append(i)
+def minRemoveToMakeValid_myself(s):  # O(n) both
+    """
+    We'll do two passes to collect indices of elements that we want to remove
+    First pass from left to right to catch unwanted closing parentheses
+    Second pass from right to left to catch unwanted opening parentheses
+    """
+    candidates, st_closing, st_opening, res = set(), [], [], ''
+    for k, v in enumerate(s):
+        if v == ')':
+            if len(st_closing) == 0:  # definitely a bad parenthesis
+                candidates.add(k)
             else:
-                st_ix.pop()
-        else:
-            continue
-    return st_ix
+                st_closing.pop()  # it means that this closing parenthesis pairs well with an opening parenthesis that happened earlier
+        if v == '(':
+            st_closing.append(v)  # just building our stack
+            
+    for i in range(len(s) - 1, -1, -1):
+        if s[i] == '(':
+            if len(st_opening) == 0:   # definitely a bad parenthesis
+                candidates.add(i)
+            else:
+                st_opening.pop()   # it means that this opening parenthesis pairs well with an closing parenthesis that happened earlier
+        if s[i] == ')':
+            st_opening.append(s[i])   # just building our stack
+            
+    for k, v in enumerate(s):
+        if k not in candidates:  # using set() for candidates allows us to make the program faster
+            res += v
+    return res
 
 
 def minRemoveToMakeValid(s):
@@ -31,6 +47,7 @@ def minRemoveToMakeValid(s):
         if i not in indexes_to_remove:
             string_builder.append(c)
     return "".join(string_builder)
+
 
 def minRemoveToMakeValid_alt(s):
     first_parse_chars = []
