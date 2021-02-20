1b7ca281a55181b040bb037b222d4db975605976
Michael Kareev <mkareev@umich.edu>
2021-02-20 13:18:15 -0500

    Update Basic Data Structures/013_Roman_to_Integers.py

--- Basic Data Structures/013_Roman_to_Integers.py
+++ Basic Data Structures/013_Roman_to_Integers.py
@@ -35,6 +35,7 @@ def romanToInt_alt(s):
             total += values[s[i]]
     return total
 
+
 def romanToInt_prev_curr(s): # easiest to follow
     d = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000}
     prev, curr, res = 0, 0, 0
@@ -51,7 +52,50 @@ def romanToInt_prev_curr(s): # easiest to follow
     return res
 
 
+def romanToInt_left_to_right(s):  # O(1) both b/c the dictionary is small and there is a finite number of roman numerals. Max number that can be written is 3999
+    """
+    Start iterating and every time if we have more or two digits to process left and the left digit < next digit:
+    add right, subtract left and make two steps for the pointer (b/c we processed two digits)
+    else: just add the number as it is and make one step
+    """
+    d = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    i = 0 
+    while i < len(s):
+        if i < len(s) - 1 and d[s[i]] < d[s[i + 1]]:
+            res = res + d[s[i + 1]] - d[s[i]]
+            i += 2
+        else:
+            res += d[s[i]]
+            i += 1
+    return res
+
+
+def romanToInt_right_to_left(s):
+    """
+    Same as above but right to left 
+    """
+    d = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C': 100, 'D': 500, 'M': 1000}
+    res = 0
+    i = len(s) - 1
+    while i >= 0:
+        if i > 0 and d[s[i]] > d[s[i - 1]]:  # while we have at least two elements to process left and this condition holds true
+            res = res + d[s[i]] - d[s[i - 1]]
+            i -= 2
+        else:
+            res += d[s[i]]
+            i -= 1
+    return res 
+
+
 if __name__ == '__main__':
     print(romanToInt('LVIII'))
     print(romanToInt_alt('LVIII'))
     print(romanToInt_prev_curr('IX'))
+    print(romanToInt_left_to_right('LVIII'))
+    print(romanToInt_left_to_right('LVII'))
+    print(romanToInt_left_to_right('IX'))
+    print(romanToInt_right_to_left('LVIII'))
+    print(romanToInt_right_to_left('LVII'))
+    print(romanToInt_right_to_left('IX'))
+    print(romanToInt_right_to_left("MCDLXXVI"))
