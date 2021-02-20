142cbef7568c0b9e4b6e7493df0a2d8abfe0b0af
Michael Kareev <mkareev@umich.edu>
2021-02-20 14:54:02 -0500

    Additions

--- Basic Data Structures/043_Multiply_Strings.py
+++ Basic Data Structures/043_Multiply_Strings.py
@@ -1,8 +1,54 @@
-class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        n1, n2 = [], []
-        for i in num1:
-            n1.append(int(i))
-        for j in num2:
-            n2.append(int(j))
-        return str(sum([v*10**k for k,v in enumerate(n1[::-1])]) * sum([v*10**k for k,v in enumerate(n2[::-1])]))
+def multiply(num1, num2):
+    n1, n2 = [], []
+    for i in num1:
+        n1.append(int(i))
+    for j in num2:
+        n2.append(int(j))
+    return str(sum([v*10**k for k, v in enumerate(n1[::-1])]) * sum([v*10**k for k, v in enumerate(n2[::-1])]))
+
+
+def multiply_ord(num1, num2):  # O(n) and O(1)
+    int_num1 = 0
+    for i in num1:
+        int_num1 = int_num1 * 10
+        int_num1 += (ord(i) - ord('0'))
+
+    int_num2 = 0
+    for i in num2:
+        int_num2 = int_num2 * 10
+        int_num2 += (ord(i) - ord('0'))
+    return str(int_num1 * int_num2)
+
+
+def multiply_one_by_one(num1, num2):
+    res = ''
+    carry = 0
+    if len(num1) < len(num2):
+        shorter = num1
+        longer  = num2
+    else:
+        shorter = num2xs
+        longer = num1
+    # i = len(longer) - 1
+    i = max(len(num1), len(num2)) - 1
+    while i != 0:
+        try:
+            digit1 = int(num1[i])
+        except IndexError:
+            digit1 = 1
+        try:
+            digit2 = int(num2[i])
+        except IndexError:
+            digit2 = 1 
+        prelim = digit1 * digit2 + carry
+        print(prelim)
+        res += str(prelim % 10)
+        carry = prelim // 10
+        i -= 1
+    if carry != 0:
+        res += str(carry)
+    return  res
+
+
+if __name__ == '__main__':
+    print(multiply_ord('2', '3'))
--- Basic Data Structures/448_Find_all_numbers_dissapeared_in_array.py
+++ Basic Data Structures/448_Find_all_numbers_dissapeared_in_array.py
@@ -1,10 +1,8 @@
 def findDisappearedNumbers(nums):
         # first solution
         # s = set(nums)
-        # return [i for i in range(1,len(nums)+1) if i not in s]
-
+        # return [i for i in range(1,len(nums)+1) if i not in s]=
         # second solution
-
         d = dict(zip(range(1, len(nums)+1),[0]*len(nums)))
         for num in nums:
             if num in d:
@@ -15,6 +13,23 @@ def findDisappearedNumbers_fast(nums):
     return [k for k, v in enumerate(nums, 1) if k not in nums]
 
 
+def findDisappearedNumbers_constant_space(nums):  # O(n) and O(1)
+    """
+    Need to go over the list and make numbers negative. 
+    Then go over the ideal list and check if any of such numbers in the original list are > 0. 
+    If so, we haven't seen such a number in nums, then add to res 
+    """
+    res = []
+    for k, v in enumerate(nums):
+        if nums[abs(v) - 1] >= 0:
+            nums[abs(v) - 1] *= -1
+    for i in range(1, len(nums) + 1):
+        if nums[i - 1] > 0:
+            res.append(i)
+    return res
+
+
 if __name__ == '__main__':
     # print(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
-    print(findDisappearedNumbers_fast([4,3,2,7,8,2,3,1]))
+    print(findDisappearedNumbers_fast([4, 3, 2, 7, 8, 2, 3, 1]))
+    print(findDisappearedNumbers_constant_space([4, 3, 2, 7, 8, 2, 3, 1]))
