a076dc669bc8a96c2abf162a63db094885c619ee
Michael Kareev <mkareev@umich.edu>
2021-02-26 08:38:13 -0500

    Update Trees/129_Sum_Root_to_Leaf_Numbers .py

--- Trees/129_Sum_Root_to_Leaf_Numbers .py
+++ Trees/129_Sum_Root_to_Leaf_Numbers .py
@@ -5,7 +5,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
-    def sumNumbers(self, root):
+    def sumNumbers(self, root):  # O(n) both
         ans = 0
         res = []
         def _helper(root, paths):
@@ -78,6 +78,51 @@ class TreeNode:
             if t.right:
                 q.append((t.right, curr_num))
         return res
+
+    
+    def sumNumbers_Morris_Preorder(self, root):  #  O(n) and O(1)
+        root_to_leaf = curr_number = 0
+        while root:  
+            # If there is a left child,
+            # then compute the predecessor.
+            # If there is no link predecessor.right = root --> set it.
+            # If there is a link predecessor.right = root --> break it.
+            if root.left: 
+                # Predecessor node is one step to the left 
+                # and then to the right till you can.
+                predecessor = root.left 
+                steps = 1
+                while predecessor.right and predecessor.right is not root: 
+                    predecessor = predecessor.right 
+                    steps += 1
+
+                # Set link predecessor.right = root
+                # and go to explore the left subtree
+                if predecessor.right is None:
+                    curr_number = curr_number * 10 + root.val                    
+                    predecessor.right = root  
+                    root = root.left  
+                # Break the link predecessor.right = root
+                # Once the link is broken, 
+                # it's time to change subtree and go to the right
+                else:
+                    # If you're on the leaf, update the sum
+                    if predecessor.left is None:
+                        root_to_leaf += curr_number
+                    # This part of tree is explored, backtrack
+                    for _ in range(steps):
+                        curr_number //= 10
+                    predecessor.right = None
+                    root = root.right 
+            # If there is no left child
+            # then just go right.        
+            else: 
+                curr_number = curr_number * 10 + root.val
+                # if you're on the leaf, update the sum
+                if root.right is None:
+                    root_to_leaf += curr_number
+                root = root.right            
+        return root_to_leaf
 
 
     def sumNumbers_DFS(self, root):  #  O(N), O(h)
