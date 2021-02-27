cd20625257a3b2bd835878808cc7acad9e77ba94
Michael Kareev <mkareev@umich.edu>
2021-02-27 16:09:11 -0500

    Update Basic Data Structures/239_Sliding_Window_Maximum.py

--- Basic Data Structures/239_Sliding_Window_Maximum.py
+++ Basic Data Structures/239_Sliding_Window_Maximum.py
@@ -43,6 +43,36 @@ def maxSlidingWindow_deque(nums, k):  # O(n) and O(n)
     return res
 
 
+def maxSlidingWindow_dp(nums, k):  # O(n) both
+    n = len(nums)
+    if n * k == 0:
+        return []
+    if k == 1:
+        return nums
+    left = [0] * n
+    left[0] = nums[0]
+    right = [0] * n
+    right[n - 1] = nums[n - 1]
+    for i in range(1, n):
+        # from left to right
+        if i % k == 0:
+            # block start
+            left[i] = nums[i]
+        else:
+            left[i] = max(left[i - 1], nums[i])
+        # from right to left
+        j = n - i - 1
+        if (j + 1) % k == 0:
+            # block end
+            right[j] = nums[j]
+        else:
+            right[j] = max(right[j + 1], nums[j])
+    output = []
+    for i in range(n - k + 1):
+        output.append(max(left[i + k - 1], right[i])) 
+    return output
+
+
 if __name__ == '__main__':
     # print(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
     # print(maxSlidingWindow_sliding([1, 3, -1, -3, 5, 3, 6, 7], 3))
