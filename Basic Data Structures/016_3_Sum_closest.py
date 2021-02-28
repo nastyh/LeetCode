07d260588622d9f4fcdd8c9c8aa31e52760387a7
Michael Kareev <mkareev@umich.edu>
2021-02-28 09:42:10 -0500

    Update Basic Data Structures/016_3_Sum_closest.py

--- Basic Data Structures/016_3_Sum_closest.py
+++ Basic Data Structures/016_3_Sum_closest.py
@@ -23,21 +23,21 @@ def threeSumClosest_alt(nums, target):
     diff = math.inf
     nums.sort()
     for i in range(len(nums) - 2):
-        p1=i + 1
-        p2=len(nums) - 1
+        p1 = i + 1
+        p2 = len(nums) - 1
         while p1 < p2:
-            s=nums[i] + nums[p1] + nums[p2]
+            s = nums[i] + nums[p1] + nums[p2]
             if abs(s - target) < diff:
                 diff = abs(s - target)
                 output = s
-            
             if s < target:
                 p1 += 1
             elif s > target:
                 p2 -= 1
             else:
                 return target
-    return output       
+    return output    
+       
 
 if __name__ == '__main__':
     print(threeSumClosest([-1, 2, 1, -4], 1))
