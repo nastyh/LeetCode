e55f9eedba8cf993d3b2391202730482f379282a
Michael Kareev <mkareev@umich.edu>
2021-03-02 07:56:54 -0500

    Update Basic Data Structures/032_Longest_Valid_Parentheses.py

---
+++ Basic Data Structures/032_Longest_Valid_Parentheses.py
@@ -0,0 +1,40 @@
+def longestValidParentheses(s):  # O(n) and O(1)
+    m = l = r = 0
+    for e in s:
+        if e == '(':
+            l += 1
+        else:
+            r += 1
+        if l==r:
+            m = max(m,r + l)
+        elif r > l:
+            l = r = 0
+    l = r = 0
+    for e in s[::-1]:
+        if e == ')':
+            r += 1
+        else: 
+            l += 1
+        if l == r: 
+            m = max(m,r + l)
+        elif l > r:
+            l = r = 0
+    return m
+
+
+def longestValidParentheses_dp(s):  # O(n) both 
+    n = len(s)
+    if n < 2:
+        return 0
+    # dp[i] = the length of the longest valid substring ending at s[i]
+    dp = [0 for _ in range(n)]
+    dp[1] = 2 if s[0:2] == '()' else 0
+    for i in range(2, n):
+        if s[i] == ')':
+            if s[i - 1] == '(':
+                dp[i] = dp[i - 2] + 2
+            else:
+                if i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == '(':
+                    prefix = dp[i - dp[i - 1] - 2] if i - dp[i - 1] - 2 >= 0 else 0
+                    dp[i] = prefix + dp[i-1] + 2
+    return max(dp)
