fa4626eec2a27b1e372a490af51fa4802e6360c7
Michael Kareev <mkareev@umich.edu>
2021-03-14 12:04:30 -0400

    Additions

--- Basic Data Structures/540_Single_Element_in_a_Sorted_Array.py
+++ Basic Data Structures/540_Single_Element_in_a_Sorted_Array.py
@@ -1,4 +1,14 @@
-def singleNonDuplicate(nums):
+def singleNonDuplicate(nums):  # O(logn) and O(1)
+    """
+    search on even indices only
+    The last index of an odd-lengthed array is always even, so we can set lo and hi to be the start and end of the array.
+    make sure our mid index is even. We can do this by dividing lo and hi in the usual way, but then decrementing it by 1 if it is odd.
+    This also ensures that if we have an even number of even indexes to search, that we are getting the lower middle 
+    Then we check whether or not the mid index is the same as the one after it.
+    If it is, then we know that mid is not the single element, and that the single element must be at an even index after mid.
+    Therefore, we set lo to be mid + 2. It is +2 rather than the usual +1 because we want it to point at an even index.
+    If it is not, then we know that the single element is either at mid, or at some index before mid. Therefore, we set hi to be mid.
+    """
     l, r = 0, len(nums) - 1
     while l < r:
         m = l + (r - l) // 2
@@ -17,6 +27,51 @@ def singleNonDuplicate(nums):
     return nums[l]
 
 
+def singleNonDuplicate_alt(nums):
+    """
+    Do edge checks first
+    Idea is that pairs always start at an even index and end at an odd index
+    """
+    if len(nums) == 1:
+        return nums[0]
+    l, r = 0, len(nums) - 1
+    if nums[l] != nums[l + 1]:
+        return nums[l]
+    if nums[r] != nums[r - 1]:
+        return nums[r]
+    while l < r:
+        m = l + (r - l) // 2
+        if nums[m] != nums[m - 1] and nums[m] != nums[m + 1]:
+            return nums[m]
+        if nums[m] == nums[m - 1]:
+            if m % 2 == 1:  # means we're looking at the second element of the pair and its index is odd. 
+                l = m + 1  # answer should be to the right, b/c to the left all pairs are good based on the definition of well-formed pairs
+            else:
+                r = m - 1  # pairs to the left are not well-formed, thus, look in the left part
+        if nums[m] == nums[m + 1]:
+            if m % 2 == 1:  # means that a pair starts at an odd index, it's against the definition of a well-formed pair
+                r = m - 1  # go to the left, b/c this is where something is wrong
+            else:
+                l = m + 1  # go to the right b/c everything to the right is good 
+    return nums[l]
+
+
+
+
+
+
+def singleNonDuplicate_xor(nums):  # O(N) and O(1)
+    """
+    0 ^ number is number
+    number ^ number is 0
+    Eventually we end with a situation 0 ^ non-repeating element and get this non-repeating element
+    """
+    res = 0
+    for num in nums:
+        res ^= num
+    return res
+
+
 if __name__ == '__main__':
-    print(singleNonDuplicate([1, 1, 2, 3, 3 ,4, 4, 8, 8]))
+    print(singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8]))
     print(singleNonDuplicate([3, 3, 7, 7, 10, 11, 11]))
--- Linked Lists/092_Reverse_Linked_List_II.py
+++ Linked Lists/092_Reverse_Linked_List_II.py
@@ -3,7 +3,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+
+    def reverseBetween(self, head, m, n):
         if not head:
             return
         zero = ListNode(-1)
@@ -19,3 +20,36 @@ class ListNode:
             next_n.next = prev.next
             prev.next = next_n
         return zero.next
+    
+    def reverseBetween_another(self, head, m, n):
+        if not head or not head.next: return head
+        if m == n: return head
+        # handle m == 1 case
+        counter = 0
+        curr = ListNode(0)
+        curr.next = head
+        head = curr
+        # traverse up to the (m-1)th node
+        while counter < m - 1 and curr:
+            counter += 1
+            curr = curr.next
+        # assign m-1th node
+        frontHeadNode = curr
+        curr = curr.next    #mth node
+        counter += 1
+        mthNode = curr
+        prev = endNode = None
+        # start from mth node up to nth node
+        while curr and counter <= n:    # do traditional reversal of LL
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+            if counter == n:    # when counter == n, grab the next value and break
+                endNode = curr if curr else None
+                break
+            counter += 1
+        # connect the reversed LL into original
+        frontHeadNode.next = prev
+        mthNode.next = endNode
+        return head.next
