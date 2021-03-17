7e81a9c87cf8fc8e4868473aaca03005ce1fb301
mkareev <mkareev@ebay.com>
2021-03-17 13:15:45 -0400

    Cleaning

--- Basic Data Structures/179_Largest_Number.py
+++ Basic Data Structures/179_Largest_Number.py
@@ -1,35 +1,35 @@
-def largestNumber(self, nums):
+def largestNumber(nums):
         if set(nums) == set([0]):
             return '0'
         n = map(str, nums)
-        for i in xrange(len(n)):
-            for j in xrange(i, len(n)):
+        for i in range(len(n)):
+            for j in range(i, len(n)):
                 if n[i] + n[j] < n[j] + n[i]:
                     n[i], n[j] = n[j], n[i]
         return ''.join(n)
 
 
-def largestNumber1(self, nums):
+def largestNumber1(nums):
     if not any(nums):
         return "0"
-    return "".join(sorted(map(str, nums), cmp=lambda n1, n2: -1 if n1+n2>n2+n1 else (1 if n1+n2<n2+n1 else 0)))
+    return "".join(sorted(map(str, nums), cmp=lambda n1, n2: -1 if n1 + n2 > n2 + n1 else (1 if n1 + n2 < n2 + n1 else 0)))
 
 # bubble sort
-def largestNumber2(self, nums):
-    for i in xrange(len(nums), 0, -1):
-        for j in xrange(i-1):
-            if not self.compare(nums[j], nums[j+1]):
-                nums[j], nums[j+1] = nums[j+1], nums[j]
+def largestNumber2(nums):
+    def compare(n1, n2):
+        return str(n1) + str(n2) > str(n2) + str(n1)
+    for i in range(len(nums), 0, -1):
+        for j in range(i - 1):
+            if not compare(nums[j], nums[j + 1]):
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
     return str(int("".join(map(str, nums))))
 
-def compare(self, n1, n2):
-    return str(n1) + str(n2) > str(n2) + str(n1)
 
 # selection sort
-def largestNumber3(self, nums):
-    for i in xrange(len(nums), 0, -1):
+def largestNumber3(nums):
+    for i in (len(nums), 0, -1):
         tmp = 0
-        for j in xrange(i):
+        for j in (i):
             if not self.compare(nums[j], nums[tmp]):
                 tmp = j
         nums[tmp], nums[i-1] = nums[i-1], nums[tmp]
@@ -37,7 +37,7 @@ def largestNumber3(self, nums):
 
 # insertion sort
 def largestNumber4(self, nums):
-    for i in xrange(len(nums)):
+    for i in range(len(nums)):
         pos, cur = i, nums[i]
         while pos > 0 and not self.compare(nums[pos-1], cur):
             nums[pos] = nums[pos-1]  # move one-step forward
@@ -46,24 +46,24 @@ def largestNumber4(self, nums):
     return str(int("".join(map(str, nums))))
 
 # merge sort
-def largestNumber5(self, nums):
-    nums = self.mergeSort(nums, 0, len(nums)-1)
+def largestNumber5(nums):
+    nums = mergeSort(nums, 0, len(nums)-1)
     return str(int("".join(map(str, nums))))
 
-def mergeSort(self, nums, l, r):
+def mergeSort(nums, l, r):
     if l > r:
         return
     if l == r:
         return [nums[l]]
-    mid = l + (r-l)//2
+    mid = l + (r - l) // 2
     left = self.mergeSort(nums, l, mid)
-    right = self.mergeSort(nums, mid+1, r)
+    right = self.mergeSort(nums, mid + 1, r)
     return self.merge(left, right)
 
-def merge(self, l1, l2):
+def merge(l1, l2):
     res, i, j = [], 0, 0
     while i < len(l1) and j < len(l2):
-        if not self.compare(l1[i], l2[j]):
+        if not compare(l1[i], l2[j]):
             res.append(l2[j])
             j += 1
         else:
@@ -73,23 +73,27 @@ def merge(self, l1, l2):
     return res
 
 # quick sort, in-place
-def largestNumber(self, nums):
-    self.quickSort(nums, 0, len(nums)-1)
+def largestNumber(nums):
+    def partition(nums, l, r):
+        low = l
+        while l < r:
+            if compare(nums[l], nums[r]):
+                nums[l], nums[low] = nums[low], nums[l]
+                low += 1
+            l += 1
+        nums[low], nums[r] = nums[r], nums[low]
+        return low
+
+    def quickSort(nums, l, r):
+        if l >= r:
+            return
+        pos = partition(nums, l, r)
+        quickSort(nums, l, pos - 1)
+        quickSort(nums, pos + 1, r)
+
+    quickSort(nums, 0, len(nums)-1)
     return str(int("".join(map(str, nums))))
 
-def quickSort(self, nums, l, r):
-    if l >= r:
-        return
-    pos = self.partition(nums, l, r)
-    self.quickSort(nums, l, pos-1)
-    self.quickSort(nums, pos+1, r)
 
-def partition(self, nums, l, r):
-    low = l
-    while l < r:
-        if self.compare(nums[l], nums[r]):
-            nums[l], nums[low] = nums[low], nums[l]
-            low += 1
-        l += 1
-    nums[low], nums[r] = nums[r], nums[low]
-    return low
+
+
--- Trees/222_Count_Complete_Tree_Nodes.py
+++ Trees/222_Count_Complete_Tree_Nodes.py
@@ -6,19 +6,103 @@ class TreeNode:
         self.left = left
         self.right = right
 
-    def countNodes(self, root):
-        q, res = deque(), []
+    def countNodes_bfs(self, root):  # O(n) and O(1)
+        if not root: return 0
+        if root and not root.left and not root.right: return 1
+        d = deque()
+        res = 0
+        d.append(root)
+        res += 1
+        while d:
+            t = d.popleft()
+            if t.left:
+                d.append(t.left)
+                res += 1
+            if t.right:
+                d.append(t.right)
+                res += 1
+        return res
+
+    
+    def countNodes_dfs(self, root):  #O(n) and O(d), where d is the depth of the tree, in this case it's logn. So space is O(logn)
+        if not root: return 0
+        if root and not root.left and not root.right: return 1
+        res = 0
+        def _helper(node):
+            nonlocal res
+            if not node: return
+            _helper(node.left)
+            res += 1
+            _helper(node.right)
+        _helper(root)
+        return res
+
+
+    def countNodes_log(self, root):  # O(logn) for tree traversal and O(logn) for searching the last row. Overall, O(log^2(n)). Space is O(1)
+        """
+        The height of the tree is h; therefore, there are 2^h nodes in the last row of the tree.
+        start by checking the middle of the last row, which is 2^(h-1). If it was present, then check the middle element of the right side,
+        and otherwise check the middle element of the left side, and continue this trend until we reach a position where the difference between
+        the position of a present element and a null pointer is just one.
+        The mid variable gets a value of 0 to 2^h. The binary representation is checked bit by bit from the most significant digit to the least.
+        If it is a one, we go to the right child, and if it is zero, we go to the left child. This way, a single number represents the path we go
+        to reach the last row of the tree. For example, for a tree with a depth of 3, there are 8 nodes in the last row. The number 4 for tree traversal is
+        100, which means root goes to the right child, then left child, and then again the left child to reach the last row.
+        """
+        height = 0
+        node = root
+        while node:
+            node = node.left
+            height += 1
+        lastLevel = int(pow(2, height - 1))
+        lastPresent = 0
+        lastNotPresent = lastLevel
+        while (lastNotPresent - lastPresent > 1):
+            mid = (lastNotPresent + lastPresent) // 2
+            bit = lastLevel >> 1
+            node = root
+            while (bit > 0):
+                if (mid & bit):
+                    node = node.right
+                else:
+                    node = node.left
+                bit >>= 1
+            if (node):
+                lastPresent = mid
+            else:
+                lastNotPresent = mid
+        return lastLevel + lastPresent
+
+
+    def countNodes_nlogn(self, root):  # O(nlogn) both
+        """
+        Number of Nodes = 2 ** (level number) - 1 + final level nodes number
+        """
         if not root:
             return 0
-        q.append(root)
-        while q:
-            t = q.popleft()
-            res.append(t.val)
-            if t.left:
-                q.append(t.left)
-            if t.right:
-                q.append(t.right)
-        return res
+        self.last_level_count = 0
+        self.level = 0
+        # Stop flag to end searching
+        self.stop = 0
+        def dfs(node, level):
+            if self.stop:
+                return
+            if not node:
+                self.level = max(self.level, level)
+                # If the reaches the final level, then count 1
+                # Note this is actually counting for # of children (None) of final level nodes
+                if self.level == level:
+                    self.last_level_count += 1
+                else:
+                    self.stop = 1
+                return
+            self.dfs(node.left, level + 1)
+            self.dfs(node.right, level + 1)
+        # Start with -1 since the searching ends on the children of leafs (None)
+        # Make -1 offset then self.level can be the real value
+        dfs(root, -1)
+        return 2 ** self.level - 1 + self.last_level_count // 2
+        
 
 
 
@@ -30,6 +114,6 @@ if __name__ == '__main__':
     l.left.right = TreeNode(5)
     l.right.left = TreeNode(6)
     # l.right.right = TreeNode(14)
-    print(l.countNodes(l))
+    print(l.countNodes_bfs(l))
 
 
