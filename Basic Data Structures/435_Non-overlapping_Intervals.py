b7b716a01f2eae07c439a27c2abc808b736a2aa9
mkareev <mkareev@ebay.com>
2021-03-18 10:43:10 -0400

    Update 435_Non-overlapping_Intervals.py

--- Basic Data Structures/435_Non-overlapping_Intervals.py
+++ Basic Data Structures/435_Non-overlapping_Intervals.py
@@ -28,6 +28,33 @@ def eraseOverlapIntervals_alt(intervals):
     return result
 
 
+def eraseOverlapIntervals_2_pointers(intervals):  # o(nlogn) and O(1)
+    """
+    Sort by the start time
+    Two pointers: l and r intervals
+    Start comparing them
+    If the end of the left is <= start of the right, there is no overlap, move both pointers
+    if the end of the left is <= end of the right: there is an overlap (b/c we're using elif, the previous case has been taken care of),
+    then let's greedily remove the right interval, increment res and move the r pointer
+    if the end of the left is > end of the right, it means that the left is larger. We'll remove it, and increment the pointers accordingly 
+    """
+    intervals.sort()
+    res = 0
+    l, r = 0, 1 
+    while r < len(intervals):
+        if intervals[l][1] <= intervals[r][0]:
+            l = r 
+            r += 1
+        elif intervals[l][1] <= intervals[r][1]:
+            res += 1
+            r += 1
+        elif intervals[l][1] > intervals[r][1]:
+            res += 1
+            l = r
+            r += 1
+    return res
+
+
 if __name__ == '__main__':
     print(eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]))
     print(eraseOverlapIntervals([[1,2],[1,2],[1,2]]))
