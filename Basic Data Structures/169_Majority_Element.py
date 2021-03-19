783485ee59ea6325ec955b800629354f259b74b8
mkareev <mkareev@ebay.com>
2021-03-19 15:39:28 -0400

    Update 169_Majority_Element.py

--- Basic Data Structures/169_Majority_Element.py
+++ Basic Data Structures/169_Majority_Element.py
@@ -16,9 +16,11 @@ def majorityElement_sort(nums):  # O(nlogn) and O(1)
 
 def majorityElement_Boyer_Moore(nums):  # O(n) and O(1)
     """
-    ook for a suffix sufsuf of nums where suf[0]suf[0] is the majority element in that suffix.
-    To do this, we maintain a count, which is incremented whenever we see an instance of our current candidate for majority element and decremented whenever we see anything else.
-    Whenever count equals 0, we effectively forget about everything in nums up to the current index and consider the current number as the candidate for majority element. 
+    Look for a suffix sufsuf of nums where suf[0]suf[0] is the majority element in that suffix.
+    To do this, we maintain a count, which is incremented whenever we see an instance of our current candidate for majority element and decremented
+    whenever we see anything else.
+    Whenever count equals 0, we effectively forget about everything in nums up to the current index and consider the current number as the candidate
+    for majority element. 
     """
     count = 0
     candidate = None
