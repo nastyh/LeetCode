374791c939f9d0a65fee7d299a5b6e71df623450
Michael Kareev <mkareev@umich.edu>
2021-03-20 17:25:34 -0400

    Update Basic Data Structures/157_Read_N_Characters_Given_Read_4.py

--- Basic Data Structures/157_Read_N_Characters_Given_Read_4.py
+++ Basic Data Structures/157_Read_N_Characters_Given_Read_4.py
@@ -1,4 +1,10 @@
 def read(buf, n):  # O(n) to copy N characters and O(1) to keep the buffer of 4 elements
+    """
+    At any point buf4, will maximum of 4 characters or count returned from the read4
+    Can read utmost 4 chars at a time, so create an array of size 4 and pass it to the function read4(buf4). 
+    That read4 function will fill buf4 and returns the count how many characters it filled.
+    take those buf4 chars filled and append it to my final buf.
+    """
     i = 0
     while i < n: 
         buf4 = ['','','','']
