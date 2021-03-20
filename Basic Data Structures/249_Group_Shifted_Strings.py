70bc9f17665b99dfa63de64170b410415425ea90
Michael Kareev <mkareev@umich.edu>
2021-03-20 18:46:45 -0400

    Update Basic Data Structures/249_Group_Shifted_Strings.py

--- Basic Data Structures/249_Group_Shifted_Strings.py
+++ Basic Data Structures/249_Group_Shifted_Strings.py
@@ -1,16 +1,16 @@
 from collections import defaultdict
-def groupStrings(strings):  # O(N) both
+def groupStrings(strings):  # O(ab) where a is the number of strings and b is the length of the longest string. O(n)
     """
-    ## 1. Intuition is: there will be some relative thing in common for all those strings. Ahhh yes, difference in ascii values
+    ## 1. Intuition is: there will be some relative thing in common for all those strings: difference in ascii values
     ## 2. Store ascii value pairs in hashmap and group together.
     ## 3. If the diff in ascii become -ve then add 26
-    ## Watchout : case : acz (2, 23), dfc (2, -3) ==> (2, -3+26 = 23) => can be clubbed together.
+    ## Watchout : case : acz (2, 23), dfc (2, -3) ==> (2, -3 + 26 = 23) => can be grouped together.
     """
     groups = defaultdict(list)
     for s in strings:
         pattern = ()
-        for i in range(1,len(s)):
-            pattern = pattern + ((ord(s[i]) - ord(s[i-1]) + 26 ) % 26,)
+        for i in range(1, len(s)):
+            pattern = pattern + ((ord(s[i]) - ord(s[i - 1]) + 26 ) % 26,)
         groups[pattern].append(s)
     return groups.values()
 
@@ -20,7 +20,7 @@ def groupStrings_another(strings):
 	for s in strings:
 		key = ()
 		for i in range(len(s) - 1):
-			circular_difference = 26 + ord(s[i+1]) - ord(s[i])
+			circular_difference = 26 + ord(s[i + 1]) - ord(s[i])
 			key += (circular_difference % 26,)
 		hashmap[key] = hashmap.get(key, []) + [s]
 	return list(hashmap.values())
