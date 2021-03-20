6e9be6152df7a18551658a9b1076ab70f7493e34
Michael Kareev <mkareev@umich.edu>
2021-03-20 17:22:42 -0400

    Update Basic Data Structures/830_Positions_of_Large_Groups.py

--- Basic Data Structures/830_Positions_of_Large_Groups.py
+++ Basic Data Structures/830_Positions_of_Large_Groups.py
@@ -1,12 +1,15 @@
-def largeGroupPositions(s):
+def largeGroupPositions(s):  # O(n) and O(1) (not incl. space for the answer)
+    """
+    Move r while it matches what is at l. 
+    Once the size is large enough, append it to res 
+    """
     res = []
-    l, r = 0, 0
+    l, r = 0, 1
     while r < len(s):
         while r < len(s) and s[l] == s[r]:
             r += 1 
         if r - l >= 3:
-            res.append([l, r - 1])
-        l = r 
+            res.append([l, r - 1])  # be accurate here with r - 1
         r += 1
     return res
     
