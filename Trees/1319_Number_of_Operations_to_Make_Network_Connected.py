a0e71eb70bc0ee6b201690019f1ecba5bcfce340
Michael Kareev <mkareev@umich.edu>
2021-03-27 13:33:09 -0400

    Update Trees/1319_Number_of_Operations_to_Make_Network_Connected.py

--- Trees/1319_Number_of_Operations_to_Make_Network_Connected.py
+++ Trees/1319_Number_of_Operations_to_Make_Network_Connected.py
@@ -26,3 +26,44 @@ def makeConnected(n, connections):  # O(V + E) both
             _helper(i, visited)
             res += 1
     return res - 1
+
+
+def makeConnected_union_find(n, connections):
+    parent = list(range(n))
+    count = n
+    redundant = 0
+    def find(x):
+        if x != parent[x]:
+            parent[x] = find(parent[x])
+        return parent[x]
+
+    def union(x, y):
+        nonlocal count
+        nonlocal redundant
+        rootx = find(x)
+        rooty = find(y)
+        if rootx != rooty:
+            parent[rootx] = rooty
+            count -= 1
+        else:
+            redundant += 1
+
+    for x, y in connections:
+        union(x, y)
+    if redundant >= count - 1:
+        return count - 1
+    else:
+        return -1
+
+if __name__ == '__main__':
+    print(makeConnected(4, [[0, 1], [0, 2], [1, 2]]))
+    print(makeConnected(6, [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]))
+    print(makeConnected(6, [[0, 1], [0, 2],[1, 2], [0, 3]]))
+    print(makeConnected(5, [[0, 1], [0, 2],[3, 4], [2, 3]]))
+    print(makeConnected_union_find(4, [[0, 1], [0, 2], [1, 2]]))
+    print(makeConnected_union_find(6, [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3]]))
+    print(makeConnected_union_find(6, [[0, 1], [0, 2],[1, 2], [0, 3]]))
+    print(makeConnected_union_find(5, [[0, 1], [0, 2],[3, 4], [2, 3]]))
+    
+    
+
