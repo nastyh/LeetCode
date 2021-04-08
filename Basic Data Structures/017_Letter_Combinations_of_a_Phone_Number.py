f2fa6bbd1b0631a9a7e8293326c13536b9042d90
Michael Kareev <mkareev@umich.edu>
2021-04-08 10:41:03 -0400

    Update Basic Data Structures/017_Letter_Combinations_of_a_Phone_Number.py

--- Basic Data Structures/017_Letter_Combinations_of_a_Phone_Number.py
+++ Basic Data Structures/017_Letter_Combinations_of_a_Phone_Number.py
@@ -1,5 +1,34 @@
 from itertools import product
 from collections import Counter
+
+def letterCombinations_recur(digits):
+    """
+    Recursive approach
+    First, write down a condition when curr_res should be appended to res
+    Second, the main part starts
+    We need to take every digit in digits, and every letter associated with this digits in the dictionary
+    """
+    d = {"2" : ["a" , "b" , "c"],
+            "3" : ["d" , "e" , "f"],
+            "4" : ["g" , "h" , "i"],
+            "5" : ["j" , "k" , "l"],
+            "6" : ["m" , "n" , "o"],
+            "7" : ["p" , "q" , "r" , "s"],
+            "8" : ["t" , "u" , "v"],
+            "9" : ["w" , "x" , "y" , "z"]}
+    if len(digits) == 0: return ''
+    res = []
+    def _recur(digits, d, curr_res, curr_ix):
+        if len(curr_res) == len(digits):
+            res.append(curr_res)
+            return
+        for i in range(curr_ix, len(digits)):
+            for ch in d[digits[i]]:
+                _recur(digits, d, curr_res + ch, i + 1)  # need to do curr_res + ch inside _recur. If you do outside, won't work
+    _recur(digits, d, '', 0)
+    return res
+
+
 def letterCombinations(digits):
     res, l = '', []
     d = {'2': 'abc',
@@ -11,7 +40,6 @@ def letterCombinations(digits):
          '8': 'tuv',
          '9': 'wxyz'
          }
-
     possible = [d[x] for x in digits]
     return [''.join(x) for x in product(*possible) if x]
     
@@ -61,6 +89,9 @@ def letterCombinations_iter(digits):
         
 
 def letterCombinations_another(digits):  # O(3^N * 4^M) both, where N is the number of digits in the input that maps to 3 letters, and M -- maps to 4 letters
+    """
+    This is backtracking b/c we remove an element from curr
+    """
     d = {'2': 'abc',
          '3': 'def',
          '4': 'ghi',
@@ -71,7 +102,6 @@ def letterCombinations_another(digits):  # O(3^N * 4^M) both, where N is the num
          '9': 'wxyz'
          }
     if len(digits) == 0: return []
-    if len(digits) == 1: return [digits[0]]
     def _helper(digits, d, ix, curr, res):
         if ix == len(digits):
             res.append(curr)
