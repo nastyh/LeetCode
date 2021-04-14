13ed91606ad09bc32ebcb34c7e90db2185f96c1a
mkareev <mkareev@ebay.com>
2021-04-14 11:44:15 -0400

    Update 1700_Number_of_Students_Unable_to_Eat_Lunch.py

--- Basic Data Structures/1700_Number_of_Students_Unable_to_Eat_Lunch.py
+++ Basic Data Structures/1700_Number_of_Students_Unable_to_Eat_Lunch.py
@@ -1,4 +1,26 @@
 from collections import Counter
+def countStudents_optimal(students, sandwiches):  # O(n) and O(1)
+    """
+    Key here is how to define a loop and what to return at the end
+    Because we remove elements from students, it will be one of the stopping conditions. 
+    Another stopping condition is mismatch != len(students). It's for a case when we're stuck and can longer find any pairs.
+    If sandwiches and students match, it's all good: remove the respective pair and nullify mismatch
+    If they don't match, move student to the end and increment mismatch 
+    """
+    mismatch = 0
+    while len(students) > 0 and mismatch != len(students):
+        if students[0] == sandwiches[0]:
+            students.pop(0)
+            sandwiches.pop(0)
+            mismatch = 0
+        else:
+            student_to_move = students[0]
+            students.pop(0)
+            students.append(student_to_move)
+            mismatch += 1
+    return len(students)
+
+
 def countStudents(students, sandwiches):     
         students = Counter(students)
         total_sandwiches = len(sandwiches)
@@ -17,3 +39,10 @@ def countStudents(students, sandwiches):
                 i += 1
         return 0
 
+def countStudents_short(students, sandwiches):
+    counts = [len(students) - sum(students), sum(students)]        
+    for s in sandwiches:
+        if not counts[s]:
+            return sum(counts)
+        counts[s] -= 1            
+    return 0
