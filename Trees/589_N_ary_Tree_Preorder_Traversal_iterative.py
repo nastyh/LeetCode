8ce4f0f247d519db1cf91ad106692b7c8544c606
Michael Kareev <mkareev@umich.edu>
2021-04-20 10:13:24 -0400

    Update Trees/589_N_ary_Tree_Preorder_Traversal_iterative.py

--- Trees/589_N_ary_Tree_Preorder_Traversal_iterative.py
+++ Trees/589_N_ary_Tree_Preorder_Traversal_iterative.py
@@ -1,13 +1,9 @@
-"""
-# Definition for a Node.
 class Node:
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children
-"""
 
-class Solution:
-    def preorder(self, root: 'Node') -> List[int]:
+    def preorder(self, root):  # O(N) both 
         res, s = [], []
         if not root:
             return None
@@ -16,6 +12,35 @@ class Solution:
             t = s.pop()
             if t:
                 res.append(t.val)
-                for i in range(len(t.children)-1, -1, -1):
+                for i in range(len(t.children) -1, -1, -1):
                     s.append(t.children[i])
+        return res
+
+
+    def preorder_iter_clean(self, root):
+        """
+        Put the root into the stack. Pop. Put its children in an opposite order b/c we need to traverse from the bottom
+        """
+        st, res = [], []
+        if not root: return
+        st.append(root)
+        while st:
+            t = st.pop()
+            res.append(t.val)
+            for child in t.children[::-1]:
+                st.append(child)
+        return res 
+        
+    
+    def preorder_recur(self, root):
+        """
+        Traditional recursive pre-order but just treat children in a for loop
+        """
+        res = []
+        def _helper(node):
+            if not node: return
+            res.append(node.val)
+            for child in node.children:
+                _helper(child)        
+        _helper(root)
         return res
