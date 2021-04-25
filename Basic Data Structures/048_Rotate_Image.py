05fe7bf73029f10f1682f660941915899e4afc14
Michael Kareev <mkareev@umich.edu>
2021-04-25 10:22:11 -0400

    Update Basic Data Structures/048_Rotate_Image.py

--- Basic Data Structures/048_Rotate_Image.py
+++ Basic Data Structures/048_Rotate_Image.py
@@ -20,9 +20,29 @@ def rotate_recur(matrix):
             tmp = matrix[start + length - 1 - i][start]
             matrix[start + length - 1 - i][start] = matrix[start + length - 1][start + length - 1 - i]
             matrix[start + length - 1][start + length - 1 - i] = matrix[start + i][start + length - 1]
-            matrix[start + i][start + length - 1] = matrix[start][start+i]                
+            matrix[start + i][start + length - 1] = matrix[start][start + i]                
             matrix[start][start + i] = tmp
         # process inner level
         rotate_recu(matrix, start + 1, length - 2)
     
     rotate_recu(matrix, start, length)
+
+
+def rotate_short(matrix):
+    matrix[:] = list(zip(*matrix[::-1]))
+
+
+def rotate_another(matrix):
+    n = len(matrix[0])
+    for i in range(n // 2 + n % 2):
+        for j in range(n // 2):
+            tmp = [0] * 4
+            row, col = i, j
+            for k in range(4):
+                tmp[k] = matrix[row][col]
+                row, col = col, n - 1 - row
+            # rotate 4 elements   
+            for k in range(4):
+                matrix[row][col] = tmp[(k - 1) % 4]
+                row, col = col, n - 1 - row
+    return matrix
