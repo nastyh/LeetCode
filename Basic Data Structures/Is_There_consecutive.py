ab204e0577792bd7d01105997e3f7887ad6535e6
Michael Kareev <mkareev@umich.edu>
2021-04-26 21:08:15 -0400

    Update Basic Data Structures/Is_There_consecutive.py

--- Basic Data Structures/Is_There_consecutive.py
+++ Basic Data Structures/Is_There_consecutive.py
@@ -4,7 +4,7 @@ Return True if the number n is in nums exactly times times in a row.
 False otherwise
 """
 from collections import Counter
-def is_there_consetuve(nums, n, times):
+def is_there_consecutive(nums, n, times):
     res = 0
     for num in nums:
         if num == n:
@@ -16,8 +16,8 @@ def is_there_consetuve(nums, n, times):
         
 
 if __name__ == '__main__':
-    print(is_there_consetuve([1, 3, 5, 5, 3, 3, 1], 3, 2))  # True
-    print(is_there_consetuve([1, 2, 3, 4, 5], 1, 1))  # True
-    print(is_there_consetuve([3], 1, 0))  # True
-    print(is_there_consetuve([2, 2, 3, 2, 2, 2, 2, 3, 4, 1, 5], 3, 2))  # False b/c there are no two 3s in a row
-    print(is_there_consetuve([5, 5, 5, 5, 5], 5, 7))  # False 
+    print(is_there_consecutive([1, 3, 5, 5, 3, 3, 1], 3, 2))  # True
+    print(is_there_consecutive([1, 2, 3, 4, 5], 1, 1))  # True
+    print(is_there_consecutive([3], 1, 0))  # True
+    print(is_there_consecutive([2, 2, 3, 2, 2, 2, 2, 3, 4, 1, 5], 3, 2))  # False b/c there are no two 3s in a row
+    print(is_there_consecutive([5, 5, 5, 5, 5], 5, 7))  # False 
