0d29296900d94f7becd5248797ea0ef1cb810a67
mkareev <mkareev@ebay.com>
2021-05-06 12:36:33 -0400

    Update 109_Convert_Sorted_Linked_List_to_BST.py

--- Linked Lists/109_Convert_Sorted_Linked_List_to_BST.py
+++ Linked Lists/109_Convert_Sorted_Linked_List_to_BST.py
@@ -9,16 +9,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def sortedListToBST(self, head: ListNode) -> TreeNode:
+
+    def sortedListToBST(self, head):  # O(N) both
         nums = []     # first create a normal list from the linked list
         curr = head
         while curr:
             nums.append(curr.val)
             curr = curr.next
-
 # the list is sorted, the median element is the root. Everything to the left: left tree. Everything to the right: right tree
-
         def _treeBuild(l):
             if not l:
                 return None
@@ -27,8 +25,27 @@ class Solution:
             node.left = _treeBuild(l[:m_ix])
             node.right = _treeBuild(l[m_ix+1:])
             return node
-
-
-
         return _treeBuild(nums)
 
+
+    def sortedListToBST_another(self, head):  # O(NlogN) and O(logN)
+        """
+        Slow and fast pointer technique.
+        We pass a list and need to find its middle. Every time  the list's length is cut in 2. 
+        A recursion stack creates O(logN) space complexity. 
+        """
+        def _middle(head):
+            prev, slow, fast = None, head, head
+            while fast and fast.next:
+                prev = slow
+                slow = slow.next
+                fast = fast.next.next 
+            prev.next = None
+            return slow
+        if not head: return None
+        if not head.next: return TreeNode(head.val)
+        mid = _middle(head)
+        res = TreeNode(mid.val) 
+        res.left = self.sortedListToBST_another(head)
+        res.right = self.sortedListToBST_another(mid.next)
+        return res
