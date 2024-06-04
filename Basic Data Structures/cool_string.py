0f467f0dcbea76eb6c83785e4f76f43ca242ee6b
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-06-03 20:06:13 -0400

    Update cool_string.py

--- Basic Data Structures/cool_string.py
+++ Basic Data Structures/cool_string.py
@@ -9,18 +9,18 @@ class Solution:
         n = []
         for v in d.values():
             n.append(v)
-        n.sort()
+        n.sort() # going up
         if len(n) < 2:
             res = True   
         elif len(n) == 2: # if the length of list is 2 and the 1st value in the is 1 then the result will be true 
             if n[0] == 1:
                 res = True
-            if n[1] - n[0] == 1:
+            if n[1] - n[0] == 1:   #if the length of list is 2 and the difference between between the 2nd and the 1st value(n[1]-n[0]) is 0 then the result will be true
                 res = True
         else:
-            if n[0] == 1 and n[1] == n[-1]:
+            if n[0] == 1 and n[1] == n[-1]:   #if the list size is greater than 2, and sortingthe 1st value is 1(n[0]==1) and the remaining values in the list are equal then the result will be true
                 res = True 
-            if n[0] == n[-2] and n[-1]-n[-2] == 1:
+            if n[0] == n[-2] and n[-1]-n[-2] == 1:  #if the list size is greater than 2, and if all the values in the list are equal and the difference between last 2 values(n[-1]-n[-2]) is 1 then the result is 1`
                 res = True
         return res
 
