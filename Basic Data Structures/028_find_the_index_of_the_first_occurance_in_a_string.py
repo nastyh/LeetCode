511088a634092bb9842141f87f04c1a13c9b8103
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-06-16 13:28:17 -0400

    Update 028_find_the_index_of_the_first_occurance_in_a_string.py
    more solutions

--- Basic Data Structures/028_find_the_index_of_the_first_occurance_in_a_string.py
+++ Basic Data Structures/028_find_the_index_of_the_first_occurance_in_a_string.py
@@ -1,4 +1,26 @@
 class Solution:
+    def strStr_mine(self, haystack: str, needle: str) -> int:
+        """
+        h_ix is the left index for haystack
+        curr_ix expands from h_ix to the length of needle
+        so we have a window of the size needs at the end
+        we restart curr_ix to zero b/c it's relative to h_ix, and it is used for going over
+        needle again from scratch once we restart the process
+        """
+        h_ix, curr_ix = 0, 0
+        while h_ix < len(haystack):
+            if haystack[h_ix] == needle[curr_ix]:
+                # if the latter match, keep looking by extending the right side
+                curr_ix += 1 # move through needle
+                # we are of the size of needle
+                if curr_ix == len(needle):
+                    return h_ix - curr_ix + 1
+            else:
+                h_ix -= curr_ix # curr ix is never larger than needle 
+                curr_ix = 0
+            h_ix += 1 # move through heystack
+        return -1
+        
     def strStr(self, haystack: str, needle: str) -> int: # O((N - M + 1) * M) and O(1)
         """
         two pointers
@@ -23,4 +45,7 @@ class Solution:
                 r = 0  # Reset r
             l += 1 # keep moving the loop
         return -1
+
+    def strStr_another_pythonic(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
         
