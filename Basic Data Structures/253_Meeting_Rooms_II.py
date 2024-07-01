1f5a9f7803aa361708bd9e0bbfc194566bf56eae
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-07-01 18:45:28 -0400

    Update 253_Meeting_Rooms_II.py
    one more w/ explanations

--- Basic Data Structures/253_Meeting_Rooms_II.py
+++ Basic Data Structures/253_Meeting_Rooms_II.py
@@ -30,6 +30,23 @@ def minMeetingRooms_heap(intervals):  # O(nlogn) b/c of sorting and O(n) to cons
         res = max(res, len(heap))
     return res
 
+def minMeetingRooms_explained(self, intervals: List[List[int]]) -> int:
+        """
+        O(nlogn) b/c of sorting and O(n) to construct a min heap that will contain the whole list in the worst case
+        maintain a min heap with end times as a key. The top element is the room that will be free the earliest.
+        len(heap) shows how many meetings are taking place. This is how many rooms we need
+        """
+        if len(intervals) == 1: return 1
+        intervals.sort()  # sort by the start time
+        h, res = [], 0
+        for interval in intervals:
+            while h and h[0] <= interval[0]: # incoming meeting ends earlier than what we have in the heap so far
+                heapq.heappop(h)
+            #maintain a in heap. On top is the meeting that ends the earliest
+            heapq.heappush(h, interval[1])
+            res = max(res, len(h))
+        return res
+
 
 if __name__ == '__main__':
     print(minMeetingRooms([[9, 10], [4, 9], [4, 17]]))
