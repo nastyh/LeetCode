5da00675b4da2ce2bd9721426eb34f714fdb2a43
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-07-19 12:09:02 -0400

    Update 445_Add_Two_Numbers_II.py
    stack added

--- Linked Lists/445_Add_Two_Numbers_II.py
+++ Linked Lists/445_Add_Two_Numbers_II.py
@@ -90,3 +90,44 @@ def addTwoNumbers_deque(self, l1, l2):
     else:
         return cur
         
+def addTwoNumbers_stack(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        extra q: without reversing explicitly (only the answer)
+        O(max(m, n))  -- the longer of the two lists
+        O(m + n) -- due to the stack
+        """
+        def _helper_reverse_result(node):
+            prev = None
+            curr = node
+            while curr:
+                next_n = curr.next
+                curr.next = prev 
+                prev = curr
+                curr = next_n
+            return prev 
+       
+        s1 = []
+        s2 = []
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+        
+        carry = 0
+        dummy = ListNode(-1)
+        curr = dummy
+        while s1 or s2 or carry:
+            v1 = s1.pop() if s1 else 0
+            v2 = s2.pop() if s2 else 0
+            curr_val = (v1 + v2 + carry)
+            carry = curr_val // 10
+            curr_val = curr_val % 10
+            curr.next = ListNode(curr_val)
+            curr = curr.next 
+        if carry:
+            curr.next = ListNode(1)
+        # return dummy.next
+        return _helper_reverse_result(dummy.next)
+        
