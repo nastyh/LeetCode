12ab0e1436348ec59e57d6c96abf000885724900
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-11-08 13:32:16 -0500

    Add files via upload

--- Basic Data Structures/1249_minimum_remove_to_make_valid_parentheses.py
+++ Basic Data Structures/1249_minimum_remove_to_make_valid_parentheses.py
@@ -31,3 +31,32 @@ class Solution:
             if k not in candidates:
                 res += v
         return res
+
+    def minRemoveToMakeValid_no_stack(self, s: str) -> str:
+        """
+        Initialize pointers for the start and end of the string, variables to
+        store the start and end parts of the string, and the final result
+        Iterate through the string from left to right. Track the count of open parentheses encountered.
+        If an excess closing parenthesis is encountered (i.e., a closing parenthesis with no matching opening parenthesis),
+        mark it with '*'. Keep track of the count of open parentheses encountered.
+        Filter out the marked characters ('*') from the character array, leaving only the valid parentheses.
+        Construct the result string from the filtered character array.
+        Return the final result string, which contains the minimum number of parentheses required to make the input string valid.
+        O(n) both
+        """
+        openParenthesesCount = 0
+        arr = list(s)
+        for i in range(len(arr)):
+            if arr[i] == '(':
+                openParenthesesCount += 1
+            elif arr[i] == ')':
+                if openParenthesesCount == 0:
+                    arr[i] = '*' # Mark excess closing parentheses
+                else:
+                    openParenthesesCount -= 1
+        for i in range(len(arr) - 1, -1, -1):
+            if openParenthesesCount > 0 and arr[i] == '(':
+                arr[i] = '*' # Mark excess opening parentheses
+                openParenthesesCount -= 1
+        result = ''.join(c for c in arr if c != '*')
+        return result
