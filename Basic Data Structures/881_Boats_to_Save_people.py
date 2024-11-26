f9a7f6fb2a3b1d934b37b1c38604db55c354f079
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-11-26 11:26:54 -0500

    Update 881_Boats_to_Save_people.py
    added an explanation

--- Basic Data Structures/881_Boats_to_Save_people.py
+++ Basic Data Structures/881_Boats_to_Save_people.py
@@ -1,10 +1,16 @@
 def numRescueBoats(people, limit):
+    """
+    Sorting allows to save the lightest and the heaviest 
+    If their weights <= limit, save both, move the pointers, increment res
+    Otherwise, move the right one, since it's too heavy 
+    """
     people.sort()
     l, r = 0, len(people) - 1
     res = 0
     while l <= r:
-        res += 1
         if people[l] + people[r] <= limit:
             l += 1
-        r -= 1
+            r += 1
+         else: r -= 1
+         res += 1
     return res
