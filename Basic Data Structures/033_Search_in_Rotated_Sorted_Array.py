6232556afa90b0e314870fa34d68571aa1c299a0
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-11-28 11:34:05 -0500

    Update 033_Search_in_Rotated_Sorted_Array.py
    best added

--- Basic Data Structures/033_Search_in_Rotated_Sorted_Array.py
+++ Basic Data Structures/033_Search_in_Rotated_Sorted_Array.py
@@ -1,3 +1,36 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        O(nlogn) and O(1)
+        if target < nums[m], target is smaller than everything to the right of m
+        if target > nums[m], target is larger than everything to the left of m 
+
+        Case 1: nums[m] >= nums[l]. Left is sorted from here 
+          nums[l] >= target and target < nums[m] --> process the left half
+          else: move to the right side
+        Case 2: nums[m] < nums[l]. Left is rotated, right is sorted. 
+          if nums[m] < target and target < nums[r] --> right contains target, move there
+          else: move to the left side
+        """
+        l, r = 0, len(nums) - 1
+        while l <= r: 
+            m = l + (r - l) // 2
+            # ideal solution
+            if nums[m] == target:
+                return m 
+            # the left portion from m is sorted
+            if nums[m] >= nums[l]:
+                if target >= nums[l] and target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if target <= nums[r] and target > nums[m]:
+                    l = m + 1
+                else:
+                    r = m - 1
+        return -1 
+        
 def search_one_pass(nums, target): # O(logN) and O(1)
     begin = 0
     end = len(nums) - 1 
