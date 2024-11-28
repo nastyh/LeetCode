1e2cb3cf09ad380e1f16d315f12b08ca96d73631
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-11-28 11:53:25 -0500

    Update 767_Reorganize_String.py
    detailed added

--- Basic Data Structures/767_Reorganize_String.py
+++ Basic Data Structures/767_Reorganize_String.py
@@ -1,3 +1,43 @@
+def reorganizeString_detailed(self, s: str) -> str:
+    """
+    O(nlog(lens(k))) k is only 26, so we can say complexity is O(n), where n is the length of s
+    O(k) for dict, but it's only 26 chars, so can say O(1)
+    build frequency dict: {a: 2, b: 1} for "aab"
+    throw into a heap, with an opposite sign so we have fast access to the most frequent element 
+    """
+    if len(s) == 0:
+        return ''
+    if len(s) == 1:
+        return s
+    res = []
+    d = Counter(s)
+    h = []
+    for k, v in d.items():
+        h.append([-v, k]) 
+    heapq.heapify(h) # [[-2, a], [-1, b]]
+    while h:
+        if len(h) >= 2: # while we have anything to combine
+            el1 = heapq.heappop(h) # most frequent element
+            res.append(el1[1]) # build the answer
+            el2 = heapq.heappop(h) # next most frequent element
+            res.append(el2[1]) # build the answer
+            # update the counts of the chars used
+            el1[0] += 1
+            el2[0] += 1
+            # push the updated results back to the heap
+            if el1[0] != 0:
+                heapq.heappush(h, [el1[0], el1[1]])
+            if el2[0] != 0:
+                heapq.heappush(h, [el2[0], el2[1]]) 
+        elif len(h) == 1: # we ran out of certain letters
+            el1 = heapq.heappop(h)
+            res.append(el1[1])
+            el1[0]+=1
+            if el1[0] != 0: # left with an unmatched char
+                return "" # cannot do it
+            else: return ''.join(ch for ch in res)
+    return ''.join(ch for ch in res)
+
 def reorganizeString(s):  # O(nlogn) b/c of sorting and O(n) 
     """
     Need to process greedily: most frequently occured char is mixed with the second most frequently occured character, etc. 
