029b2ef7b1191ae4e0c4990842a3b38aa1304f3c
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-11-28 13:44:59 -0500

    Update 230_kth_smallest_element_in_a_bst.py
    added two heap solutions

--- Basic Data Structures/230_kth_smallest_element_in_a_bst.py
+++ Basic Data Structures/230_kth_smallest_element_in_a_bst.py
@@ -36,4 +36,52 @@ class Solution:
             _helper(node.right)
         _helper(root)
 
+    def kthSmallest_heap(self, root: Optional[TreeNode], k: int) -> int: 
+        """
+        Traverse everything blindly
+        keap the heap
+        take the k-th smallest element
+        O(nlogk)
+        O(n)
+        """
+        h = []
+        if not root: return []
+        def _helper(node, h, k):
+            if not node: return
+            heapq.heapify(h)
+            heapq.heappush(h, node.val)
+            if node.left:
+                _helper(node.left, h, k)
+            if node.right:
+                _helper(node.right, h, k)
+        _helper(root, h, k)
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(h))
+        return res[-1]
+
+    def kthSmallest_heap_optimized(self, root: Optional[TreeNode], k: int) -> int: # keep building the list: O(n) both
+        """
+        Traverse but always keep the h of the size <= k
+        So we always have no more than k smallest elements we've seen so far
+        do it by changing the sign of the input
+        O(nlogk)
+        O(n)
+        """
+        h = []
+        if not root: return []
+        def _helper(node, h, k):
+            if not node: return
+            heapq.heapify(h)
+            if len(h) < k:
+                heapq.heappush(h, -node.val) # the largest value is on top
+            else: 
+                heapq.heappushpop(h, -node.val) # get rid of it so we only actually have the actual largest values
+            if node.left:
+                _helper(node.left, h, k)
+            if node.right:
+                _helper(node.right, h, k)
+        _helper(root, h, k)
+        return -h[0] # it's the k-th smallest element in the heap, but with an opposite sign it's the k-largest 
+
 
