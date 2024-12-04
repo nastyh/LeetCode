7d0b801a57bc1c8b7e19d015ba196767f086ca64
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-12-04 13:53:05 -0500

    Update 792_Number_of_Matching_Subsequences.py
    cleaner added

--- Basic Data Structures/792_Number_of_Matching_Subsequences.py
+++ Basic Data Structures/792_Number_of_Matching_Subsequences.py
@@ -1,3 +1,27 @@
+def numMatchingSubseq_optimal(self, s: str, words: List[str]) -> int:
+    """
+    O(n+m)
+    O(m), where n is the length of s and m the lengths of all words
+    word_d is first_letter: list of words starting with this letter
+    while iterating through letters of S, we will move our words through different buckets.
+    """
+    res, word_d = 0, defaultdict(list)
+
+    for word in words:
+        word_d[word[0]].append(word)
+        
+    for char in s:
+        word_list = word_d[char] # take the list of values for a given letter
+        word_d[char] = []
+            
+        for w in word_list: 
+            if len(w) == 1:
+                res += 1
+            else:
+                word_d[w[1]].append(w[1:])
+           
+    return res
+
 def numMatchingSubseq(s, words):
     res = 0
     if len(words) == 0:
