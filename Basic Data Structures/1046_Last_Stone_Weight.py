a9b075a6c891514cf9ed3a29495672540ea3e07f
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-12-23 16:54:47 -0500

    Update 1046_Last_Stone_Weight.py
    one more solution

--- Basic Data Structures/1046_Last_Stone_Weight.py
+++ Basic Data Structures/1046_Last_Stone_Weight.py
@@ -16,6 +16,29 @@ def lastStoneWeight(stones):  # O(NlogN) to create a heap and take out elements 
     return -heapq.heappop(h) if h else 0
 
 
+def lastStoneWeight_another_heap(self, stones: List[int]) -> int:
+    """
+    O(nlogn) both since it's a heap
+    it's a max heap, thus, with the negative values
+    We take out the elements in pairs, so need to make sure the heap
+    always has at least two elements. So we have the while loop
+    At the end, the heap will either have two or one elements left.
+    Process both variants manually 
+    """
+    if len(stones) == 1: return 1
+    h = []
+    for stone in stones:
+        heapq.heappush(h, -stone)
+    while len(h) > 2:
+        y_stone = - heapq.heappop(h)
+        x_stone = - heapq.heappop(h)
+        if x_stone < y_stone:
+            heapq.heappush(h, -(y_stone - x_stone))
+    if len(h) == 2:
+        return -heapq.heappop(h) - (-heapq.heappop(h))
+    else:
+        return -heapq.heappop(h)
+        
 def lastStoneWeight_sorting(stones):  # O(nlogn) and O(1)
     while len(stones) !=1:
         stones = sorted(stones)
