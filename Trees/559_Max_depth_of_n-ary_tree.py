0533a5355726a5e8fe026246c92a216839636f30
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-12-25 17:56:24 -0500

    Update 559_Max_depth_of_n-ary_tree.py
    clean solutions

--- Trees/559_Max_depth_of_n-ary_tree.py
+++ Trees/559_Max_depth_of_n-ary_tree.py
@@ -5,17 +5,35 @@ class Node:
         self.val = val
         self.children = children
 
-    def maxDepth(self, root: Node) -> int:  # times out
+    def maxDepth_dfs(self, root: 'Node') -> int:
+        """
+        O(n) both
+        DFS, recursive 
+        """
+        if not root: return 0
         res = 0
-        if not root:
-            return 0
-        levels = deque([root])
-        while levels:
-            curr = levels.popleft()
-            res += 1
-            if root.children:
-                levels.append(root.children)
-        return res
+        for child in root.children:
+            res = max(res, self.maxDepth(child))
+        return res + 1
+    
+    def maxDepth_bfs(self, root: 'Node') -> int:
+        """
+        O(n) both
+        Normal BFS
+        add (node, level) to the deque
+        if there is a child, add it, increase the level
+        keep track of the abs best result in res 
+        """
+        if not root: return 0
+        d = deque()
+        res = -math.inf
+        d.append((root, 1))
+        while d:
+            t, curr_depth = d.popleft()
+            res = max(res, curr_depth)
+            for ch in t.children:
+                d.append((ch, curr_depth + 1))
+        return res 
 
 
     def maxDepth_dfs(self, root):  # works
