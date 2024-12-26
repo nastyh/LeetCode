4a305db0f56579db209ce720c1b01bdde87cf365
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-12-26 14:39:31 -0500

    Update 408_Valid_Word_Abbreviation.py
    another solution

--- Basic Data Structures/408_Valid_Word_Abbreviation.py
+++ Basic Data Structures/408_Valid_Word_Abbreviation.py
@@ -15,3 +15,24 @@ def validWordAbbreviation(word, abbr):
                 j += 1 
             i += temp  
     return j == len(abbr) and i == len(word)
+    
+def validWordAbbreviation_another(word, abbr):
+    orig_pos = 0 # index at the original position
+    i = 0 # to iterate over an observation
+    while i < len(abbr):
+        # invalid cases
+        if abbr[i] == '0' or orig_pos >= len(word):
+            return False
+        num = ''
+        # processing numbers
+        while i < len(abbr) and abbr[i].isdigit():
+            num += abbr[i]
+            i += 1
+        if num:
+            orig_pos += int(num)
+        else:
+            if abbr[i]!= word[orig_pos]:
+                return False
+            orig_pos += 1
+            i += 1
+    return orig_pos == len(word)
