a284120c529f119dd19892c969343fabe598b436
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2024-12-26 14:45:18 -0500

    Update 568_Maximum_Vacation_days.py
    explanations added

--- Basic Data Structures/568_Maximum_Vacation_days.py
+++ Basic Data Structures/568_Maximum_Vacation_days.py
@@ -1,4 +1,20 @@
 def maxVacationDays(flights, days):
+    """
+    O(n^2*k) where n -- num of cities, k is num of weeks
+    O(nk) for the dp
+    a 2-D dp, in which dp[i][k] represents the maximum number of vacations,
+    that can be taken starting from the i-th city in the k-th week.
+    This dp is filled in from the  back(in terms of the week number).
+    We start from the i-th city in the k-th week and stay in the same city for the (k+1) th week.
+    Thus, the factor to be considered for updating the dp[i][k] entry will be given by: days[i][k]+dp[i,k+1].
+    We start from the i-th city in the k-th week and move to the j-th city in the (k+1) thweek. But, for changing the city in this manner,
+    we need to be able to move from the i-th city to the j-th
+    city i.e. flights[i][j] should be 1 for such i and j.
+    At the end, we need to find the maximum out of these two factors to update the dp[i][k] value.
+    In order to fill the dp values, we start by filling the entries for the last week and proceed backwards.
+    At last, the value of dp[0][0] gives the required result.
+    
+    """
     # build adjacency graph
     g = {row: set([row] + [col for col in range(len(flights[0])) if flights[row][col]]) for row in range(len(flights))}
 
