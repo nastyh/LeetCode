e9ee5d70710e1a2d118ab5aab27f0e9de08194b1
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2025-01-28 13:27:42 -0500

    Update 2215_find_the_diff_of_two_arrays.py
    more

--- Basic Data Structures/2215_find_the_diff_of_two_arrays.py
+++ Basic Data Structures/2215_find_the_diff_of_two_arrays.py
@@ -1,6 +1,14 @@
 class Solution:
     def findDifference_pythonic(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
         return [list(set(nums1) - set(nums2)), list(set(nums2) - set(nums1))]
+        
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        s1, s2 = set(nums1), set(nums2)
+        diff1 = [num for num in s1 if num not in s2]
+        diff2 = [num for num in s2 if num not in s1]
+        return [diff1, diff2]
+
+    
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]: # O(n) both
         # two passes, sets to make sure we don't readd the elements we've seen
         res, d1, d2 = [[], []], set(), set()
