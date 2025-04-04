8d1b1ba3ad5d4a7e022dcc144a0911ee20bdce58
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2025-04-04 09:53:05 -0400

    Update 1123_Lowest_Common_Ancestor_of_Deepest_Leaves.py
    added a bfs solution

--- Trees/1123_Lowest_Common_Ancestor_of_Deepest_Leaves.py
+++ Trees/1123_Lowest_Common_Ancestor_of_Deepest_Leaves.py
@@ -49,6 +49,36 @@ class TreeNode:
         n, d = _helper(root)
         return n
 
+    def lcaDeepestLeaves_bfs_another(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        """
+        O(n) both 
+        traverse level by level and have a dict shaped as node: its parent 
+        collect each level into level_nodes and rewrite it for each level
+        at the end, level_nodes will contain the deepest/last level 
+        than go through these results, reference the dict and extract a parent for each
+        node from the deepest level. 
+        """
+        if not root: return []
+        parent = {root: None}
+        q = deque([root])
+        deepest = []
+        while q:
+            level_nodes = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                level_nodes.append(node)
+                if node.left:
+                    parent[node.left] = node
+                    q.append(node.left)
+                if node.right:
+                    parent[node.right] = node
+                    q.append(node.right)
+            deepest = level_nodes 
+            nodes = deepest
+            while len(nodes) > 1:
+                nodes = list({parent[node] for node in nodes})
+        return nodes[0]
+
 
 
 if __name__ == '__main__':
