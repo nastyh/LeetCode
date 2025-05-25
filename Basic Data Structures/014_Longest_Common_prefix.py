16286db9a2cc8392b9dcc773d406d23dfd989557
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2025-05-25 11:04:52 -0400

    Update 014_Longest_Common_prefix.py
    more solutions added

--- Basic Data Structures/014_Longest_Common_prefix.py
+++ Basic Data Structures/014_Longest_Common_prefix.py
@@ -74,6 +74,57 @@ def longestCommonPrefix_trie(strs):
         t.insert(word)
     return t.getLongest()
 
+def longestCommonPrefix(self, strs: List[str]) -> str:
+        """
+        O(nm), num of strings, m length of the shortest string
+        O(1)
+        """
+        if len(strs) == 1: 
+            return strs[0]
+        # strs.sort(key=lambda x: len(x)) # no need to have it actually
+        for i in range(len(strs[0])):
+            ch = strs[0][i]
+            for word in strs[1:]:
+                # i >= len(word) covers a case when we have a longer potential prefix than the word in question
+                if i >= len(word) or word[i] != ch:
+                    return strs[0][:i]
+        return strs[0]
+    
+    def longestCommonPrefix_with_sorting(self, strs: List[str]) -> str:
+        """
+        still fast 
+        O(nlogn), n num of elements in strs
+        O(1)
+        """
+        if len(strs) == 1: 
+            return strs[0]
+        strs.sort(key=lambda x: len(x))
+        for i in range(len(strs[0])):
+            ch = strs[0][i]
+            for word in strs[1:]:
+                if word[i] != ch: # no need for i >= len(word) since the prefix candidates are always coming from the shortest word 
+                    return strs[0][:i]
+        return strs[0]
+    
+    def longestCommonPrefix_binary_search(self, strs: List[str]) -> str:
+        min_len = min(len(s) for s in strs)
+        def is_common_prefix(length: int):
+            """
+            check if all strings have the same prefix of length `length`
+            """
+            prefix = strs[0][:length]
+            return all(s.startswith(prefix) for s in strs)
+        
+        low, high = 0, min_len
+        while low < high:
+            mid = (low + high + 1) // 2  # upper mid to avoid infinite loop
+            if is_common_prefix(mid):
+                low = mid
+            else:
+                high = mid - 1
+        
+        return strs[0][:low]
+
 
 if __name__ == '__main__':
     print(longestCommonPrefix(["flower","flow","flight"]))
