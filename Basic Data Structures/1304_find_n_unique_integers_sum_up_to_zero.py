c1d2cc899ab40265d945a82fb07a8dfdd43024ed
MichaelKareev <42699002+nastyh@users.noreply.github.com>
2025-09-07 09:24:23 -0400

    Add files via upload

--- Basic Data Structures/1304_find_n_unique_integers_sum_up_to_zero.py
+++ Basic Data Structures/1304_find_n_unique_integers_sum_up_to_zero.py
@@ -1,4 +1,30 @@
+from typing import List
+
+
 class Solution:
+    def sumZero_clean(self, n: int) -> List[int]:
+        """
+        O(n) both to go over and create a list
+        Idea is that if n is odd, our res should be symmetrical around 0
+        if n == 5, we will have [-2, -1, 0, 1, 2]
+        If n is even, we don't include 0 in res, so for 
+        n = 4 we have [-2, -1, 1, 2]
+        Start with res = []
+        if n is odd, we can extend it as range(0, n//2+1) -- gives the right part 
+        if n is even, we can extend it without a zero as range(1, n//2+1) -- still gives us the right part
+        then for both cases we need to take the part that is [1, n//2+1], change the sign and add these numbers
+        The result will be in an order positives, negatives, but it's still ok 
+        """
+        res = []
+        if n % 2 == 1:
+            res.extend(x for x in range(0, n//2 + 1))
+        else:
+            res.extend(x for x in range(1, n//2 + 1))
+        res.extend(-x for x in range(1, n//2 + 1))
+        return res
+
+
+
     def sumZero(self, n: int) -> List[int]:
         """
         O(n) and O(n)
